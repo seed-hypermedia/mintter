6729b0fe1ca90ede1415a194a66944a535e44b41
Alexandr Burdiyan <burdiyan@gmail.com>
2020-03-09 12:09:20 +0100

    build: add grpc-web plugin for protoc to nix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,13 @@
 let
     pkgs = import ./nix/nixpkgs.nix;
     redo = pkgs.callPackage ./nix/redo {};
+    protoc-gen-grpc-web = pkgs.callPackage ./nix/protoc-gen-grpc-web {};
     go = pkgs.go_1_14;
     buildGoModule = (pkgs.buildGoModule.override { go = go; });
     go-protobuf = pkgs.callPackage ./nix/go-protobuf { buildGoModule = buildGoModule; };
     shell = pkgs.mkShell rec {
         buildInputs = [
+            protoc-gen-grpc-web
             pkgs.nodejs-13_x
             (pkgs.yarn.override { nodejs = pkgs.nodejs-13_x; })
             go
