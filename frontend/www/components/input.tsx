623bef4944d3104db05be78622055a72f5674a58
Horacio Herrera <hi@horacioh.com>
2020-03-10 01:43:20 +0100

    password connected

--- frontend/www/components/input.tsx
+++ frontend/www/components/input.tsx
@@ -1,17 +1,22 @@
+import React from 'react'
 export interface InputProps extends React.HTMLAttributes<HTMLInputElement> {
   type: 'text' | 'password'
+  name: string
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ref: any
 }
 
-export default function Input({
-  className = '',
-  type = 'text',
-  ...props
-}: InputProps) {
-  return (
+// eslint-disable-next-line react/display-name
+const Input = React.forwardRef(
+  ({className = '', type = 'text', ...props}: InputProps, ref) => (
     <input
       className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 focus:outline-none focus:shadow-outline ${className}`}
       type={type}
+      ref={ref}
       {...props}
     />
-  )
-}
+  ),
+)
+
+export default Input
--- frontend/www/pages/welcome/create-password.tsx
+++ frontend/www/pages/welcome/create-password.tsx
@@ -6,64 +6,101 @@ import {NextButton, BackButton} from '../../components/welcome-buttons'
 import Footer from '../../components/footer'
 import Content from '../../components/content'
 import Input from '../../components/input'
+import {useRouter} from 'next/router'
+import {useForm} from 'react-hook-form'
 
 export default function CreatePassword() {
+  const {register, watch, handleSubmit, errors} = useForm({
+    mode: 'onChange',
+  })
+
+  const router = useRouter()
+
+  function onSubmit(data) {
+    console.log('submit => ', data)
+  }
+
+  async function handleNext(e) {
+    await handleSubmit(onSubmit)(e)
+    await router.push('/welcome/edit-profile')
+  }
+
+  const psswd = watch('password')
   return (
     <Layout>
-      <Container>
-        <Heading>Welcome!</Heading>
-        <P className="text-center">
-          Set a password to encrypt your identity. This password will be needed
-          to unlock your identity in the future
-        </P>
-        <Content className="flex-wrap flex w-full flex-col">
-          <div className="flex-1">
-            <label
-              className="block text-gray-500 text-xs font-semibold mb-1"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <Input
-              id="password"
-              type="password"
-              placeholder="******************"
-            />
-            <p className="text-red-500 text-xs italic">
-              Please choose a password.
-            </p>
-          </div>
-          <div className="flex-1 mt-6">
-            <label
-              className="block text-gray-500 text-xs font-semibold mb-1"
-              htmlFor="repeat_password"
-            >
-              Retype Password
-            </label>
-            <Input
-              id="repeat_password"
-              type="password"
-              placeholder="******************"
-            />
-            <p className="text-red-500 text-xs italic">
-              Please choose a password.
-            </p>
-          </div>
-        </Content>
-      </Container>
-      <Footer className="flex-none">
+      <form>
         <Container>
-          <div className="flex w-full justify-between">
-            <BackButton
-              to="/welcome"
-              onClick={() => console.log('starting over!')}
-            >
-              ← start over
-            </BackButton>
-            <NextButton to="/welcome/edit-profile">Next →</NextButton>
-          </div>
+          <Heading>Welcome!</Heading>
+          <P className="text-center">
+            Set a password to encrypt your identity. This password will be
+            needed to unlock your identity in the future
+          </P>
+          <Content className="flex-wrap flex w-full flex-col">
+            <div className="flex-1">
+              <label
+                className="block text-gray-500 text-xs font-semibold mb-1"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <Input
+                name="password"
+                id="password"
+                type="password"
+                placeholder="******************"
+                ref={register({required: true, minLength: 8})}
+              />
+              {errors.password && (
+                <p className="text-red-500 text-xs italic">
+                  Please choose a password with more than 8 characters.
+                </p>
+              )}
+            </div>
+            <div className="flex-1 mt-6">
+              <label
+                className="block text-gray-500 text-xs font-semibold mb-1"
+                htmlFor="repeat_password"
+              >
+                Retype Password
+              </label>
+              <Input
+                name="repeat_password"
+                id="repeat_password"
+                type="password"
+                placeholder="******************"
+                ref={register({
+                  required: true,
+                  validate: value => value === psswd,
+                })}
+              />
+              {errors.repeat_password && (
+                <p className="text-red-500 text-xs italic">
+                  Please choose a password.
+                </p>
+              )}
+            </div>
+          </Content>
         </Container>
-      </Footer>
+        <Footer className="flex-none">
+          <Container>
+            <div className="flex w-full justify-between">
+              <BackButton
+                to="/welcome"
+                onClick={() => console.log('starting over!')}
+              >
+                ← start over
+              </BackButton>
+              <NextButton
+                type="submit"
+                onClick={handleNext}
+                disabled={Object.keys(errors).length !== 0}
+              >
+                Next →
+              </NextButton>
+            </div>
+          </Container>
+        </Footer>
+      </form>
     </Layout>
   )
 }
