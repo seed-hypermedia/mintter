12d3cad0724636a1031246c5df0d2322e24d67ca
Horacio Herrera <hi@horacioh.com>
2020-03-17 03:07:01 +0100

    snapshot to input

--- frontend/www/components/__tests__/input.js
+++ frontend/www/components/__tests__/input.js
@@ -3,7 +3,7 @@ import {render} from '@testing-library/react'
 import Input from '../input'
 
 test('<Input />', async () => {
-  const {getByLabelText, debug} = render(
+  const {container, getByLabelText} = render(
     <form>
       <label htmlFor="input-demo">demo</label>
       <Input name="input-demo" id="input-demo" />
@@ -11,6 +11,15 @@ test('<Input />', async () => {
   )
 
   const input = getByLabelText(/demo/i)
+
+  expect(input).toMatchInlineSnapshot(`
+    <input
+      class="block w-full border border-gray-300 rounded bg-white px-3 py-2 focus:outline-none focus:border-gray-600 "
+      id="input-demo"
+      name="input-demo"
+      type="text"
+    />
+  `)
 
   const results = await axe(input)
   expect(results).toHaveNoViolations()
