582b5980e51e49c2ab64f087c0fa6862b07211f6
Alexandr Burdiyan <burdiyan@gmail.com>
2020-03-18 15:16:15 +0100

    ci(backend): add ci to test backend with nix

--- nix/go-protobuf/default.nix
+++ nix/go-protobuf/default.nix
@@ -12,6 +12,7 @@ buildGoModule {
   };
 
   modSha256 = "0sjjj9z1dhilhpc8pq4154czrb79z9cm044jvn75kxcjv6v5l2m5";
+  subPackages = ["protoc-gen-go/"];
 
   meta = with lib; {
     description = "Go plugin for Protobuf compiler";
