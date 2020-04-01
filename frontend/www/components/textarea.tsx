ddc0b9993da57d592a958329971a6dd3a2e29456
Horacio Herrera <hi@horacioh.com>
2020-04-01 16:57:29 +0200

    feat(edit-profile): add bio to welcome edit profile

--- frontend/www/components/textarea.tsx
+++ frontend/www/components/textarea.tsx
@@ -1,63 +1,76 @@
-import React from 'react'
+import React, {forwardRef} from 'react'
 import {css} from 'emotion'
 
-export default function EditorTitle({
-  value = '',
-  onChange = null,
-  className = '',
-  ...props
-}) {
-  const [text, setText] = React.useState<string>(value)
-  const ref = React.useRef<HTMLTextAreaElement>()
-  const divRef = React.useRef<HTMLDivElement>()
-
-  function handleChange(e) {
-    setText(e.target.value)
-    onChange && onChange(e.target.value)
-  }
-
-  function handleInput(e) {}
-
-  React.useEffect(() => {
-    const textarea = ref.current
-    textarea.addEventListener('input', handleInput)
-
-    return () => {
-      textarea.removeEventListener('input', handleInput)
-    }
-  }, [])
-
-  React.useEffect(() => {
-    const txt = ref.current
-    if (ref.current) {
-      const div = divRef.current
-      const content = txt.value
-      div.innerHTML = content
-      div.style.visibility = 'hidden'
-      div.style.display = 'block'
-      txt.style.height = `${div.offsetHeight}px`
-      div.style.visibility = 'visible'
-      div.style.display = 'none'
-    }
-  }, [text])
-
-  return (
-    <>
-      <textarea
-        ref={ref}
-        {...props}
-        value={text}
-        onChange={handleChange}
-        className={`resize-none overflow-hidden text-base leading-normal w-full outline-none bg-transparent ${css`
-          word-wrap: break-word;
-        `} ${className}`}
-      />
-      <div
-        ref={divRef}
-        className={`hidden whitespace-pre-wrap break-words text-base leading-normal ${css`
-          word-wrap: break-word;
-        `} ${className}`}
-      />
-    </>
-  )
+interface TextareaProps extends React.HTMLAttributes<HTMLTextAreaElement> {
+  value?: string
+  name?: string
 }
+
+// eslint-disable-next-line react/display-name
+const Textarea = forwardRef(
+  (
+    {value = '', onChange = null, className = '', ...props}: TextareaProps,
+    ref,
+  ) => {
+    const [text, setText] = React.useState<string>(value)
+    const innerRef = React.useRef<HTMLTextAreaElement>()
+    const divRef = React.useRef<HTMLDivElement>()
+
+    function handleChange(e) {
+      setText(e.target.value)
+      onChange && onChange(e.target.value)
+    }
+
+    function handleInput(e) {}
+
+    React.useEffect(() => {
+      const textarea = innerRef.current
+      textarea.addEventListener('input', handleInput)
+
+      return () => {
+        textarea.removeEventListener('input', handleInput)
+      }
+    }, [])
+
+    console.log('ref => ', ref)
+
+    React.useEffect(() => {
+      const txt = innerRef.current
+      if (innerRef.current) {
+        const div = divRef.current
+        const content = txt.value
+        div.innerHTML = content
+        div.style.visibility = 'hidden'
+        div.style.display = 'block'
+        txt.style.height = `${div.offsetHeight}px`
+        div.style.visibility = 'visible'
+        div.style.display = 'none'
+      }
+    }, [text])
+
+    return (
+      <>
+        <textarea
+          ref={r => {
+            innerRef.current = r
+            ref(r)
+          }}
+          {...props}
+          value={text}
+          onChange={handleChange}
+          className={`resize-none overflow-hidden text-base leading-normal w-full outline-none bg-transparent ${css`
+            word-wrap: break-word;
+          `} ${className}`}
+        />
+        <div
+          ref={divRef}
+          className={`hidden whitespace-pre-wrap break-words text-base leading-normal ${css`
+            word-wrap: break-word;
+          `} ${className}`}
+        />
+      </>
+    )
+  },
+)
+
+export default Textarea
