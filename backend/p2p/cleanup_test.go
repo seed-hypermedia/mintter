d9b0ff594a1d495fc6536fedef5b849abdd14bf7
Alexandr Burdiyan <burdiyan@gmail.com>
2020-04-21 13:07:40 +0200

    test(backend/p2p): add cleanup tests

---
+++ backend/p2p/cleanup_test.go
@@ -0,0 +1,47 @@
+package p2p
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCleanup(t *testing.T) {
+	var errs []error
+
+	e1 := errors.New("one")
+	e2 := errors.New("two")
+	e3 := errors.New("three")
+
+	expected := []error{e3, e2, e1}
+
+	c := cleanup{
+		funcCloser(func() error {
+			errs = append(errs, e1)
+			return e1
+		}),
+		funcCloser(func() error {
+			errs = append(errs, e2)
+			return e2
+		}),
+		funcCloser(func() error {
+			return nil
+		}),
+		funcCloser(func() error {
+			errs = append(errs, e3)
+			return e3
+		}),
+	}
+
+	err := c.Close()
+	require.Error(t, err)
+	require.Equal(t, expected, errs)
+	require.Contains(t, err.Error(), "three; two; one")
+}
+
+type funcCloser func() error
+
+func (f funcCloser) Close() error {
+	return f()
+}
