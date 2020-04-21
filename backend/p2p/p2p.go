005b91f596131da9cad450a5cd44405f60be1473
Alexandr Burdiyan <burdiyan@gmail.com>
2020-04-21 13:07:40 +0200

    test(backend/p2p): add cleanup tests

--- backend/p2p/p2p.go
+++ backend/p2p/p2p.go
@@ -4,7 +4,6 @@ package p2p
 import (
 	"context"
 	"fmt"
-	"io"
 	"net"
 	"path/filepath"
 	"time"
@@ -282,18 +281,4 @@ func logClose(l *zap.Logger, fn func() error, errmsg string) {
 	if err := fn(); err != nil {
 		l.Warn("CloseError", zap.Error(err), zap.String("details", errmsg))
 	}
-}
-
-type cleanup []io.Closer
-
-func (c cleanup) Close() error {
-	var err error
-
-	// We have to close in reverse order because some later dependencies
-	// can use previous ones. This is similar to defer statement.
-	for i := len(c) - 1; i >= 0; i-- {
-		err = multierr.Append(err, c[i].Close())
-	}
-
-	return err
 }
