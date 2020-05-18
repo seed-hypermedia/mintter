75165210ad3dfac1c2d6cffe791d96bb3c8a420a
Horacio Herrera <hi@horacioh.com>
2020-05-18 15:56:22 +0200

    feat(section): dotted line divider

--- frontend/www/components/section.tsx
+++ frontend/www/components/section.tsx
@@ -1,6 +1,7 @@
 import React, {RefObject} from 'react'
 import {RenderElementProps, ReactEditor} from 'slate-react'
 import {Icons} from '@mintter/editor'
+import {css} from 'emotion'
 import {Editor} from 'slate'
 
 export function Section(
@@ -20,21 +21,44 @@ export function Section(
       data-slate-type={element.type}
       // {...attributes}
       ref={ref}
-      className={`relative px-8 py-8 pt-4 mt-8 first:mt-0 border-t first:border-0 border-muted group`}
+      className={`relative px-8 pt-12 pb-16 group ${css`
+        &:after {
+          display: ${path[0] === 0 ? 'none' : 'block'};
+          content: '';
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 2px;
+          z-index: 100;
+          background-image: linear-gradient(
+            to right,
+            black 33%,
+            rgba(255, 255, 255, 0) 0%
+          );
+          background-position: bottom;
+          background-size: 10px 2px;
+          background-repeat: repeat-x;
+        }
+
+        &:first {
+          &:after {
+            display: none;
+          }
+        }
+      `}`}
     >
       <div
         contentEditable={false}
-        className="absolute right-0 top-0 bg-gray-800 rounded shadows-md opacity-0 group-hover:opacity-100 transition duration-200 theme-dark flex items-center pl-2"
+        className="absolute right-0 top-0 mt-4 mr-4 bg-gray-800 rounded shadows-md opacity-0 group-hover:opacity-100 transition duration-200 theme-dark flex items-center pl-2 text-xs leading-none text-body"
       >
-        <p className="text-body font-bold text-xs leading-none border-r px-2">
-          Section text
+        <p className="font-bold border-r px-2">Section text</p>
+        <p className={`text-body border-r px-2 ${css``}`}>
+          <span>Characters:</span>{' '}
+          {/* TODO: FIX avoid characters to juno when change chars number */}
+          <span className={`inline-block text-right`}>{sectionChars}</span>
         </p>
-        <p className="text-body text-xs leading-none border-r px-2">
-          Characters: {sectionChars}
-        </p>
-        <p className="text-body text-xs leading-none border-r px-2">
-          Royalties $0.02
-        </p>
+        <p className=" border-r px-2">Royalties $0.02</p>
         <button
           className="px-3 py-2"
           onClick={() => alert('section settings clicked')}
