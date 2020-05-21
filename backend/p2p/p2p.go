77432fc7c3f91d2c9104cd9b3cc73b5be3fda057
Alexandr Burdiyan <burdiyan@gmail.com>
2020-05-21 12:22:57 +0200

    chore(backend/p2p): remove dead code [skip ci]

--- backend/p2p/p2p.go
+++ backend/p2p/p2p.go
@@ -16,7 +16,6 @@ import (
 	"mintter/backend/store"
 
 	"github.com/ipfs/go-datastore"
-	format "github.com/ipfs/go-ipld-format"
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
@@ -177,10 +176,6 @@ func (n *Node) Account() identity.Account {
 // Addrs return p2p multiaddresses this node is listening on.
 func (n *Node) Addrs() []multiaddr.Multiaddr {
 	return n.addrs
-}
-
-func (n *Node) unwrapIPLD(ipld format.Node, v interface{}) {
-
 }
 
 // makeHost creates a new libp2p host.
