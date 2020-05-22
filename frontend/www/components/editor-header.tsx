16d900be0c10d555e35d02669c80fe708ae86808
Horacio Herrera <hi@horacioh.com>
2020-05-23 00:35:29 +0200

    fix(header): add library link to logo

--- frontend/www/components/editor-header.tsx
+++ frontend/www/components/editor-header.tsx
@@ -1,6 +1,7 @@
 import Logo from './logo_square'
 import IconSettings from './icon-settings'
 import IconEye from './icon-eye'
+import Link from 'next/link'
 
 interface Props {
   onPublish: () => void
@@ -10,7 +11,11 @@ export default function EditorHeader({onPublish}: Props) {
   return (
     <div className="flex items-center mx-4">
       <div className="text-primary">
-        <Logo width="50px" className="fill-current" />
+        <Link href="/app/library">
+          <a>
+            <Logo width="50px" className="fill-current" />
+          </a>
+        </Link>
       </div>
       <div className="flex-1"></div>
       <button className="text-muted-hover bg-transparent hover:shadow-lg hover:text-muted-hover p-2 rounded-full shadow transition duration-200">
