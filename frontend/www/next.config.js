a699f42ae3aeb623c4332ddffab17449e6ab6cb8
Horacio Herrera <hi@horacioh.com>
2020-06-01 17:25:04 +0200

    feat(env): set env vars for second client

--- frontend/www/next.config.js
+++ frontend/www/next.config.js
@@ -1,3 +1,11 @@
+const port = process.env.MINTTER_PORT || `55001`
+const hostname = process.env.MINTTER_HOSTNAME || `http://localhost`
+
 module.exports = {
   /* config options here */
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    MINTTER_HOSTNAME: hostname,
+    MINTTER_PORT: port,
+  },
 }
