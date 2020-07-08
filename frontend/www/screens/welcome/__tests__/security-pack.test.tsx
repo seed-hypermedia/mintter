62fb5f8576b5737cb30324e21eb2204b7d693b71
Horacio Herrera <hi@horacioh.com>
2020-07-08 13:24:37 +0200

    remove retype-seed from welcome flow

--- frontend/www/screens/welcome/__tests__/security-pack.test.tsx
+++ frontend/www/screens/welcome/__tests__/security-pack.test.tsx
@@ -3,6 +3,7 @@ import {
   screen,
   userEvent,
   waitFor,
+  act,
   waitForLoadingToFinish,
   fireEvent,
 } from 'test/app-test-utils'
@@ -10,6 +11,7 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import {ThemeProvider} from 'shared/themeContext'
 import {ProfileProvider} from 'shared/profileContext'
 import {MintterProvider} from 'shared/mintterContext'
+import {ToastProvider} from 'react-toast-notifications'
 import WelcomeProvider from 'shared/welcomeProvider'
 import SecurityPack from '../security-pack'
 import {GenSeedResponse, Profile} from '@mintter/proto/mintter_pb'
@@ -17,29 +19,50 @@ import * as clientMock from 'shared/mintterClient'
 
 jest.mock('shared/mintterClient')
 
+const mnemonicList = ['word-1', 'word-2', 'word-3']
+
 beforeEach(() => {
   clientMock.genSeed.mockResolvedValueOnce({
-    getMnemonicList: jest.fn(() => ['word-1', 'word-2', 'word-3']),
+    getMnemonicList: jest.fn(() => mnemonicList),
   })
 
   clientMock.getProfile.mockResolvedValueOnce(null)
+
+  clientMock.createProfile = jest.fn()
 })
 
 async function renderWelcomeScreen() {
   const route = `/welcome/security-pack`
-  const utils = await render(<SecurityPack />, {route})
+  const utils = await render(<SecurityPack />, {
+    route,
+    wrapper: ({children}) => (
+      <Router>
+        <ProfileProvider>
+          <WelcomeProvider
+            value={{
+              state: {progress: 1, aezeedPassphrase: ''},
+              dispatch: jest.fn(),
+            }}
+          >
+            <ToastProvider>{children}</ToastProvider>
+          </WelcomeProvider>
+        </ProfileProvider>
+      </Router>
+    ),
+  })
   const nextBtn = screen.getByText(/Next →/i)
 
   return {
     ...utils,
     nextBtn,
+    mnemonicList,
   }
 }
 
 test('Welcome - Security Pack Screen', async () => {
-  const {nextBtn} = await renderWelcomeScreen()
+  const {nextBtn, mnemonicList} = await renderWelcomeScreen()
 
-  waitFor(() => {
+  await act(async () => {
     expect(clientMock.genSeed).toBeCalledTimes(1)
   })
 
@@ -47,4 +70,18 @@ test('Welcome - Security Pack Screen', async () => {
 
   expect(nextBtn).toBeInTheDocument()
   expect(nextBtn).not.toBeDisabled()
+
+  await act(async () => await userEvent.click(nextBtn))
+
+  await waitFor(() => {
+    expect(clientMock.createProfile).toHaveBeenCalledTimes(1)
+  })
+
+  await waitFor(() => {
+    expect(clientMock.createProfile).toHaveBeenCalledWith({
+      walletPassword: '',
+      aezeedPassphrase: '',
+      mnemonicList,
+    })
+  })
 })
