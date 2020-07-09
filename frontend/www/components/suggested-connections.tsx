80845ef16b695c5b43b73e70e62ac63477e4a5ae
Horacio Herrera <hi@horacioh.com>
2020-07-09 16:59:47 +0200

    suggested connection component

--- frontend/www/components/suggested-connections.tsx
+++ frontend/www/components/suggested-connections.tsx
@@ -46,7 +46,7 @@ export function SuggestedConnections() {
     return <ErrorMessage error={error} />
   }
 
-  
+  const list = resolvedData?.toObject().profilesList
 
   return (
     <div
@@ -56,61 +56,69 @@ export function SuggestedConnections() {
       `}`}
     >
       <h3 className="font-bold text-heading">Suggested Connections</h3>
-      <ul>
-        {resolvedData?.toObject().profilesList.map(c => {
-          const isConnected = c.connectionStatus === ConnectionStatus.CONNECTED
+      {list.length === 0 ? (
+        <p className="p-4 mt-4 rounded bg-background-muted text-body">no suggestions available :(</p>
+      ) : (
+        <ul>
+          {list.map(c => {
+            const isConnected =
+              c.connectionStatus === ConnectionStatus.CONNECTED
 
-          return (
-            <li
-              key={c.accountId}
-              className={`text-body text-sm mt-2 flex items-center ${
-                isConnected ? 'opacity-100' : 'opacity-50'
-              }`}
-            >
-              <Tippy
-                content={
-                  <span
-                    className={`px-2 py-1 text-xs font-light transition duration-200 rounded bg-muted-hover ${css`
-                      background-color: #333;
-                      color: #ccc;
-                    `}`}
-                  >
-                    {isConnected ? 'connected' : 'not connected'}
-                  </span>
-                }
+            return (
+              <li
+                key={c.accountId}
+                className={`text-body text-sm mt-2 flex items-center ${
+                  isConnected ? 'opacity-100' : 'opacity-50'
+                }`}
               >
-                <div
-                  className={`w-2 h-2 rounded-full mr-2 flex-none ${connectionStatusColor(
-                    c,
-                  )}`}
-                />
-              </Tippy>
-              <Tippy
-                delay={1000}
-                content={
-                  <span
-                    className={`px-2 py-1 text-xs font-light transition duration-200 rounded bg-muted-hover ${css`
-                      background-color: #333;
-                      color: #ccc;
-                    `}`}
-                  >
-                    {c.accountId}
-                  </span>
-                }
-              >
-                <div className="flex items-center group">
-                  <span className="text-primary hover:text-primary-hover hover:underline hover:cursor-not-allowed mr-2">
-                    {`${c.username} (${c.accountId.slice(-8)})`}
-                  </span>
-                  <button onClick={() => handlePeerConnection(c.addrsList)} className="opacity-0 group-hover:opacity-100 transition duration-75 px-2 rounded-full bg-info hover:bg-info-hover text-white">
-                    connect
-                  </button>
-                </div>
-              </Tippy>
-            </li>
-          )
-        })}
-      </ul>
+                <Tippy
+                  content={
+                    <span
+                      className={`px-2 py-1 text-xs font-light transition duration-200 rounded bg-muted-hover ${css`
+                        background-color: #333;
+                        color: #ccc;
+                      `}`}
+                    >
+                      {isConnected ? 'connected' : 'not connected'}
+                    </span>
+                  }
+                >
+                  <div
+                    className={`w-2 h-2 rounded-full mr-2 flex-none ${connectionStatusColor(
+                      c,
+                    )}`}
+                  />
+                </Tippy>
+                <Tippy
+                  delay={1000}
+                  content={
+                    <span
+                      className={`px-2 py-1 text-xs font-light transition duration-200 rounded bg-muted-hover ${css`
+                        background-color: #333;
+                        color: #ccc;
+                      `}`}
+                    >
+                      {c.accountId}
+                    </span>
+                  }
+                >
+                  <div className="flex items-center group">
+                    <span className="text-primary hover:text-primary-hover hover:underline hover:cursor-not-allowed mr-2">
+                      {`${c.username} (${c.accountId.slice(-8)})`}
+                    </span>
+                    <button
+                      onClick={() => handlePeerConnection(c.addrsList)}
+                      className="opacity-0 group-hover:opacity-100 transition duration-75 px-2 rounded-full bg-info hover:bg-info-hover text-white"
+                    >
+                      connect
+                    </button>
+                  </div>
+                </Tippy>
+              </li>
+            )
+          })}
+        </ul>
+      )}
     </div>
   )
 }
