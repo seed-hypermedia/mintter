09fd924d318f3023ce6746fc3c67341553d31e75
Horacio Herrera <hi@horacioh.com>
2020-07-23 01:30:11 +0200

    refactor: headings

--- frontend/packages/editor/src/options.ts
+++ frontend/packages/editor/src/options.ts
@@ -1,8 +1,9 @@
-import {PARAGRAPH_OPTIONS} from './elements'
+import {PARAGRAPH_OPTIONS, HEADING_OPTIONS} from './elements'
 import {BOLD_OPTIONS} from './marks'
 
 export const options = {
   ...PARAGRAPH_OPTIONS,
+  ...HEADING_OPTIONS,
   //marks
   ...BOLD_OPTIONS,
 }
--- frontend/packages/editor/src/renderElements/renderElements.ts
+++ frontend/packages/editor/src/renderElements/renderElements.ts
@@ -1,6 +1,3 @@
-import {renderHeadingOne} from './renderHeadingOne'
-import {renderHeadingTwo} from './renderHeadingTwo'
-import {renderHeadingThree} from './renderHeadingThree'
 import {renderBlockquote} from './renderBlockquote'
 import {renderUnorderedList} from './renderUnorderedList'
 import {renderOrderedList} from './renderOrderedList'
@@ -10,9 +7,6 @@ import {renderCodeBlockElement} from './renderCodeBlockElement'
 import {renderImageBlock} from './renderImageBlock'
 
 export const renderElements = [
-  renderHeadingOne(),
-  renderHeadingTwo(),
-  renderHeadingThree(),
   renderBlockquote(),
   renderUnorderedList(),
   renderOrderedList(),
