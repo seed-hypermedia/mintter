94a10b2e773203caa8818f3d46ec1f6ac26f478e
Horacio Herrera <hi@horacioh.com>
2020-08-08 14:49:00 +0200

    insertBlock logic working

--- frontend/packages/editor/src/HelperPlugin/transforms/insertBlock.ts
+++ frontend/packages/editor/src/HelperPlugin/transforms/insertBlock.ts
@@ -1,13 +1,9 @@
-import {
-  Editor,
-  Transforms,
-  // Path,
-  Range,
-} from 'slate'
+import {Editor, Transforms, Range, Path} from 'slate'
 
 import {v4 as uuid} from 'uuid'
 import {InsertBlockOptions} from '../types'
 import {ELEMENT_IMAGE, ELEMENT_BLOCK, ELEMENT_PARAGRAPH} from '../../elements'
+import {ReactEditor} from 'slate-react'
 
 export function insert(editor, {type, location}) {
   switch (type) {
@@ -42,44 +38,10 @@ export function insert(editor, {type, location}) {
   }
 }
 
-export function set(editor, {type, location}) {
-  switch (type) {
-    case ELEMENT_IMAGE:
-      Transforms.setNodes(
-        editor,
-        {
-          type,
-          url: '',
-          children: [{text: ''}],
-        },
-        {at: location},
-      )
-      return
-    case ELEMENT_BLOCK:
-      Transforms.insertNodes(
-        editor,
-        {
-          type,
-          children: [
-            {
-              type: ELEMENT_PARAGRAPH,
-              children: [{text: ''}],
-            },
-          ],
-        },
-        {at: location},
-      )
-      return
-  }
-}
-
 export const insertBlock = (editor: Editor, options: InsertBlockOptions) => {
   if (Range.isRange(options.target)) {
     // is called from the editor with the `/`
     Transforms.delete(editor, {reverse: true, unit: 'line'})
-    if (Editor.string(editor, options.target)) {
-    }
-    console.log('inEmpty => ', Editor.string(editor, options.target))
     const parent = Editor.above(editor, {
       match: n => [ELEMENT_BLOCK].includes(n.type as string),
     })
@@ -89,38 +51,42 @@ export const insertBlock = (editor: Editor, options: InsertBlockOptions) => {
       // const nextBlockPath = Path.next(parentBlockPath)
 
       if (options.type === ELEMENT_IMAGE) {
-        Transforms.insertNodes(
-          editor,
-          {
-            type: options.type,
-            id: uuid(),
-            url: '',
-            children: [{text: ''}],
-          },
-          {at: parentBlockPath},
-        )
+        insert(editor, {type: options.type, location: parentBlockPath})
       }
     }
   } else {
     // is called from the `+` button
-    // const nextPath = Path.next(options.target)
+    ReactEditor.focus(editor as ReactEditor)
+    const nextPath = Path.next(options.target)
+    if (Editor.string(editor, options.target)) {
+      // if has content
+      if (options.type === ELEMENT_BLOCK) {
+        // content + block = insert below
+        console.log('insertBlock => content + block')
+        insert(editor, {type: options.type, location: nextPath})
+        Transforms.select(editor, nextPath)
+        return
+      }
 
-    if (options.type === ELEMENT_IMAGE) {
-      Transforms.insertNodes(
-        editor,
-        {
-          type: options.type,
-          id: uuid(),
-          url: '',
-          children: [{text: ''}],
-        },
-        {at: options.target},
-      )
+      if (options.type === ELEMENT_IMAGE) {
+        // content + image = insert bellow + a new block below image
+        console.log('insertBlock => content + image')
+        insert(editor, {type: ELEMENT_BLOCK, location: nextPath})
+        insert(editor, {type: options.type, location: nextPath})
+      }
     } else {
-      if (Editor.string(editor, options.target)) {
-        insert(editor, {type: options.type, location: options.target})
-      } else {
+      if (options.type === ELEMENT_BLOCK) {
+        // blank + block = select current block
+        console.log('insertBlock => blank + block')
         Transforms.select(editor, options.target)
+        return
+      }
+
+      if (options.type === ELEMENT_IMAGE) {
+        console.log('insertBlock => blank + image')
+        // blank + image = insert image in the same path
+        insert(editor, {type: options.type, location: options.target})
+        Transforms.select(editor, nextPath)
       }
     }
   }
