b47ea8391d8c77ca7ff6a90e58d733730230fc78
Alexandr Burdiyan <burdiyan@gmail.com>
2020-09-07 17:19:35 +0200

    refactor(backend/daemon): improve lazy initialization

---
+++ backend/daemon/daemon_test.go
@@ -0,0 +1,52 @@
+package daemon
+
+import (
+	"context"
+	"mintter/backend/config"
+	"mintter/backend/testutil"
+	v2 "mintter/proto/v2"
+	"testing"
+	"time"
+
+	"github.com/burdiyan/go/mainutil"
+	"github.com/sanity-io/litter"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+)
+
+func TestDaemon(t *testing.T) {
+	t.SkipNow()
+
+	ctx, cancel := context.WithCancel(mainutil.TrapSignals())
+	defer cancel()
+
+	cfg := config.Config{
+		HTTPPort:      "55001",
+		GRPCPort:      "55002",
+		NoOpenBrowser: true,
+		RepoPath:      testutil.MakeRepoPath(t),
+		P2P: config.P2P{
+			Addr:        "/ip4/0.0.0.0/tcp/0",
+			NoBootstrap: true,
+			NoRelay:     true,
+			NoTLS:       true,
+		},
+	}
+
+	done := make(chan struct{}, 1)
+	go func() {
+		require.NoError(t, Run(ctx, cfg))
+		done <- struct{}{}
+	}()
+	time.Sleep(2 * time.Second)
+	conn, err := grpc.DialContext(ctx, "localhost:55002", grpc.WithInsecure())
+	require.NoError(t, err)
+	client := v2.NewDocumentsClient(conn)
+	resp, err := client.ListDocuments(ctx, &v2.ListDocumentsRequest{})
+	require.NoError(t, err)
+
+	litter.Dump(resp)
+
+	cancel()
+	<-done
+}
