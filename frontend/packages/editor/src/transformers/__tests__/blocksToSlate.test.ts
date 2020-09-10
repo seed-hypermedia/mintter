556bdbf56057adc23be18871fdf726fe694a149c
Horacio Herrera <hi@horacioh.com>
2020-09-10 18:00:48 +0200

    redirect to publication after publish a draft

---
+++ frontend/packages/editor/src/transformers/__tests__/blocksToSlate.test.ts
@@ -0,0 +1,52 @@
+import {
+  Block,
+  Paragraph,
+  InlineElement,
+  TextStyle,
+} from '@mintter/proto/v2/documents_pb'
+import {toBlocks, makeProto} from '../transformers'
+
+test('should return the proper result', () => {
+  const slateBlocks = [
+    {
+      type: 'block',
+      id: 'block-test-id',
+      author: '',
+      children: [
+        {
+          type: 'p',
+          children: [
+            {
+              text: 'Hello ',
+            },
+            {
+              text: 'World!',
+              bold: true,
+            },
+          ],
+        },
+      ],
+    },
+  ]
+
+  const expected: Block[] = [
+    makeProto(new Block(), {
+      id: 'block-2',
+      paragraph: makeProto(new Paragraph(), {
+        inlineElements: [
+          makeProto(new InlineElement(), {
+            text: 'Hello ',
+          }),
+          makeProto(new InlineElement(), {
+            text: 'World!',
+            textStyle: makeProto(new TextStyle(), {
+              bold: true,
+            }),
+          }),
+        ],
+      }),
+    }),
+  ]
+
+  expect(toBlocks(slateBlocks)).toEqual(expected)
+})
