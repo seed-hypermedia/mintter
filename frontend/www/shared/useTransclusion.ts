a957952f2b2802b09f629e55e1ec59399edc19c6
Horacio Herrera <hi@horacioh.com>
2020-09-16 07:05:30 +0200

    husky pre-push command

--- frontend/www/shared/useTransclusion.ts
+++ frontend/www/shared/useTransclusion.ts
@@ -16,9 +16,21 @@ import {
   updateDraftWithRequest,
 } from 'shared/mintterClient'
 
+export interface CreateTransclusionRequest {
+  source: string // source publication version
+  destination?: string // destination draft version (undefined if needs to create a new one)
+  block: SlateBlock // publication block that will be transclude
+  destinationPath?: number[] // destination path in which will be transclude to (draft destination path)
+}
+
 export function useTransclusion({editor}) {
   const createTransclusion = useCallback(
-    async ({source, destination, destinationPath, block}) => {
+    async ({
+      source,
+      destination,
+      destinationPath,
+      block,
+    }: CreateTransclusionRequest): Promise<string> => {
       let draft: Document
       let transclusionId: string = `${destination}/${block.id}`
       if (destination) {
@@ -143,3 +155,58 @@ function createBlockRef(sourceRef: BlockRef.AsObject) {
 
   return makeProto(new BlockRef(), blockRef)
 }
+
+// const n = await createDraft()
+// const newDraft = n.toObject()
+// const transclusionId = `${version}/${block.id}`
+// const req = new UpdateDraftRequest()
+// const map: Map<string, Block> = req.getBlocksMap()
+// const emptyBlockId = uuid()
+// const emptyBlock = {
+//   type: ELEMENT_BLOCK,
+//   id: emptyBlockId,
+//   children: [
+//     {
+//       type: ELEMENT_PARAGRAPH,
+//       children: [
+//         {
+//           text: '',
+//         },
+//       ],
+//     },
+//   ],
+// }
+// map.set(emptyBlockId, toBlock(emptyBlock))
+// const update = toDocument({
+//   document: {
+//     id: newDraft.id,
+//     author: newDraft.author,
+//     version: newDraft.version,
+//   },
+//   state: {
+//     title: '',
+//     subtitle: '',
+//     blocks: [
+//       {
+//         type: ELEMENT_BLOCK_LIST,
+//         id: uuid(),
+//         listType: BlockRefList.Style.NONE,
+//         children: [
+//           {
+//             type: ELEMENT_TRANSCLUSION,
+//             id: transclusionId,
+//             children: block.children,
+//           },
+//           {
+//             ...emptyBlock,
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// })
+// req.setDocument(update)
+// await updateDraftWithRequest(req)
+// push({
+//   pathname: `/editor/${newDraft.version}`,
+// })
