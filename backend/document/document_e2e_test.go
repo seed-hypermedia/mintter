6321e115efb695b5a49141c87ba9752d042f03cf
Alexandr Burdiyan <burdiyan@gmail.com>
2020-09-17 22:45:16 +0200

    fix(backend/document): fix documents with only reused blocks

--- backend/document/document_e2e_test.go
+++ backend/document/document_e2e_test.go
@@ -81,6 +81,77 @@ func TestTransclusionEndToEnd(t *testing.T) {
 	testutil.ProtoEqual(t, wantRootList, draftDoc.Document.BlockRefList, "ref list doesn't match")
 }
 
+func TestTransclusionEmptyDraft(t *testing.T) {
+	srv, _, ctx := makeServer(t, "alice")
+
+	draft1, err := srv.CreateDraft(ctx, &v2.CreateDraftRequest{})
+	require.NoError(t, err, "must create draft 1")
+	updateReq1 := testDoc1(draft1)
+	updateResp1, err := srv.UpdateDraft(ctx, updateReq1)
+	require.NoError(t, err, "must update draft 1")
+	require.NotNil(t, updateResp1)
+	pub1, err := srv.PublishDraft(ctx, &v2.PublishDraftRequest{
+		Version: draft1.Version,
+	})
+	require.NoError(t, err, "must publish draft 1")
+	require.NotNil(t, pub1)
+
+	draft2, err := srv.CreateDraft(ctx, &v2.CreateDraftRequest{})
+	require.NoError(t, err, "must create draft 2")
+	draft2.Title = "Document with only transcluded blocks"
+	draft2.BlockRefList = &v2.BlockRefList{
+		Refs: []*v2.BlockRef{
+			{
+				Ref: pub1.Version + "/block-list-parent",
+				BlockRefList: &v2.BlockRefList{
+					Style: v2.BlockRefList_BULLET,
+					Refs: []*v2.BlockRef{
+						{Ref: pub1.Version + "/block-list-child-1"},
+						{Ref: pub1.Version + "/block-list-child-2"},
+					},
+				},
+			},
+		},
+	}
+	updateReq2 := &v2.UpdateDraftRequest{Document: draft2}
+
+	updateResp2, err := srv.UpdateDraft(ctx, updateReq2)
+	require.NoError(t, err, "must update draft 2")
+	require.NotNil(t, updateResp2)
+
+	pubDoc, err := srv.GetDocument(ctx, &v2.GetDocumentRequest{
+		Version: pub1.Version,
+	})
+	require.NoError(t, err, "must get published doc 1")
+
+	draftDoc, err := srv.GetDocument(ctx, &v2.GetDocumentRequest{
+		Id: draft2.Id,
+	})
+	require.NoError(t, err, "must get draft 2")
+
+	for _, reusedID := range []string{"block-list-parent", "block-list-child-1", "block-list-child-2"} {
+		want := pubDoc.Blocks[reusedID]
+		got := draftDoc.Blocks[pub1.Version+"/"+reusedID]
+		testutil.ProtoEqual(t, want, got, "block %s doesn't match", reusedID)
+	}
+
+	wantRootList := &v2.BlockRefList{
+		Refs: []*v2.BlockRef{
+			{
+				Ref: pub1.Version + "/" + "block-list-parent",
+				BlockRefList: &v2.BlockRefList{
+					Style: v2.BlockRefList_BULLET,
+					Refs: []*v2.BlockRef{
+						{Ref: pub1.Version + "/" + "block-list-child-1"},
+						{Ref: pub1.Version + "/" + "block-list-child-2"},
+					},
+				},
+			},
+		},
+	}
+	testutil.ProtoEqual(t, wantRootList, draftDoc.Document.BlockRefList, "ref list doesn't match")
+}
+
 func testDoc2(d *v2.Document) *v2.UpdateDraftRequest {
 	d.Title = "Second document"
 	d.Subtitle = "This is the first Mintter document that will reuse a block."
