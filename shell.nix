455a57ac1479328d8dfe5d6fd46897200bbe6213
Alexandr Burdiyan <burdiyan@gmail.com>
2020-09-23 16:32:42 +0200

    build: Fight VS Code Go extension to use correct Go version from Nix Shell

--- shell.nix
+++ shell.nix
@@ -6,6 +6,8 @@ let
         doCheck = false;
     };
     protoc-gen-grpc-web = pkgs.callPackage ./nix/protoc-gen-grpc-web {};
+    # When updating Go version remember to change .vscode/settings.json with the new
+    # GOROOT. Otherwise the extension will pick up globally installed Go and will mangle the shell PATH.
     go = pkgs.go_1_14;
     buildGoModule = (pkgs.buildGoModule.override { go = go; });
     go-protobuf = pkgs.callPackage ./nix/go-protobuf { buildGoModule = buildGoModule; };
