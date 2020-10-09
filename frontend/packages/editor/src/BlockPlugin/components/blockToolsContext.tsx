1474323cacd4713585cb2d7aa94e4480bd50ceb7
Horacio Herrera <hi@horacioh.com>
2020-10-09 20:02:10 +0200

    fix build

--- frontend/packages/editor/src/BlockPlugin/components/blockToolsContext.tsx
+++ frontend/packages/editor/src/BlockPlugin/components/blockToolsContext.tsx
@@ -1,4 +1,4 @@
-import React, {createContext, useState, useMemo, useContext} from 'react'
+import React, {createContext, useState, useContext} from 'react'
 
 export const BlockToolsContext = createContext<{
   id: any
--- frontend/packages/editor/src/useEditor.ts
+++ frontend/packages/editor/src/useEditor.ts
@@ -8,47 +8,26 @@ import {withReact} from 'slate-react'
 import {
   withAutoformat,
   withDeserializeHTML,
-  withLink,
-  withToggleType,
-  // withDeserializeMd,
   pipe,
   withInlineVoid,
   withTransforms,
-  withImageUpload,
 } from '@udecode/slate-plugins'
-// import {nodeTypes} from './nodeTypes'
 import {withHistory} from 'slate-history'
 import {autoformatRules} from './autoformatRules'
 import {withMintter} from './MintterPlugin'
 import {withTransclusion} from './TransclusionPlugin'
 
-// need this object because the plugin required it, I made an issue in the plugin's repo
-// const resetOptions = {
-//   types: [
-//     options.blockquote.type,
-//     ELEMENT_CODE_BLOCK,
-//     options.h1.type,
-//     options.h2.type,
-//     options.h3.type,
-//     options.img.type,
-//   ],
-//   defaultType: options.p.type,
-// }
-
 export function useEditor(plugins: any[], options): Editor {
   const withPlugins = [
     withReact,
     withHistory,
-    withLink(),
     withDeserializeHTML({plugins}),
-    withToggleType({defaultType: options.p.type}),
     withAutoformat({
       rules: autoformatRules,
     }),
     withTransforms(),
     // withDeserializeMd(plugins),
     withInlineVoid({plugins}),
-    withImageUpload(options),
     withTransclusion(options),
     withMintter(options),
   ] as const
