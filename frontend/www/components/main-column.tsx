aa6f3cc86a77be01ca8d816a1323419bf3d00713
Horacio Herrera <hi@horacioh.com>
2020-11-24 18:20:24 +0100

    publication screen responsive v1 (#38)
    * text-body + remove some logs
    
    * responsive publication. backlinks visible
    
    Former-commit-id: 32d0bf2be3c4197e0d7488947d7ba5f9a9ee9e43

--- frontend/www/components/main-column.tsx
+++ frontend/www/components/main-column.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 // import {css} from 'emotion'
 export function MainColumn({children, className = ''}) {
   return (
-    <div className={`w-full mb-12 pt-4 mx-4 md:mx-0 ${className}`}>
-      {children}
-    </div>
+    <div className={`mb-12 pt-4 px-4 box-content ${className}`}>{children}</div>
   )
 }
