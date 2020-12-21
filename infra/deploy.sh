44398757c859405e8c550b837a62c3f1dfa6f7b4
Alexandr Burdiyan <burdiyan@gmail.com>
2020-12-21 15:58:32 +0100

    build: change deployment scripts
    Former-commit-id: 492ea5348e2da4126134bf8c54cc429a0feff297

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -7,11 +7,10 @@ set -e
 BIN="out/linux_amd64/mintterd"
 
 [ -z "$BIN" ] && echo "Argument with path to the binary is expected!" && exit 1
-
-ALICE_ADDR="root@159.89.8.72"
+[ -z "$DEPLOY_ADDR" ] && echo "$DEPLOY_ADDR must be set!" && exit 1
 
 echo "Transferring the binary..."
-scp $BIN $ALICE_ADDR:/usr/local/bin/mintterd.new
+scp $BIN "root@$DEPLOY_ADDR":/usr/local/bin/mintterd.new
 
 echo "Restarting the systemd service..."
-ssh $ALICE_ADDR "mv /usr/local/bin/mintterd.new /usr/local/bin/mintterd && systemctl restart mintterd.service"
+ssh "root@$DEPLOY_ADDR" "mv /usr/local/bin/mintterd.new /usr/local/bin/mintterd && systemctl restart mintterd.service"
