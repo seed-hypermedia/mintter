c7d501a3d02d1c4211b62740c468f7c488041b93
Horacio Herrera <hi@horacioh.com>
2021-01-11 17:49:02 +0100

    fix(editor): paste block duplication failed again
    Former-commit-id: f1c2932f6100b8bb6f41689f2bac0afa92970928

--- frontend/www/shared/use-editor.ts
+++ frontend/www/shared/use-editor.ts
@@ -11,10 +11,8 @@ import {
   // withDeserializeHTML,
   pipe,
   withInlineVoid,
-  // withDeserializeHTML,
-  WithDeserializeHTMLOptions,
-  deserializeHTMLToDocumentFragment,
-  SlateDocumentDescendant,
+  // WithDeserializeHTMLOptions,
+  // deserializeHTMLToDocumentFragment,
 } from '@udecode/slate-plugins'
 import {withHistory} from 'slate-history'
 import {autoformatRules} from '../editor/autoformat-rules'
@@ -29,62 +27,10 @@ export function useEditor(plugins: any[], options): Editor {
       rules: autoformatRules,
     }),
     // withDeserializeMd(plugins),
-    withDeserializeHTML({plugins}),
     withInlineVoid({plugins}),
     withTransclusion(options),
-    withMintter(options),
+    withMintter({plugins, options}),
   ] as const
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   return React.useMemo(() => pipe(createEditor(), ...withPlugins), [])
-}
-
-export const withDeserializeHTML = ({
-  plugins = [],
-}: WithDeserializeHTMLOptions = {}) => <T extends ReactEditor>(editor: T) => {
-  const {insertData} = editor
-
-  const inlineTypes = plugins.reduce((arr: string[], plugin) => {
-    const types = plugin.inlineTypes || []
-    return arr.concat(types)
-  }, [])
-
-  editor.insertData = (data: DataTransfer) => {
-    const html = data.getData('text/html')
-
-    if (html) {
-      const {body} = new DOMParser().parseFromString(html, 'text/html')
-      const prefragment = deserializeHTMLToDocumentFragment({
-        plugins,
-        element: body,
-      })
-      const fragment = prefragment.map(orphanTextNodesToBlock())
-      const firstNodeType = fragment[0].type as string | undefined
-
-      // replace the selected node type by the first block type
-      if (firstNodeType && !inlineTypes.includes(firstNodeType)) {
-        Transforms.setNodes(editor, {type: fragment[0].type})
-      }
-      Transforms.insertFragment(editor, fragment)
-      // Editor.normalize(editor, {force: true})
-      return
-    }
-
-    insertData(data)
-  }
-
-  return editor
-}
-
-export const orphanTextNodesToBlock = (defaultType = 'p') => (
-  node: SlateDocumentDescendant,
-) => {
-  if (node.type === undefined) {
-    return {
-      type: defaultType,
-      children: [node],
-    }
-  }
-
-  return node
 }
