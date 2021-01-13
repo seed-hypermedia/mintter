cb6bc2ce53045f5d8a3e0b703ae9ce332e8a7d8f
Horacio Herrera <hi@horacioh.com>
2021-01-13 11:10:21 +0100

    fix(editor): transclusions as it was
    I removed all the code I was adding to controls some things on a transclusion, which will not be needed with the new api
    
    
    Former-commit-id: 1bda7ea8a07e2b8b21859c735634d2f3f18f1480

--- frontend/www/editor/transclusion-plugin/with-transclusion.ts
+++ frontend/www/editor/transclusion-plugin/with-transclusion.ts
@@ -1,9 +1,5 @@
 import {ReactEditor} from 'slate-react'
-import {
-  Editor,
-  Transforms,
-  // Path
-} from 'slate'
+import {Editor, Transforms} from 'slate'
 import {id} from '../id'
 // import {ELEMENT_PARAGRAPH} from '../elements/defaults'
 import {ELEMENT_TRANSCLUSION} from './defaults'
@@ -13,57 +9,43 @@ export const withTransclusion = options => <T extends ReactEditor>(
   editor: T,
 ) => {
   const e = editor as T & ReactEditor
-  const {deleteBackward, deleteFragment} = e
+  const {deleteBackward} = e
 
   e.deleteBackward = unit => {
     const {selection} = editor
 
     if (selection) {
-      const [parentNode, parentPath] = Editor.parent(editor, selection)
-      // console.log('=== TRANSCLUSION -> SELECTION PARENT', {
-      //   unit,
-      //   selection,
-      //   parentNode,
-      //   parentPath,
-      // })
-      if (parentNode.type === ELEMENT_READ_ONLY) {
-        const [blockNode, blockPath] = Editor.parent(editor, parentPath)
-        // console.log('=== TRANSCLUSION -> READ ONLY PARENT', {
-        //   blockNode,
-        //   blockPath,
-        // })
-        if (blockNode.type === ELEMENT_TRANSCLUSION) {
-          const [blockListNode] = Editor.parent(editor, blockPath)
+      const parent = Editor.parent(editor, selection)
+      if (parent) {
+        const [parentNode, parentPath] = parent
+        if (parentNode.type === ELEMENT_READ_ONLY) {
+          const [blockNode, blockPath] = Editor.parent(editor, parentPath)
+          if (blockNode.type === ELEMENT_TRANSCLUSION) {
+            const [blockListNode] = Editor.parent(editor, blockPath)
 
-          Editor.withoutNormalizing(editor, () => {
-            Transforms.delete(editor, {at: blockPath})
-            if (blockListNode.children.length === 0) {
-              Transforms.insertNodes(
-                editor,
-                {
-                  type: options.block.type,
-                  id: id(),
-                  children: [{type: options.p.type, children: [{text: ''}]}],
-                },
-                {at: blockPath},
-              )
-              Transforms.select(editor, blockPath)
-            }
-          })
+            Editor.withoutNormalizing(editor, () => {
+              Transforms.delete(editor, {at: blockPath})
+              if (blockListNode.children.length === 0) {
+                Transforms.insertNodes(
+                  editor,
+                  {
+                    type: options.block.type,
+                    id: id(),
+                    children: [{type: options.p.type, children: [{text: ''}]}],
+                  },
+                  {at: blockPath},
+                )
+                Transforms.select(editor, blockPath)
+              }
+            })
 
-          return
+            return
+          }
         }
       }
     }
 
     deleteBackward(unit)
-  }
-
-  e.deleteFragment = () => {
-    const {selection} = e
-    console.log('withTransclusion => deleteFragment', selection)
-
-    deleteFragment()
   }
 
   return e
