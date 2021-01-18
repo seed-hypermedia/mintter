6bd3e8e7c21d254ab6c0bfa4281b3a51f61a9714
Alexandr Burdiyan <burdiyan@gmail.com>
2021-01-18 22:29:59 +0100

    test(backend/p2p): add transitive fetch test

--- backend/p2p/p2p_test.go
+++ backend/p2p/p2p_test.go
@@ -9,9 +9,38 @@ import (
 	"mintter/backend/store"
 	"mintter/backend/testutil"
 
+	blocks "github.com/ipfs/go-block-format"
+	"github.com/ipfs/go-cid"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/multiformats/go-multihash"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 )
+
+func TestTransitiveFetch(t *testing.T) {
+	alice := makeTestNode(t, "alice")
+	bob := makeTestNode(t, "bob")
+	carol := makeTestNode(t, "carol")
+	ctx := context.Background()
+
+	connectLibp2p(t, ctx, alice, bob)
+
+	blkalice := newBlock(t, []byte("This is Alice!"))
+	require.NoError(t, alice.IPFS().BlockStore().Put(blkalice))
+
+	blkbob, err := bob.IPFS().BlockStore().Get(blkalice.Cid())
+	require.NoError(t, err, "bob must be able to fetch block from alice")
+	require.NotNil(t, blkbob)
+
+	connectLibp2p(t, ctx, bob, carol)
+
+	// Make sure Carol is not connected to Alice directly.
+	require.NoError(t, carol.Host().Network().ClosePeer(alice.Host().ID()))
+
+	blkcarol, err := carol.IPFS().BlockStore().Get(blkalice.Cid())
+	require.NoError(t, err, "carol must be able to fetch alice's block via bob")
+	require.NotNil(t, blkcarol)
+}
 
 func makeTestNode(t *testing.T, name string) *p2p.Node {
 	t.Helper()
@@ -41,6 +70,33 @@ func makeTestNode(t *testing.T, name string) *p2p.Node {
 	return n
 }
 
+func newBlock(t *testing.T, data []byte) blocks.Block {
+	mh, err := multihash.Sum(data, multihash.SHA2_256, -1)
+	require.NoError(t, err)
+
+	blk, err := blocks.NewBlockWithCid(data, cid.NewCidV1(cid.Raw, mh))
+	require.NoError(t, err)
+	return blk
+}
+
+func connectLibp2p(t *testing.T, ctx context.Context, p1, p2 *p2p.Node) {
+	addrs, err := p1.Addrs()
+	require.NoError(t, err)
+
+	infos, err := peer.AddrInfosFromP2pAddrs(addrs...)
+	require.NoError(t, err)
+
+	var done bool
+	for _, i := range infos {
+		if err := p2.Host().Connect(ctx, i); err == nil {
+			done = true
+			break
+		}
+	}
+	if !done {
+		t.Fatal("failed to connect peers")
+	}
+}
 func connectPeers(t *testing.T, ctx context.Context, p1, p2 *p2p.Node) {
 	t.Helper()
 
