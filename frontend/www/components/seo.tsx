62f28dcf35b6644878b2b6775ce8338b77465611
Horacio Herrera <hi@horacioh.com>
2021-03-17 12:59:35 +0100

    hotfix: seo component metatags

--- frontend/www/components/seo.tsx
+++ frontend/www/components/seo.tsx
@@ -46,7 +46,7 @@ const dataTags: {[name: string]: SiteTags} = {
 }
 
 export default function Seo({children, ...props}: SeoProps) {
-  const meta = dataTags[window.location.hostname]
+  const meta = dataTags[window.location.hostname] || dataTags.localhost
   const {
     title,
     description = meta.description,
