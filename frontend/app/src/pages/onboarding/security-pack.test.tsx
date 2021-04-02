3be473e088037853bba363b0903813f642ac4c57
Horacio Herrera <hi@horacioh.com>
2021-04-02 19:08:59 +0200

    use esm-bundle/chai

--- frontend/app/src/pages/onboarding/security-pack.test.tsx
+++ frontend/app/src/pages/onboarding/security-pack.test.tsx
@@ -1,13 +1,10 @@
-import * as React from 'react';
 import { render, screen } from 'test/app-test-utils';
-import { expect } from 'chai';
+import { expect } from '@esm-bundle/chai';
 import { SecurityPack } from './security-pack';
-
+import clientMock from '@mintter/client';
 describe('Onboarding screens', () => {
   it('<SecurityPack />', async () => {
     await render(<SecurityPack />);
-
-    screen.debug();
     // expect(screen.getByText(/Welcome to Mintter/i));
     // expect(screen.getByText(/Start/i));
   });
--- frontend/app/src/pages/onboarding/welcome.test.tsx
+++ frontend/app/src/pages/onboarding/welcome.test.tsx
@@ -1,6 +1,5 @@
-import * as React from 'react';
 import { render, screen } from 'test/app-test-utils';
-import { expect } from 'chai';
+import { expect } from '@esm-bundle/chai';
 import { Welcome } from './welcome';
 
 describe('Onboarding screens', () => {
