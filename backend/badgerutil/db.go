b178b5916a07bedf011615a315fa652f7e709b4c
Alexandr Burdiyan <burdiyan@gmail.com>
2021-05-03 17:47:25 +0200

    WIP
    Former-commit-id: b95071cd26973491ac38f09b89c9b00f0fb2af36

--- backend/badgerutil/db.go
+++ backend/badgerutil/db.go
@@ -10,12 +10,12 @@ import (
 type DB struct {
 	*badger.DB
 	seq *badger.Sequence
-	ns  []byte
+	ns  string
 }
 
 // NewDB creates a new DB instance.
-func NewDB(b *badger.DB, namespace []byte) (*DB, error) {
-	k, _ := makeKeyBuf(namespace, PrefixInternal, KeyTypeData, lastUIDPred, 0)
+func NewDB(b *badger.DB, namespace string) (*DB, error) {
+	k, _ := makeKey(namespace, PrefixInternal, KeyTypeData, "last-uid", 0)
 
 	// We want our uid sequence to start from 1, so we do all this crazyness
 	// to detect if we need to waist the 0 sequence.
@@ -54,85 +54,8 @@ func NewDB(b *badger.DB, namespace []byte) (*DB, error) {
 	}, nil
 }
 
-// UIDReadOnly tries to read the uid for a given kind with given id. Fails if not found.
-func (db *DB) UIDReadOnly(txn *badger.Txn, kind, xid []byte) (uint64, error) {
-	k := uidKey(db.ns, kind, xid)
-	return GetUint64(txn, k)
-}
-
-// UID reads or creates a new uid for something of a given kind with the given id.
-func (db *DB) UID(txn *badger.Txn, kind, xid []byte) (uint64, error) {
-	k := uidKey(db.ns, kind, xid)
-
-	uid, err := GetUint64(txn, k)
-	if err == nil {
-		return uid, nil
-	}
-
-	if err != badger.ErrKeyNotFound {
-		return 0, err
-	}
-
-	uid, err = db.seq.Next()
-	if err != nil {
-		return 0, err
-	}
-
-	if err := SetUint64(txn, k, uid); err != nil {
-		return 0, fmt.Errorf("failed to store allocated uid: %w", err)
-	}
-
-	return uid, nil
-}
-
-// SetData sets the value for the predicate.
-func (db *DB) SetData(txn *badger.Txn, predicate []byte, uid uint64, v []byte) error {
-	k := dataKey(db.ns, predicate, uid)
-	return txn.Set(k, v)
-}
-
-// GetData gets the value for the predicate. Make sure to copy the returned value
-// bytes if you need to use it outside the transaction.
-func (db *DB) GetData(txn *badger.Txn, predicate []byte, uid uint64, codec func([]byte) error) error {
-	k := dataKey(db.ns, predicate, uid)
-	item, err := txn.Get(k)
-	if err != nil {
-		return err
-	}
-
-	return item.Value(codec)
-}
-
-// ScanData creates a new iterator for a given predicate data.
-func (db *DB) ScanData(txn *badger.Txn, predicate []byte, opts ...ScanOption) *badger.Iterator {
-	itopts := badger.DefaultIteratorOptions
-	for _, o := range opts {
-		o(&itopts)
-	}
-	itopts.Prefix = dataPrefix(db.ns, predicate)
-
-	return txn.NewIterator(itopts)
-}
-
 // Close the underlying resources of the database.
 // Users must close Badger instance explicitly elsewhere.
 func (db *DB) Close() error {
 	return db.seq.Release()
-}
-
-// ScanOptions is a functional option type for badger iterator.
-type ScanOption func(*badger.IteratorOptions)
-
-// WithScanPrefetchValues sets prefetch values option for badger iterator.
-func WithScanPrefetchValues(v bool) ScanOption {
-	return func(opts *badger.IteratorOptions) {
-		opts.PrefetchValues = v
-	}
-}
-
-// WithScanPrefetchSize sets prefetch values option for badger iterator.
-func WithScanPrefetchSize(v int) ScanOption {
-	return func(opts *badger.IteratorOptions) {
-		opts.PrefetchSize = v
-	}
 }
