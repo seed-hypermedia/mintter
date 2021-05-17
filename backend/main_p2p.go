2df1c268c8d4ac45affb7840df3a2a63d888990e
Alexandr Burdiyan <burdiyan@gmail.com>
2021-05-17 16:13:49 +0200

    feat(backend): assemble ipfs components separately
    Former-commit-id: 4026d644f9e86a05144fc5244ac7c37788059c56

--- backend/main_p2p.go
+++ backend/main_p2p.go
@@ -4,13 +4,17 @@ import (
 	"context"
 	"path/filepath"
 
-	"github.com/ipfs/go-datastore"
-	"github.com/libp2p/go-libp2p"
-	"go.uber.org/fx"
-
 	"mintter/backend/badger3ds"
 	"mintter/backend/config"
 	"mintter/backend/ipfsutil"
+
+	"github.com/ipfs/go-blockservice"
+	"github.com/ipfs/go-datastore"
+	blockstore "github.com/ipfs/go-ipfs-blockstore"
+	provider "github.com/ipfs/go-ipfs-provider"
+	"github.com/libp2p/go-libp2p"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
 )
 
 var moduleP2P = fx.Options(
@@ -19,6 +23,11 @@ var moduleP2P = fx.Options(
 		ipfsutil.DefaultBootstrapPeers,
 		provideDatastore,
 		ipfsutil.NewBlockstore,
+		provideBitswap,
+		provideReprovider,
+		provideBlockService,
+		provideIPFS,
+		provideP2P,
 	),
 )
 
@@ -41,23 +50,13 @@ func provideDatastore(lc fx.Lifecycle, cfg config.Config) (datastore.Batching, e
 	return ds, nil
 }
 
-type libp2pIn struct {
-	fx.In
-
-	Config    config.P2P
-	Datastore datastore.Batching
-	Repo      *repo
-
-	// These can be provided by ipfsutil.DHTModule.
-	Bootstrappers ipfsutil.Bootstrappers
-}
-
-func provideLibp2p(lc fx.Lifecycle, in libp2pIn) (*ipfsutil.LibP2PNode, error) {
+// provideLibp2p assembles libp2p node ready to use. Listening must be started elsewhere.
+func provideLibp2p(lc fx.Lifecycle, cfg config.P2P, ds datastore.Batching, r *repo, boot ipfsutil.Bootstrappers) (*ipfsutil.LibP2PNode, error) {
 	opts := []libp2p.Option{
 		libp2p.UserAgent(userAgent),
 	}
 
-	if !in.Config.NoRelay {
+	if !cfg.NoRelay {
 		opts = append(opts,
 			libp2p.EnableRelay(), // TODO: enable OptHop for public nodes.
 			libp2p.EnableAutoRelay(),
@@ -65,29 +64,110 @@ func provideLibp2p(lc fx.Lifecycle, in libp2pIn) (*ipfsutil.LibP2PNode, error) {
 		)
 	}
 
-	if !in.Config.NoTLS {
+	if !cfg.NoTLS {
 		opts = append(opts, libp2p.DefaultSecurity)
 	}
 
-	node, err := ipfsutil.NewLibP2PNode(in.Repo.Device().priv, in.Datastore, in.Bootstrappers, opts...)
+	node, err := ipfsutil.NewLibP2PNode(r.Device().priv, ds, boot, opts...)
 	if err != nil {
 		return nil, err
 	}
 
 	lc.Append(fx.Hook{
-		OnStart: func(context.Context) error {
-			mas, err := ipfsutil.ParseMultiaddrs([]string{in.Config.Addr}) // This will eventually be a slice of addrs.
-			if err != nil {
-				return err
-			}
-
-			// We explicitly start listening only when application starts.
-			return node.Host.Network().Listen(mas...)
-		},
 		OnStop: func(context.Context) error {
 			return node.Close()
 		},
 	})
 
 	return node, nil
+}
+
+func provideBitswap(lc fx.Lifecycle, n *ipfsutil.LibP2PNode, bs blockstore.Blockstore) (*ipfsutil.Bitswap, error) {
+	bswap, err := ipfsutil.NewBitswap(n.Host, n.Routing, bs)
+	if err != nil {
+		return nil, err
+	}
+
+	lc.Append(fx.Hook{
+		OnStop: func(context.Context) error {
+			return bswap.Close()
+		},
+	})
+
+	return bswap, nil
+}
+
+func provideReprovider(lc fx.Lifecycle, bs blockstore.Blockstore, ds datastore.Batching, n *ipfsutil.LibP2PNode) (provider.System, error) {
+	sys, err := ipfsutil.NewProviderSystem(bs, ds, n.Routing)
+	if err != nil {
+		return nil, err
+	}
+
+	lc.Append(fx.Hook{
+		OnStart: func(context.Context) error {
+			sys.Run()
+			return nil
+		},
+		OnStop: func(context.Context) error {
+			return sys.Close()
+		},
+	})
+
+	return sys, nil
+}
+
+func provideBlockService(lc fx.Lifecycle, bs blockstore.Blockstore, bswap *ipfsutil.Bitswap) (blockservice.BlockService, error) {
+	blksvc := blockservice.New(bs, bswap)
+
+	// No need to call Close() for block service, because it's only closing the exchange,
+	// which it in fact doesn't own and should not be closing it. Exchange is closed inside the bitswap provide func.
+
+	return blksvc, nil
+}
+
+func provideIPFS(n *ipfsutil.LibP2PNode, bs blockservice.BlockService, bswap *ipfsutil.Bitswap, prov provider.System) *ipfsutil.IPFS {
+	return &ipfsutil.IPFS{
+		Host:           n.Host,
+		Routing:        n.Routing,
+		Provider:       prov,
+		BitswapNetwork: bswap.Net,
+		Exchange:       bswap,
+		BlockService:   bs,
+	}
+}
+
+func provideP2P(lc fx.Lifecycle, log *zap.Logger, cfg config.P2P, node *ipfsutil.IPFS, repo *repo) (*p2pNode, error) {
+	p2p := newP2PNode(node, repo)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	done := make(chan struct{})
+	lc.Append(fx.Hook{
+		OnStart: func(context.Context) error {
+			mas, err := ipfsutil.ParseMultiaddrs([]string{cfg.Addr}) // This will eventually be a slice of addrs.
+			if err != nil {
+				return err
+			}
+
+			go func() {
+				if err := p2p.Start(ctx, mas...); err != nil && err != context.Canceled {
+					panic(err) // This can only happen if there's a bug.
+				}
+				close(done)
+				addrs, err := node.Host.Network().InterfaceListenAddresses()
+				if err != nil {
+					panic(err)
+				}
+				log.Info("P2PNodeStarted", zap.Any("addrs", addrs))
+			}()
+
+			return nil
+		},
+		OnStop: func(context.Context) error {
+			cancel()
+			<-done
+			return nil
+		},
+	})
+
+	return p2p, nil
 }
--- backend/p2p.go
+++ backend/p2p.go
@@ -1,7 +1,13 @@
 package backend
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/libp2p/go-libp2p-core/protocol"
+	"github.com/multiformats/go-multiaddr"
+
+	"mintter/backend/ipfsutil"
 )
 
 // Prototcol values.
@@ -13,3 +19,57 @@ const (
 )
 
 var userAgent = "mintter/" + Version
+
+// p2pNode wraps IPFS node that would be only initialized after account is registered within the node,
+// so all the components must only be accessed after making sure node is ready.
+type p2pNode struct {
+	ipfs  *ipfsutil.IPFS
+	repo  *repo
+	ready chan struct{}
+}
+
+// newP2PNode creates a new Mintter P2P wrapper.
+func newP2PNode(n *ipfsutil.IPFS, r *repo) *p2pNode {
+	p2p := &p2pNode{
+		ipfs:  n,
+		repo:  r,
+		ready: make(chan struct{}),
+	}
+
+	return p2p
+}
+
+// Start will start the services when the underlying repo is ready. It will start
+// listening on the given addresses. The call is blocking and will return error
+// when ctx is canceled before repo is ready.
+func (n *p2pNode) Start(ctx context.Context, addrs ...multiaddr.Multiaddr) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case <-n.repo.Ready():
+		// Start initialization only after repo is ready.
+	}
+
+	if err := n.ipfs.Host.Network().Listen(addrs...); err != nil {
+		return err
+	}
+
+	close(n.ready)
+
+	return nil
+}
+
+// IPFS returns the undelying ipfs instance or fails if it's not ready yet.
+func (n *p2pNode) IPFS() (*ipfsutil.IPFS, error) {
+	select {
+	case <-n.Ready():
+		return n.ipfs, nil
+	default:
+		return nil, fmt.Errorf("p2p node is not ready yet")
+	}
+}
+
+// Ready can be used to check when the node is ready to use.
+func (n *p2pNode) Ready() <-chan struct{} {
+	return n.ready
+}
