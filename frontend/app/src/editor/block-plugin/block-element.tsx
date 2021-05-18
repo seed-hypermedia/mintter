432125792eedf494a15a4586c83b0c59409e19f6
Horacio Herrera <hi@horacioh.com>
2021-05-19 01:28:54 +0200

    wip context menu in block
    Former-commit-id: 963eb07cf247d82549c945a8d25ab855eaed3cd3

--- frontend/app/src/editor/block-plugin/block-element.tsx
+++ frontend/app/src/editor/block-plugin/block-element.tsx
@@ -1,4 +1,5 @@
 import { Text } from '@mintter/ui/text';
+import * as ContextMenu from '@radix-ui/react-context-menu';
 import { ELEMENT_BLOCK } from './create-block-plugin';
 // TODO: fix types
 export function BlockElement({
@@ -10,15 +11,27 @@ export function BlockElement({
 }: any) {
   if (element.type === ELEMENT_BLOCK) {
     return (
+      <ContextMenu.Root>
+        <ContextMenu.Trigger>
       <Text
         alt
         size="4"
         {...attributes}
         className={className}
         data-block-id={element.id}
+        css={{":hover": {
+          background: '$background-neutral-soft'
+        }}}
       >
         {children}
       </Text>
+      </ContextMenu.Trigger>
+      <ContextMenu.Content>
+        <ContextMenu.Item>item one</ContextMenu.Item>
+        <ContextMenu.Item>item two</ContextMenu.Item>
+        <ContextMenu.Item>item three</ContextMenu.Item>
+      </ContextMenu.Content>
+      </ContextMenu.Root>
     );
   }
 }
