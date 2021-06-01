243fd066e487c1171b5e2f569002ce4c9360344b
JonasKruckenberg <iterpre@protonmail.com>
2021-06-01 09:59:40 +0200

    feat: augment slate insertData hook to detect mintter links

---
+++ frontend/app/src/editor/link-plugin/with-mintter-link.ts
@@ -0,0 +1,51 @@
+import { SPEditor, upsertLinkAtSelection, WithOverride, someNode, isUrl } from '@udecode/slate-plugins'
+import { useContext } from 'react'
+import { Editor } from 'slate'
+import { ReactEditor } from 'slate-react'
+import { ELEMENT_LINK, MINTTER_LINK_PREFIX } from './create-link-plugin'
+
+export interface WithMintterLinkOptions {
+  openMintterLinkMenu: (coords: { x: number, y: number }) => void
+}
+
+export function withMintterLink(options: WithMintterLinkOptions): WithOverride<ReactEditor & SPEditor> {
+  return (editor) => {
+    const { insertData, insertText } = editor
+
+    editor.insertData = (data) => {
+      const text = data.getData('text/plain')
+      const link = { url: text, wrap: false }
+
+      if (text) {
+        if (someNode(editor, { match: { type: ELEMENT_LINK } })) {
+          return insertText(text)
+        }
+
+        if (text.includes(MINTTER_LINK_PREFIX)) {
+          console.log('this is a mintter link => ', text, editor.selection)
+
+          const range = editor.selection && ReactEditor.toDOMRange(editor, editor.selection)
+          const rect = range?.getBoundingClientRect()
+
+          if (rect) {
+            options.openMintterLinkMenu({
+              x: rect.right + window.pageXOffset,
+              y: rect.bottom + window.pageYOffset
+            })
+          }
+
+          return upsertLinkAtSelection(editor, link)
+        }
+
+        if (isUrl(text)) {
+          console.log('link inserted at => ', editor.selection)
+          return upsertLinkAtSelection(editor, link)
+        }
+      }
+
+      insertData(data)
+    }
+
+    return editor
+  }
+}
