ac0c8ec7c9cd83f5a4b6ea3a399f5757449257cf
JonasKruckenberg <iterpre@protonmail.com>
2021-06-03 18:12:39 +0200

    improve mapping of doc values to slate values

--- frontend/app/src/to-editor-value.ts
+++ frontend/app/src/to-editor-value.ts
@@ -2,7 +2,8 @@ import type { Document } from '@mintter/api/documents/v1alpha/documents'
 
 import { ELEMENT_BLOCK } from './editor/block-plugin';
 import { ELEMENT_LINK } from './editor/link-plugin';
-import type { SlateBlock } from './editor/types';
+import { ELEMENT_QUOTE } from './editor/quote-plugin';
+import type { EditorTextRun, SlateBlock, SlateLink, SlateQuote } from './editor/types';
 
 export function toEditorValue(entry: Document): Array<SlateBlock> {
   let currentDoc = entry;
@@ -15,18 +16,34 @@ export function toEditorValue(entry: Document): Array<SlateBlock> {
       id: block?.id,
       type: ELEMENT_BLOCK,
       depth: 0,
-      listStyle: block?.childListStyle,
-      children: block?.elements.map(({ textRun, image, quote }) => {
-        if (textRun) {
+      listStyle: block.childListStyle,
+      children: block.elements.map<any>(({ textRun, image, quote }) => {
+        if (image) {
+          return {
+            type: 'image',
+            url: linksMap[image.linkKey].uri,
+            alt_text: image.altText,
+            children: [{ text: '' }]
+          }
+        } else if (textRun) {
           if (textRun.linkKey) {
             return {
               type: ELEMENT_LINK,
-              url: linksMap.get(textRun.linkKey)?.toObject().uri,
-              children: [textRun],
-            };
+              url: linksMap[textRun.linkKey].uri,
+              children: [textRun]
+            }
           } else {
-            return textRun;
+            return textRun
           }
+        } else if (quote) {
+          return {
+            type: ELEMENT_QUOTE,
+            id: '',
+            url: linksMap[quote.linkKey].uri,
+            children: [{ text: '' }]
+          }
+        } else {
+          throw new Error(`unkown element`)
         }
       }),
     };
