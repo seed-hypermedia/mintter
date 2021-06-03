b8a5df3e2afcf5fa90d1fbfbb02ffe9e091ea7e1
JonasKruckenberg <iterpre@protonmail.com>
2021-06-03 18:11:59 +0200

    update test to new testing setup & ts api

--- frontend/app/src/to-editor-value.test.ts
+++ frontend/app/src/to-editor-value.test.ts
@@ -1,8 +1,5 @@
-import { expect } from '@esm-bundle/chai';
-import { makeProto } from '@utils/make-proto';
-import * as documents from '@mintter/api/documents/v1alpha/documents_pb';
+import { Block, InlineElement, ListStyle, TextRun } from '@mintter/api/documents/v1alpha/documents';
 import { buildDocument } from '@utils/generate';
-import { createId } from '@utils/create-id';
 import { toInlineElement, toLink, toTextRun } from './inline-element';
 import { ELEMENT_BLOCK } from './editor/block-plugin';
 import type { SlateBlock } from './editor/types';
@@ -11,26 +8,21 @@ import { ELEMENT_LINK } from './editor/link-plugin';
 
 describe('toEditorValue', () => {
   it('simple document', () => {
-    let block = makeProto<documents.Block, documents.Block.AsObject>(
-      new documents.Block(),
-      {
-        id: 'block-1',
-        parent: '',
-        childListStyle: documents.ListStyle.NONE,
-      },
-    );
+    const block = Block.fromPartial({
+      id: 'block-1',
+      parent: '',
+      childListStyle: ListStyle.NONE,
+      elements: [
+        InlineElement.fromPartial({
+          textRun: TextRun.fromPartial({ text: 'hello world' })
+        })
+      ]
+    })
 
-    block.setElementsList([
-      toInlineElement({
-        textRun: toTextRun({ text: 'hello world' }),
-      }),
-    ]);
-
-    // console.log({ block: JSON.stringify(block.toObject(), null, 2) });
     const doc = buildDocument({
       blocks: [block],
       id: 'doc-id',
-      childrenListStyle: documents.ListStyle.NONE,
+      childrenListStyle: ListStyle.NONE,
     });
 
     let expected: Array<SlateBlock> = [
@@ -38,86 +30,70 @@ describe('toEditorValue', () => {
         type: ELEMENT_BLOCK,
         id: 'block-1',
         depth: 0,
-        listStyle: documents.ListStyle.NONE,
+        listStyle: ListStyle.NONE,
         children: [
-          toTextRun({
+          TextRun.fromPartial({
             text: 'hello world',
-          }).toObject(),
+          }),
         ],
       },
     ];
 
-    // console.log({
-    //   result: JSON.stringify(toEditorValue(doc)[0], null, 2),
-    //   expected: JSON.stringify(expected[0], null, 2),
-    // });
-
-    expect(toEditorValue(doc)).to.deep.equal(expected);
+    expect(toEditorValue(doc)).toEqual(expected);
   });
 
   it('with link', () => {
-    let block = makeProto<documents.Block, documents.Block.AsObject>(
-      new documents.Block(),
-      {
-        id: 'block-1',
-        parent: '',
-        childListStyle: documents.ListStyle.NONE,
-      },
-    );
+    const block = Block.fromPartial({
+      id: 'block-1',
+      parent: '',
+      childListStyle: ListStyle.NONE,
+      elements: [
+        toInlineElement({
+          textRun: toTextRun({ text: 'hello world', linkKey: 'link-key' }),
+        })
+      ]
+    })
 
-    block.setElementsList([
-      toInlineElement({
-        textRun: toTextRun({ text: 'hello world', linkKey: 'link-key' }),
-      }),
-    ]);
-
-    // console.log({ block: JSON.stringify(block.toObject(), null, 2) });
     const doc = buildDocument({
       blocks: [block],
       id: 'doc-id',
-      childrenListStyle: documents.ListStyle.NONE,
+      childrenListStyle: ListStyle.NONE,
+      links: {
+        'link-key': toLink({
+          type: ELEMENT_LINK,
+          id: 'link-key',
+          url: 'https://example.test',
+          children: [
+            toTextRun({
+              text: 'hello world',
+            }),
+          ],
+        })
+      }
     });
-
-    doc.getLinksMap().set(
-      'link-key',
-      toLink({
-        type: ELEMENT_LINK,
-        id: 'link-key',
-        url: 'https://example.test',
-        children: [
-          toTextRun({
-            text: 'hello world',
-          }).toObject(),
-        ],
-      }),
-    );
 
     let expected: Array<SlateBlock> = [
       {
         type: ELEMENT_BLOCK,
         id: 'block-1',
         depth: 0,
-        listStyle: documents.ListStyle.NONE,
+        listStyle: ListStyle.NONE,
         children: [
           {
             type: ELEMENT_LINK,
+            id: 'link-key',
             url: 'https://example.test',
             children: [
               toTextRun({
                 text: 'hello world',
                 linkKey: 'link-key',
-              }).toObject(),
+              }),
             ],
           },
         ],
       },
     ];
 
-    // console.log({
-    //   result: JSON.stringify(toEditorValue(doc)[0], null, 2),
-    //   expected: JSON.stringify(expected[0], null, 2),
-    // });
-
-    expect(toEditorValue(doc)).to.deep.equal(expected);
+    expect(toEditorValue(doc)).toEqual(expected);
   });
 });
