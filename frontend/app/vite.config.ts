fee75157f135f944a8f78e8ec7e6523941b318c1
JonasKruckenberg <iterpre@protonmail.com>
2021-06-05 09:40:42 +0200

    fix build time imports

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import { join } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
