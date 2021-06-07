a821f572a690c03c87d9567bb4efeb431dd820aa
JonasKruckenberg <iterpre@protonmail.com>
2021-06-07 11:56:08 +0200

    add vite-react-jsx & vite-tsconfig-paths
    This allows vite to use the new jsx transform from react 17 and use the same paths resolution typescript uses

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -1,19 +1,13 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-import { join } from 'path'
+import reactJSX from 'vite-react-jsx'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@components': join(process.cwd(), './src/components'),
-      '@pages': join(process.cwd(), './src/pages'),
-      '@utils': join(process.cwd(), './src/utils'),
-      '@mintter/editor': join(process.cwd(), './src/editor'),
-      test: join(process.cwd(), './src/test'),
-    },
-  },
   plugins: [
-    reactRefresh()
+    reactRefresh(),
+    reactJSX(),
+    tsconfigPaths()
   ]
 })
