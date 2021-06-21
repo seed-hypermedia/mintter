bc8940688f7aa361d2b4242cdef95e4853f5042c
JonasKruckenberg <iterpre@protonmail.com>
2021-06-21 16:30:28 +0200

    fix: update types

--- frontend/client/hooks/types.ts
+++ frontend/client/hooks/types.ts
@@ -1,17 +1,6 @@
 import type {UseQueryOptions} from 'react-query'
-import type {grpc} from '@improbable-eng/grpc-web'
+import type { GrpcClient } from '../src/grpc-client'
 
 export interface HookOptions<T> extends UseQueryOptions<T, unknown, T> {
-  rpc?: GrpcWebImpl
-}
-
-interface GrpcWebImplOptions {
-  transport?: grpc.TransportFactory
-  debug?: boolean
-  metadata?: grpc.Metadata
-}
-
-interface GrpcWebImpl {
-  new (host: string, options: GrpcWebImplOptions): this
-  unary<T>(methodDesc: T, _request: any, metadata?: grpc.Metadata): Promise<any>
+  rpc?: GrpcClient
 }
