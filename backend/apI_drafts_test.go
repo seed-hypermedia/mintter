d6216ffb58bf8147c7946dcbd8a75a142774d215
Alexandr Burdiyan <burdiyan@gmail.com>
2021-06-22 19:39:26 +0200

    feat(backend): implement documents.ListDrafts [WIP]

--- backend/apI_drafts_test.go
+++ backend/apI_drafts_test.go
@@ -5,8 +5,10 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
 
 	documents "mintter/api/go/documents/v1alpha"
+	"mintter/backend/testutil"
 )
 
 func TestAPICreateDraft(t *testing.T) {
@@ -21,4 +23,41 @@ func TestAPICreateDraft(t *testing.T) {
 	require.False(t, doc.UpdateTime.AsTime().IsZero())
 	require.False(t, doc.CreateTime.AsTime().IsZero())
 	require.Nil(t, doc.PublishTime)
+}
+
+func TestAPIListDrafts(t *testing.T) {
+	back := makeTestBackend(t, "alice", true)
+	api := newDraftsAPI(back)
+	ctx := context.Background()
+
+	var docs [5]*documents.Document
+
+	g, ctx := errgroup.WithContext(ctx)
+	for i := range docs {
+		i := i
+		g.Go(func() error {
+			doc, err := api.CreateDraft(ctx, &documents.CreateDraftRequest{})
+			if err != nil {
+				return err
+			}
+			docs[i] = doc
+			return nil
+		})
+	}
+	require.NoError(t, g.Wait())
+
+	list, err := api.ListDrafts(ctx, &documents.ListDraftsRequest{})
+	require.NoError(t, err)
+
+	for _, doc := range list.Documents {
+		var ok bool
+		for _, d := range docs {
+			if d.Id == doc.Id {
+				ok = true
+				testutil.ProtoEqual(t, d, doc, "documents don't match")
+				break
+			}
+		}
+		require.True(t, ok, "document was not found "+doc.Id)
+	}
 }
--- backend/api_drafts.go
+++ backend/api_drafts.go
@@ -3,11 +3,15 @@ package backend
 import (
 	"context"
 	"fmt"
+	"time"
 
+	"github.com/ipfs/go-cid"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
 	documents "mintter/api/go/documents/v1alpha"
+	"mintter/backend/badgergraph"
 )
 
 type draftsAPI struct {
@@ -49,4 +53,90 @@ func (srv *draftsAPI) CreateDraft(ctx context.Context, in *documents.CreateDraft
 	}
 
 	return doc, nil
+}
+
+func (srv *draftsAPI) ListDrafts(ctx context.Context, in *documents.ListDraftsRequest) (*documents.ListDraftsResponse, error) {
+	cids, err := srv.back.drafts.ListDrafts()
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &documents.ListDraftsResponse{
+		Documents: make([]*documents.Document, len(cids)),
+	}
+
+	// TODO: as we're reading all records, we can take advantage of the sorted nature of Badger.
+	// But we need to test if it's actually better.
+	// The idea is to use a single transaction with multiple iterators that would iterate concurrently
+	// and populate the corresponding field each. Similar to a SELECT statement in a columnar database.
+	// NOTICE that it would only work if all the nodes have all the selected predicates, otherwise the ordering
+	// would be screwed up.
+	//
+	// Intuitively this should be better because:
+	// 1. We'd do sequential reads instead of random ones.
+	// 2. We'd launch a lot less goroutines: one per predicate instead of one per node.
+
+	var g errgroup.Group
+	for i, c := range cids {
+		i, c := i, c
+		g.Go(func() error {
+			return srv.back.db.db.View(func(txn *badgergraph.Txn) error {
+				duid, err := txn.UID(typeDocument, c.Hash())
+				if err != nil {
+					return err
+				}
+
+				title, err := txn.GetProperty(duid, pDocumentTitle.FullName())
+				if err != nil {
+					return err
+				}
+
+				auid, err := txn.GetProperty(duid, pDocumentAuthor.FullName())
+				if err != nil {
+					return err
+				}
+
+				ahash, err := txn.XID(typeAccount, auid.(uint64))
+				if err != nil {
+					return err
+				}
+
+				createTime, err := txn.GetProperty(duid, pDocumentCreateTime.FullName())
+				if err != nil {
+					return err
+				}
+
+				createTimeParsed, err := time.Parse(time.RFC3339, createTime.(string))
+				if err != nil {
+					return err
+				}
+
+				updateTime, err := txn.GetProperty(duid, pDocumentUpdateTime.FullName())
+				if err != nil {
+					return err
+				}
+
+				updateTimeParsed, err := time.Parse(time.RFC3339, updateTime.(string))
+				if err != nil {
+					return err
+				}
+
+				resp.Documents[i] = &documents.Document{
+					Id:         c.String(),
+					Author:     cid.NewCidV1(codecAccountID, ahash).String(),
+					Title:      title.(string),
+					CreateTime: timestamppb.New(createTimeParsed),
+					UpdateTime: timestamppb.New(updateTimeParsed),
+				}
+
+				return nil
+			})
+		})
+	}
+
+	if err := g.Wait(); err != nil {
+		return nil, err
+	}
+
+	return resp, nil
 }
