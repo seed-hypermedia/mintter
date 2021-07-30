3687cedc70b85d0e4ca3994eac326b0d7d5f699f
Jonas Kruckenberg <iterpre@protonmail.com>
2021-07-30 18:19:06 +0300

    feat: add mttast types & utils

---
+++ frontend/mttast/src/types.ts
@@ -0,0 +1,130 @@
+import type {Node as UnistNode, Parent as UnistParent, Literal as UnistLiteral, Data as UnistData} from 'unist'
+
+// Nodes
+
+export interface Literal extends UnistLiteral {
+  value: string
+}
+
+export interface Parent extends UnistParent {
+  children: Array<UnistNode<UnistData>>
+}
+
+export interface Document extends Parent {
+  type: 'document'
+  author?: string
+  schema?: string
+  stylesheet?: string
+  children: FlowContent[]
+}
+
+export interface Group extends Parent {
+  type: 'group'
+  children: Array<FlowContent>
+}
+
+export interface OrderedList extends Parent {
+  type: 'orderedList'
+  children: Array<FlowContent>
+}
+
+export interface UnorderedList extends Parent {
+  type: 'unorderedList'
+  children: Array<FlowContent>
+}
+
+export interface Paragraph extends Parent {
+  type: 'paragraph'
+  children: Array<PhrasingContent>
+}
+
+export interface StaticParagraph extends Parent {
+  type: 'staticParagraph'
+  children: Array<StaticPhrasingContent>
+}
+
+export interface Statement extends Parent {
+  type: 'statement'
+  children: [Content, GroupingContent] | [Content]
+}
+
+export interface Header extends Parent {
+  type: 'header'
+  children: [Content, GroupingContent] | [Content]
+}
+
+export interface Blockquote extends Parent {
+  type: 'blockquote'
+  children: [Content, GroupingContent] | [Content]
+}
+
+export interface Code extends Parent {
+  type: 'code'
+  lang?: string
+  meta?: string
+  children: [Content, GroupingContent] | [Content]
+}
+
+export interface Callout extends Parent {
+  type: 'callout'
+  children: [Content, GroupingContent] | [Content]
+}
+
+export interface Video extends Alternative, Resource {
+  type: 'video'
+}
+
+export interface Image extends Alternative, Resource {
+  type: 'image'
+}
+
+export interface Embed extends Alternative, Resource {
+  type: 'embed'
+}
+
+export interface Link extends Resource {
+  type: 'link'
+}
+
+export interface Text extends Literal {
+  type: 'text'
+  strong?: boolean
+  emphasis?: boolean
+  underline?: boolean
+  strikethrough?: boolean
+  superscript?: boolean
+  subscript?: boolean
+  get text(): string
+  set text(val: string)
+}
+
+// Mixin
+
+export interface Alternative {
+  alt?: string
+}
+
+export interface Resource {
+  url?: string
+  title?: string
+}
+
+// Content Model
+
+export type Content = Paragraph
+
+export type StaticContent = StaticParagraph
+
+export type GroupingContent = Group | OrderedList | UnorderedList
+
+// The statements of a document
+export type FlowContent = Statement | Header | Blockquote
+
+// Content that is embeded from elsewhere
+export type EmbeddedContent = Embed | Video | Image
+
+// the text and markup of a document that's not intended for
+export type StaticPhrasingContent = Text
+
+// the text and markup of a document
+export type PhrasingContent = Link | StaticPhrasingContent | EmbeddedContent
