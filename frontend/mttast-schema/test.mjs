c48f0ba2414d6793723afc419a346d83002a8106
Jonas Kruckenberg <iterpre@protonmail.com>
2021-07-30 18:19:17 +0300

    feat: add mttast schema

---
+++ frontend/mttast-schema/test.mjs
@@ -0,0 +1,17 @@
+import Ajv from 'ajv'
+import { doc, statement, paragraph, text } from '../builder/dist/index.js'
+import fs from 'fs'
+const schema = JSON.parse(fs.readFileSync('./schema.json'),'utf8')
+
+const ajv = new Ajv()
+
+const data = doc([
+    statement(
+        paragraph([text('foobar')])
+    )
+])
+
+const validate = ajv.compile(schema)
+const valid = validate(data)
+console.log(`is valid: ${valid}`);
+// if (!valid) console.log(validate.errors)
