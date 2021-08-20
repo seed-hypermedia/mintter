30dc275a377d1d79d6ed015cf6a5f9fe1f9f03fd
Jonas Kruckenberg <iterpre@protonmail.com>
2021-08-20 13:24:14 +0200

    fix: misc type errors

--- frontend/app/src/utils/get-format-date.ts
+++ frontend/app/src/utils/get-format-date.ts
@@ -8,6 +8,6 @@ type KeyOfType<T, U = Keys<T>> = {[P in keyof T]: T[P] extends U ? P : never}[ke
 
 export type DateKeys = KeyOfType<Document, Date | undefined>
 
-export function getDateFormat(document: Document, key: DateKeys, dateFormat?: string = 'MMMM d, yyyy') {
+export function getDateFormat(document: Document, key: DateKeys, dateFormat: string = 'MMMM d, yyyy') {
   return format(new Date(document[key]), dateFormat)
 }
