6a29e29bed5ad2ed2742e131e470d64fcee780fe
Jonas Kruckenberg <iterpre@protonmail.com>
2021-08-20 13:24:06 +0200

    fix: no deep import of packages

--- frontend/app/src/editor/use-editor-draft.ts
+++ frontend/app/src/editor/use-editor-draft.ts
@@ -5,7 +5,7 @@ import {assign, createMachine} from 'xstate'
 import type {ActionFunction} from 'xstate'
 import isEqual from 'lodash/isequal'
 import {QueryClient, useQueryClient} from 'react-query'
-import {group, paragraph, statement, text} from 'frontend/mttast-builder/dist'
+import {group, paragraph, statement, text} from '@mintter/mttast-builder'
 import {publishDraft} from 'frontend/client/src/drafts'
 
 export type DRAFT_FETCH_EVENT = {
