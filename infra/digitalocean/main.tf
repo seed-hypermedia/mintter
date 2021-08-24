4d3e3a371c26774b505877b761cd003d3495e4b4
Alexandr Burdiyan <burdiyan@gmail.com>
2021-08-24 14:16:18 +0200

    infra: add ethosfera node to terraform config
    Former-commit-id: 202e97088d861ca79da0bb3413712a578c64cf5f

--- infra/digitalocean/main.tf
+++ infra/digitalocean/main.tf
@@ -1,3 +1,19 @@
 resource "digitalocean_project" "main" {
-  name = "Mintter"
+  name        = "Mintter"
+  environment = "Production"
+  purpose     = "Other"
+}
+
+resource "digitalocean_droplet" "ethosfera" {
+  name               = "example"
+  size               = "s-1vcpu-2gb"
+  image              = "centos-7-x64"
+  region             = "fra1"
+  backups            = false
+  monitoring         = true
+  private_networking = true
+  ipv6               = true
+  resize_disk        = true
+  tags               = ["mtt-public"]
+  vpc_uuid           = "9f5682e5-cf83-491c-acdb-0e3eded52fbb"
 }
