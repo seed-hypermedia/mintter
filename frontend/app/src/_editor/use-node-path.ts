a3e40cf065f74de5f875890cbf62e43b7a43467d
Jonas Kruckenberg <iterpre@protonmail.com>
2021-09-01 09:00:45 +0200

    style: remove old editor code

--- frontend/app/src/_editor/use-node-path.ts
+++
@@ -1,8 +0,0 @@
-import {useStoreEditorState} from '@udecode/slate-plugins'
-import type {Path} from 'slate'
-import {ReactEditor} from 'slate-react'
-import type {EditorBlock} from './types'
-
-export function useNodePath(editor: ReactEditor, node: EditorBlock): Path {
-  return ReactEditor.findPath(editor, node)
-}
