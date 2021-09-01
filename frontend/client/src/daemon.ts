eee3a3df0bba6632ed4f1998ded59fc3cc6c2d6e
Horacio Herrera <hi@horacioh.com>
2021-09-01 14:22:38 +0200

    embeds inside embeds
    Former-commit-id: 218b0421f905c28fe61ff4900b4cbb5ba2f42b70

--- frontend/client/src/daemon.ts
+++ frontend/client/src/daemon.ts
@@ -1,6 +1,6 @@
 import {DaemonClientImpl, GetInfoRequest} from '../.generated/daemon/v1alpha/daemon'
 import {createGrpcClient} from './grpc-client'
-import type { GrpcClient } from './grpc-client'
+import type {GrpcClient} from './grpc-client'
 /**
  *
  * @param rpc
@@ -10,7 +10,6 @@ export async function getInfo(rpc?: GrpcClient) {
   rpc ||= createGrpcClient()
 
   const request = GetInfoRequest.fromPartial({})
-  const response = await new DaemonClientImpl(rpc).getInfo()
-  console.log('🚀 ~ file: daemon.ts ~ line 14 ~ getInfo ~ response', response)
+  const response = await new DaemonClientImpl(rpc).getInfo(request)
   return response
 }
