e1d7b503a7f07fa20af4138916b71c2467618d89
Horacio Herrera <hi@horacioh.com>
2021-09-04 01:33:10 +0200

    block quote
    Former-commit-id: 94c29d11aed28ef981fc1fc12610ad5cf2dde9f7

--- frontend/app/src/editor/statement-tools.tsx
+++ frontend/app/src/editor/statement-tools.tsx
@@ -10,7 +10,8 @@ import {useSlateStatic} from 'slate-react'
 import {ReactEditor} from 'slate-react'
 import {Transforms, Editor, Path} from 'slate'
 import type {MTTEditor} from './utils'
-import {heading, statement} from 'frontend/mttast-builder/dist'
+import {blockquote, code, heading, statement} from 'frontend/mttast-builder/dist'
+import {useReadOnly} from 'slate-react'
 
 export const Tools = styled('div', {
   height: '100%',
@@ -38,6 +39,62 @@ export const Dragger = styled('div', {
   },
 })
 
+const items = [
+  {
+    label: 'Statement',
+    iconName: 'Paragraph',
+    onSelect: setToStatement,
+  },
+  {
+    label: 'Heading',
+    iconName: 'Heading',
+    onSelect: setToHeading,
+  },
+  {
+    label: 'Blockquote',
+    iconName: 'MessageBubble',
+    onSelect: setToBlockquote,
+  },
+  {
+    label: 'Code block',
+    iconName: 'AddCircle',
+    onSelect: setCodeblock,
+  },
+]
+
+export const StatementTools = forwardRef(({element}: {element: FlowContent}, ref) => {
+  const editor = useSlateStatic()
+  const isReadOnly = useReadOnly()
+  const path = ReactEditor.findPath(editor, element)
+  return (
+    !isReadOnly && (
+      <Tools contentEditable={false} ref={ref}>
+        <Dropdown.Root>
+          <Dropdown.Trigger as={Slot}>
+            <Dragger data-dragger>
+              <Icon name="AddCircle" size="2" color="muted" />
+            </Dragger>
+          </Dropdown.Trigger>
+          <Dropdown.Content align="start" side="bottom" css={{minWidth: 220}}>
+            <Dropdown.Label>
+              <Text color="muted" size="2" css={{marginHorizontal: '$3', marginVertical: '$2'}}>
+                Turn Statement into:
+              </Text>
+            </Dropdown.Label>
+            {items.map((item) => (
+              <Dropdown.Item key={item.label} onSelect={() => item.onSelect(editor, element, path)}>
+                <Icon size="2" name={item.iconName as any} />
+                {item.label}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Content>
+        </Dropdown.Root>
+        <Marker element={element} />
+      </Tools>
+    )
+  )
+})
+
 function setToStatement(editor: MTTEditor, element: FlowContent, at: Path) {
   Editor.withoutNormalizing(editor, function () {
     const {children, type, ...props} = element
@@ -54,55 +111,18 @@ function setToHeading(editor: MTTEditor, element: FlowContent, at: Path) {
   })
 }
 
-const items = [
-  {
-    label: 'Statement',
-    iconName: 'Paragraph',
-    onSelect: setToStatement,
-  },
-  {
-    label: 'Heading',
-    iconName: 'Heading',
-    onSelect: setToHeading,
-  },
-  {
-    label: 'Blockquote',
-    iconName: 'MessageBubble',
-    onSelect: setToHeading,
-  },
-  {
-    label: 'Code block',
-    iconName: 'AddCircle',
-    onSelect: setToHeading,
-  },
-]
+function setToBlockquote(editor: MTTEditor, element: FlowContent, at: Path) {
+  Editor.withoutNormalizing(editor, function () {
+    const {children, type, ...props} = element
+    Transforms.removeNodes(editor, {at})
+    Transforms.insertNodes(editor, blockquote({...props}, children), {at})
+  })
+}
 
-export const StatementTools = forwardRef(({element}: {element: FlowContent}, ref) => {
-  const editor = useSlateStatic()
-  const path = ReactEditor.findPath(editor, element)
-  return (
-    <Tools contentEditable={false} ref={ref}>
-      <Dropdown.Root>
-        <Dropdown.Trigger as={Slot}>
-          <Dragger data-dragger>
-            <Icon name="AddCircle" size="2" color="muted" />
-          </Dragger>
-        </Dropdown.Trigger>
-        <Dropdown.Content align="start" side="bottom" css={{minWidth: 220}}>
-          <Dropdown.Label>
-            <Text color="muted" size="2" css={{marginHorizontal: '$3', marginVertical: '$2'}}>
-              Turn Statement into:
-            </Text>
-          </Dropdown.Label>
-          {items.map((item) => (
-            <Dropdown.Item key={item.label} onSelect={() => item.onSelect(editor, element, path)}>
-              <Icon size="2" name={item.iconName as any} />
-              {item.label}
-            </Dropdown.Item>
-          ))}
-        </Dropdown.Content>
-      </Dropdown.Root>
-      <Marker element={element} />
-    </Tools>
-  )
-})
+function setCodeblock(editor: MTTEditor, element: FlowContent, at: Path) {
+  Editor.withoutNormalizing(editor, function () {
+    const {children, type, ...props} = element
+    Transforms.removeNodes(editor, {at})
+    Transforms.insertNodes(editor, code({...props}, children), {at})
+  })
+}
