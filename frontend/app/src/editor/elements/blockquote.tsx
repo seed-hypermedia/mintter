f722b59426ae5c0a993ee47248efac50c06033c3
Horacio Herrera <hi@horacioh.com>
2021-09-04 01:33:10 +0200

    block quote

--- frontend/app/src/editor/elements/blockquote.tsx
+++ frontend/app/src/editor/elements/blockquote.tsx
@@ -1,5 +1,6 @@
+import {isBlockquote} from '@mintter/mttast'
 import {styled} from '@mintter/ui/stitches.config'
-import {StatementTools} from '../statement-tools'
+import {StatementTools, Tools} from '../statement-tools'
 import type {EditorPlugin} from '../types'
 import {statementStyle} from './statement'
 
@@ -7,12 +8,12 @@ export const ELEMENT_BLOCKQUOTE = 'blockquote'
 
 // TODO: add dragger + new grid styles
 
-export const BlockQuote = styled('blockquote', statementStyle)
+export const BlockQuote = styled('li', statementStyle)
 
 export const createBlockquotePlugin = (): EditorPlugin => ({
   name: ELEMENT_BLOCKQUOTE,
   renderElement({attributes, children, element}) {
-    if (element.type === ELEMENT_BLOCKQUOTE) {
+    if (isBlockquote(element)) {
       return (
         <BlockQuote data-element-type={element.type} {...attributes}>
           <StatementTools element={element} />
