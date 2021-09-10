fa36a9fdd8de3712c3162ad04d90af35e87fca8a
Jonas Kruckenberg <iterpre@protonmail.com>
2021-09-10 16:10:42 +0200

    feat: toggle color mark through rich text input

--- frontend/app/src/editor/leafs/color.tsx
+++ frontend/app/src/editor/leafs/color.tsx
@@ -1,10 +1,14 @@
+import type {Editor} from 'slate'
 import type {EditorPlugin} from '../types'
+import {toggleMark} from '../utils'
 
 export const MARK_COLOR = 'color'
 
 export function createColorPlugin(): EditorPlugin {
+  let editor: Editor
   return {
     name: MARK_COLOR,
+    configureEditor: (e) => (editor = e),
     renderLeaf({attributes, children, leaf}) {
       if (leaf[MARK_COLOR] && leaf.value) {
         return (
@@ -12,6 +16,12 @@ export function createColorPlugin(): EditorPlugin {
             {children}
           </span>
         )
+      }
+    },
+    onDOMBeforeInput(ev) {
+      if (ev.inputType == 'formatFontColor' && editor.selection) {
+        ev.preventDefault()
+        toggleMark(editor, 'color', ev.data)
       }
     },
   }
