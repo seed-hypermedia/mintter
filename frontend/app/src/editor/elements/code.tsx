f0d9b406ef5a7dcef93109676a88e47d2a978e1c
Jonas Kruckenberg <iterpre@protonmail.com>
2021-09-10 13:39:57 +0200

    perf: dynamically attach the correct highlighter to codeblocks instead of loading languages upfront
    Former-commit-id: bc5438160cc7175bec494df7515f4d764d2abce0

--- frontend/app/src/editor/elements/code.tsx
+++ frontend/app/src/editor/elements/code.tsx
@@ -1,11 +1,12 @@
-import type {IThemeRegistration} from 'shiki'
+import type {Highlighter, IThemeRegistration} from 'shiki'
 import type {EditorPlugin} from '../types'
 import {resetFlowContent} from '../utils'
 import {setCDN, getHighlighter} from 'shiki'
 import {Box} from '@mintter/ui/box'
 import {styled} from '@mintter/ui/stitches.config'
-import {isCode, isText} from '@mintter/mttast'
-import {Range, Editor} from 'slate'
+import {isCode, isParagraph} from '@mintter/mttast'
+import {Editor, Node, Transforms} from 'slate'
+import type {Range} from 'slate'
 import {MARK_EMPHASIS} from '../leafs/emphasis'
 import {MARK_STRONG} from '../leafs/strong'
 import {MARK_UNDERLINE} from '../leafs/underline'
@@ -13,6 +14,8 @@ import {StatementTools} from '../statement-tools'
 import {statementStyle} from './statement'
 
 export const ELEMENT_CODE = 'code'
+
+const HIGHLIGHTER = Symbol('shiki highlighter')
 
 export const Code = styled('pre', statementStyle)
 
@@ -24,15 +27,7 @@ export const createCodePlugin = async (props: CodePluginProps = {}): Promise<Edi
   const {theme = 'github-dark'} = props
 
   setCDN('/shiki/')
-  /*
-   * @todo `getHighlighter() await takes too long?`
-   * @body can we do this after we render the editor? this function is taking long and is making the editor look that it loads slower than it should.
-   *
-   * Any ideas?
-   */
-  const highlighter = await getHighlighter({
-    theme,
-  })
+
   let editor: Editor
 
   return {
@@ -58,10 +53,10 @@ export const createCodePlugin = async (props: CodePluginProps = {}): Promise<Edi
                 backgroundColor: '$background-muted',
                 padding: '$7',
                 borderRadius: '$2',
-                overflow: 'hidden',
+                overflow: 'scroll',
                 position: 'relative',
-                color: highlighter.getForegroundColor(theme as string),
-                caretColor: highlighter.getForegroundColor(theme as string),
+                // color: highlighter.getForegroundColor(theme as string),
+                // caretColor: highlighter.getForegroundColor(theme as string),
                 // backgroundColor: highlighter.getBackgroundColor(theme as string),
               }}
             >
@@ -71,48 +66,47 @@ export const createCodePlugin = async (props: CodePluginProps = {}): Promise<Edi
         )
       }
     },
-    renderLeaf({attributes, children, leaf}) {
-      if (leaf.data?.color) {
-        return (
-          <span style={{color: leaf.data.color as string}} {...attributes}>
-            {children}
-          </span>
-        )
-      }
-    },
     decorate([node, path]) {
       const ranges: Array<Range> = []
 
-      if (isText(node)) {
+      if (isCode(node) && !node.data?.[HIGHLIGHTER] && node.lang) {
+        getHighlighter({theme, langs: [node.lang]}).then((highlighter) => {
+          Transforms.setNodes(editor, {data: {...node.data, [HIGHLIGHTER]: highlighter}}, {at: path})
+        })
+      }
+
+      if (isParagraph(node)) {
         const [code] =
           Editor.above(editor, {
             at: path,
             match: isCode,
           }) || []
 
-        if (!code) return []
+        if (!code || !code.data?.[HIGHLIGHTER]) return []
 
-        const [tokens] = highlighter.codeToThemedTokens(node.value, code.lang)
-        let offset = 0
+        for (const [text, textPath] of Node.texts(node)) {
+          const tokens = (code.data?.[HIGHLIGHTER] as Highlighter)
+            .codeToThemedTokens(text.value, code.lang)
+            .flatMap((l) => l)
 
-        for (const token of tokens) {
-          const range: Range & Record<string, unknown> = {
-            anchor: {path, offset},
-            focus: {path, offset: offset + token.content.length},
-            data: {
+          let offset = 0
+
+          for (const token of tokens) {
+            const range: Range & Record<string, unknown> = {
+              anchor: {path: [...path, ...textPath], offset},
+              focus: {path: [...path, ...textPath], offset: offset + token.content.length},
               color: token.color,
-            },
+            }
+
+            if (token.fontStyle === 1) range[MARK_EMPHASIS] = true
+            if (token.fontStyle === 2) range[MARK_STRONG] = true
+            if (token.fontStyle === 4) range[MARK_UNDERLINE] = true
+
+            ranges.push(range)
+            offset += token.content.length
           }
-
-          if (token.fontStyle === 1) range[MARK_EMPHASIS] = true
-          if (token.fontStyle === 2) range[MARK_STRONG] = true
-          if (token.fontStyle === 4) range[MARK_UNDERLINE] = true
-
-          ranges.push(range)
-          offset += token.content.length
         }
       }
-
       return ranges
     },
   }
