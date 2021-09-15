e531b30b8e8900355ccda6ced2b5edf37d74fa21
Alexandr Burdiyan <burdiyan@gmail.com>
2021-09-15 12:41:30 +0200

    build(nix): update nixpkgs
    Former-commit-id: 3e9309665721a1fc182f8f05acb470ac2b8fe952

--- build/nix/nixpkgs.nix
+++ build/nix/nixpkgs.nix
@@ -2,9 +2,9 @@ import (builtins.fetchGit {
   # Descriptive name to make the store path easier to identify
   name = "nixpkgs-unstable";
   url = https://github.com/nixos/nixpkgs;
-  # Commit hash for nixos-unstable as of 2021-08-19.
+  # Commit hash for nixos-unstable as of 2021-09-15.
   # Run `git ls-remote https://github.com/nixos/nixpkgs refs/heads/nixpkgs-unstable`
   # to get the most recent revision.
   ref = "refs/heads/nixpkgs-unstable";
-  rev = "253aecf69ed7595aaefabde779aa6449195bebb7";
+  rev = "071317d543205ee5f5611d391a37582f9b282240";
 }) {}
