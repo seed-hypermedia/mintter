35ab7023e5d6e630ca72a3682e4e511b1e693c92
Alexandr Burdiyan <burdiyan@gmail.com>
2021-09-15 13:58:46 +0200

    build: bump Go to 1.17

--- shell.nix
+++ shell.nix
@@ -11,7 +11,7 @@ let
             pkgs.coreutils
             pkgs.findutils
             pkgs.protobuf
-            pkgs.go
+            pkgs.go_1_17
             pkgs.terraform
             pkgs.bazelisk
             node
