ca0c15454b3fac4c2b37ef7e6836d21020a72ea3
Alexandr Burdiyan <burdiyan@gmail.com>
2021-09-21 10:27:45 +0200

    build(nix): use proper nix-shell instead of our previous hackery

--- shell.nix
+++ shell.nix
@@ -1,22 +1,25 @@
 let
-    pkgs = import ./build/nix/nixpkgs.nix;
-    protoc-gen-grpc-web = pkgs.callPackage ./build/nix/protoc-gen-grpc-web {};
-    node = pkgs.nodejs-16_x;
-    yarn = (pkgs.yarn.override { nodejs = node; });
+  pkgs = import ./build/nix/nixpkgs.nix;
+  protoc-gen-grpc-web = pkgs.callPackage ./build/nix/protoc-gen-grpc-web {};
+  node = pkgs.nodejs-16_x;
+  yarn = (pkgs.yarn.override { nodejs = node; });
     
-    tools = pkgs.buildEnv rec {
-        name = "mintter-tools";
-        paths = [
-            pkgs.bash
-            pkgs.coreutils
-            pkgs.findutils
-            pkgs.protobuf
-            pkgs.go_1_17
-            pkgs.terraform
-            pkgs.bazelisk
-            node
-            yarn
-        ];
-    };
+  shell = pkgs.mkShell rec {
+    nativeBuildInputs = [
+      pkgs.bash
+      pkgs.coreutils
+      pkgs.findutils
+      pkgs.protobuf
+      pkgs.go_1_17
+      pkgs.terraform
+      pkgs.bazelisk
+      node
+      yarn
+  ];
+  shellHook = ''
+    export CURRENT_PLATFORM="$(go env GOOS)_$(go env GOARCH)"
+    export BAZEL_SH="$(which bash)"
+  '';
+};
 in
-    tools
+  shell
