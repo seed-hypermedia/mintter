29ecb18bc2b53724057e7dac3d1879b04b7a5db5
Jonas Kruckenberg <iterpre@protonmail.com>
2021-09-22 10:57:13 +0200

    build(frontend): preliminary support for Tauri
    chore: remove global prettier config
    
    chore: setup covector
    
    chore: cleanup
    
    chore: update tsconfig
    
    chore(ui): setup eslint and prettier
    
    chore(mttast): setup eslint and prettier
    
    chore: remove unused mttast-schema package
    
    chore(mttast-builder): setup eslint and prettier
    
    chore(client): setup eslint and prettier
    
    fix: no need to import fragment
    
    disable absolute imports
    
    feat(app): enable prerendering
    
    feat(app): add tauri
    
    add new icons
    
    change build file
    
    Create release.tauri.conf.json
    
    feat(host): add window menu
    
    feat(host): add system tray menu placeholder
    
    Update vite.config.ts
    
    chore: enable system try feature flag
    
    fix: attach menu before running the app
    
    fix: app names and description
    
    fix: uncapitalize app name
    
    Update Cargo.lock

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -1,5 +1,5 @@
-import {defineConfig} from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import {defineConfig} from 'vite'
 import reactJSX from 'vite-react-jsx'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
