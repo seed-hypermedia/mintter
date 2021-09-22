5b51bdef8255a6f09c981f242e649eb1cd3e0c41
Horacio Herrera <hi@horacioh.com>
2021-09-22 18:01:13 +0200

    wip storybook
    Former-commit-id: 739acd1a42ec86b6f84fae45f1bc138be28f73d1

---
+++ frontend/ui/src/stories/Header.stories.tsx
@@ -0,0 +1,18 @@
+import type {ComponentMeta, ComponentStory} from '@storybook/react'
+import React from 'react'
+import {Header} from './Header'
+
+export default {
+  title: 'Example/Header',
+  component: Header,
+} as ComponentMeta<typeof Header>
+
+const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />
+
+export const LoggedIn = Template.bind({})
+LoggedIn.args = {
+  user: {},
+}
+
+export const LoggedOut = Template.bind({})
+LoggedOut.args = {}
