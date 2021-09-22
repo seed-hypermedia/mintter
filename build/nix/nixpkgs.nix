61bda4b234607cb16f6448f12778062228966942
Alexandr Burdiyan <burdiyan@gmail.com>
2021-09-22 15:55:06 +0200

    build: add system deps for Tauri for Darwin

--- build/nix/nixpkgs.nix
+++ build/nix/nixpkgs.nix
@@ -2,9 +2,9 @@ import (builtins.fetchGit {
   # Descriptive name to make the store path easier to identify
   name = "nixpkgs-unstable";
   url = https://github.com/nixos/nixpkgs;
-  # Commit hash for nixos-unstable as of 2021-09-15.
+  # Commit hash for nixos-unstable as of 2021-09-22.
   # Run `git ls-remote https://github.com/nixos/nixpkgs refs/heads/nixpkgs-unstable`
   # to get the most recent revision.
   ref = "refs/heads/nixpkgs-unstable";
-  rev = "071317d543205ee5f5611d391a37582f9b282240";
+  rev = "e0ce3c683ae677cf5aab597d645520cddd13392b";
 }) {}
--- shell.nix
+++ shell.nix
@@ -3,8 +3,8 @@ let
   protoc-gen-grpc-web = pkgs.callPackage ./build/nix/protoc-gen-grpc-web {};
   node = pkgs.nodejs-16_x;
   yarn = (pkgs.yarn.override { nodejs = node; });
-    
-  shell = pkgs.mkShell rec {
+in
+  pkgs.mkShell rec {
     nativeBuildInputs = [
       pkgs.bash
       pkgs.coreutils
@@ -13,14 +13,29 @@ let
       pkgs.go_1_17
       pkgs.terraform
       pkgs.bazelisk
-      pkgs.rustup
+      pkgs.rustc
+      pkgs.cargo
+      pkgs.rustfmt
       node
       yarn
-  ];
-  shellHook = ''
-    export CURRENT_PLATFORM="$(go env GOOS)_$(go env GOARCH)"
-    export BAZEL_SH="$(which bash)"
-  '';
-};
-in
-  shell
+      pkgs.pkg-config
+      pkgs.gcc    
+      pkgs.libiconv
+    ] ++ (
+      pkgs.lib.optionals pkgs.stdenv.isDarwin [
+        pkgs.darwin.libobjc
+        pkgs.darwin.apple_sdk.frameworks.Security
+        pkgs.darwin.apple_sdk.frameworks.CoreServices
+        pkgs.darwin.apple_sdk.frameworks.CoreFoundation
+        pkgs.darwin.apple_sdk.frameworks.Foundation
+        pkgs.darwin.apple_sdk.frameworks.AppKit
+        pkgs.darwin.apple_sdk.frameworks.WebKit
+        pkgs.darwin.apple_sdk.frameworks.Cocoa
+      ]
+    );
+    shellHook = ''
+      rm -rf bazel
+      export CURRENT_PLATFORM="$(go env GOOS)_$(go env GOARCH)"
+      export BAZEL_SH="$(which bash)"
+    '';
+  }
