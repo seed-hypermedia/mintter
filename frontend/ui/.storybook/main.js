7a79059a7d794e51988a6437f269c2e00513e25c
Horacio Herrera <hi@horacioh.com>
2021-10-01 13:03:04 +0200

    buton story first iteration
    Former-commit-id: 44ac1917cdfc577008600a7a5d89027fde5e9f6a

--- frontend/ui/.storybook/main.js
+++ frontend/ui/.storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(tsx|mdx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   core: {
     builder: 'storybook-builder-vite',
