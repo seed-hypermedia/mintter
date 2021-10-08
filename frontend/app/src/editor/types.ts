f4318dc8a1f94373fecd1ec64c15c97c73a06987
Jonas Kruckenberg <iterpre@protonmail.com>
2021-10-08 12:21:35 +0200

    fix: make `EditorPlugin` event handler typing more strict
    Former-commit-id: 6ab14eb25f5dbcbda16e92c3c557a311c7ce151b

--- frontend/app/src/editor/types.ts
+++ frontend/app/src/editor/types.ts
@@ -1,7 +1,8 @@
 import type {Document, Image, MttastContent, Text, Video} from '@mintter/mttast'
+import React from 'react'
 import type {BaseEditor, Editor, NodeEntry, Range} from 'slate'
 import type {HistoryEditor} from 'slate-history'
-import type {Editable, ReactEditor, RenderElementProps, RenderLeafProps} from 'slate-react'
+import type {ReactEditor, RenderElementProps, RenderLeafProps} from 'slate-react'
 
 declare module 'slate' {
   export interface CustomTypes {
@@ -11,18 +12,137 @@ declare module 'slate' {
   }
 }
 
-export type EditorEventHandlers = {
-  [Event in keyof React.DOMAttributes<HTMLTextAreaElement> as Exclude<
-    Event,
-    'children' | 'dangerouslySetInnerHTML'
-  >]?: Parameters<typeof Editable>[0][Event]
-}
-
-interface BeforeInputHandler {
+export interface EditorEventHandlers {
   onDOMBeforeInput?: (event: InputEvent) => void
+  // now come all the event handlers, this mirrors all supported react events right now
+  // for a proper plugin system we will want to trim this list somewhat
+
+  // Clipboard Events
+  onCopy?: React.ClipboardEventHandler
+  onCopyCapture?: React.ClipboardEventHandler
+  onCut?: React.ClipboardEventHandler
+  onCutCapture?: React.ClipboardEventHandler
+  onPaste?: React.ClipboardEventHandler
+  onPasteCapture?: React.ClipboardEventHandler
+
+  // Composition Events
+  onCompositionEnd?: React.CompositionEventHandler
+  onCompositionEndCapture?: React.CompositionEventHandler
+  onCompositionStart?: React.CompositionEventHandler
+  onCompositionStartCapture?: React.CompositionEventHandler
+  onCompositionUpdate?: React.CompositionEventHandler
+  onCompositionUpdateCapture?: React.CompositionEventHandler
+
+  // Focus Events
+  onFocus?: React.FocusEventHandler
+  onFocusCapture?: React.FocusEventHandler
+  onBlur?: React.FocusEventHandler
+  onBlurCapture?: React.FocusEventHandler
+
+  // Keyboard Events
+  onKeyDown?: React.KeyboardEventHandler
+  onKeyDownCapture?: React.KeyboardEventHandler
+  onKeyPress?: React.KeyboardEventHandler
+  onKeyPressCapture?: React.KeyboardEventHandler
+  onKeyUp?: React.KeyboardEventHandler
+  onKeyUpCapture?: React.KeyboardEventHandler
+
+  // MouseEvents
+  onClick?: React.MouseEventHandler
+  onClickCapture?: React.MouseEventHandler
+  onContextMenu?: React.MouseEventHandler
+  onContextMenuCapture?: React.MouseEventHandler
+  onDblClick?: React.MouseEventHandler
+  onDblClickCapture?: React.MouseEventHandler
+  onDrag?: React.DragEventHandler
+  onDragCapture?: React.DragEventHandler
+  onDragEnd?: React.DragEventHandler
+  onDragEndCapture?: React.DragEventHandler
+  onDragEnter?: React.DragEventHandler
+  onDragEnterCapture?: React.DragEventHandler
+  onDragExit?: React.DragEventHandler
+  onDragExitCapture?: React.DragEventHandler
+  onDragLeave?: React.DragEventHandler
+  onDragLeaveCapture?: React.DragEventHandler
+  onDragOver?: React.DragEventHandler
+  onDragOverCapture?: React.DragEventHandler
+  onDragStart?: React.DragEventHandler
+  onDragStartCapture?: React.DragEventHandler
+  onDrop?: React.DragEventHandler
+  onDropCapture?: React.DragEventHandler
+  onMouseDown?: React.MouseEventHandler
+  onMouseDownCapture?: React.MouseEventHandler
+  onMouseEnter?: React.MouseEventHandler
+  onMouseEnterCapture?: React.MouseEventHandler
+  onMouseLeave?: React.MouseEventHandler
+  onMouseLeaveCapture?: React.MouseEventHandler
+  onMouseMove?: React.MouseEventHandler
+  onMouseMoveCapture?: React.MouseEventHandler
+  onMouseOut?: React.MouseEventHandler
+  onMouseOutCapture?: React.MouseEventHandler
+  onMouseOver?: React.MouseEventHandler
+  onMouseOverCapture?: React.MouseEventHandler
+  onMouseUp?: React.MouseEventHandler
+  onMouseUpCapture?: React.MouseEventHandler
+
+  // Selection Events
+  onSelect?: React.EventHandler<React.SyntheticEvent>
+  onSelectCapture?: React.EventHandler<React.SyntheticEvent>
+
+  // Touch Events
+  onTouchCancel?: React.TouchEventHandler
+  onTouchCancelCapture?: React.TouchEventHandler
+  onTouchEnd?: React.TouchEventHandler
+  onTouchEndCapture?: React.TouchEventHandler
+  onTouchMove?: React.TouchEventHandler
+  onTouchMoveCapture?: React.TouchEventHandler
+  onTouchStart?: React.TouchEventHandler
+  onTouchStartCapture?: React.TouchEventHandler
+
+  // Pointer Events
+  onPointerOver?: React.PointerEventHandler
+  onPointerOverCapture?: React.PointerEventHandler
+  onPointerEnter?: React.PointerEventHandler
+  onPointerEnterCapture?: React.PointerEventHandler
+  onPointerDown?: React.PointerEventHandler
+  onPointerDownCapture?: React.PointerEventHandler
+  onPointerMove?: React.PointerEventHandler
+  onPointerMoveCapture?: React.PointerEventHandler
+  onPointerUp?: React.PointerEventHandler
+  onPointerUpCapture?: React.PointerEventHandler
+  onPointerCancel?: React.PointerEventHandler
+  onPointerCancelCapture?: React.PointerEventHandler
+  onPointerOut?: React.PointerEventHandler
+  onPointerOutCapture?: React.PointerEventHandler
+  onPointerLeave?: React.PointerEventHandler
+  onPointerLeaveCapture?: React.PointerEventHandler
+  onGotPointerCapture?: React.PointerEventHandler
+  onGotPointerCaptureCapture?: React.PointerEventHandler
+  onLostPointerCapture?: React.PointerEventHandler
+  onLostPointerCaptureCapture?: React.PointerEventHandler
+
+  // UI Events
+  onScroll?: React.UIEventHandler
+  onScrollCapture?: React.UIEventHandler
+
+  // Wheel Events
+  onWheel?: React.WheelEventHandler
+  onWheelCapture?: React.WheelEventHandler
+
+  // Animation Events
+  onAnimationStart?: React.AnimationEventHandler
+  onAnimationStartCapture?: React.AnimationEventHandler
+  onAnimationEnd?: React.AnimationEventHandler
+  onAnimationEndCapture?: React.AnimationEventHandler
+  onAnimationIteration?: React.AnimationEventHandler
+  onAnimationIterationCapture?: React.AnimationEventHandler
+
+  // Transition Events
+  onTransitionEnd?: React.TransitionEventHandler
+  onTransitionEndCapture?: React.TransitionEventHandler
 }
 
-export interface EditorPlugin extends EditorEventHandlers, BeforeInputHandler {
+export interface EditorPlugin extends EditorEventHandlers {
   name: string
   apply?: string | ((mode: string) => boolean)
   configureEditor?: (editor: Editor) => Editor | undefined | void
