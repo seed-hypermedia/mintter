6a8c34b2c6bcb85561b5388c54efa736aeff054d
Alexandr Burdiyan <burdiyan@gmail.com>
2021-10-11 09:55:43 +0200

    build(nix): use Bazel from nixpkgs

--- build/nix/nixpkgs.nix
+++ build/nix/nixpkgs.nix
@@ -12,7 +12,6 @@ import (builtins.fetchGit {
     (self: super: {
       go = super.go_1_17;
       nodejs = super.nodejs-16_x;
-      bazel = super.callPackage ./bazel {};
       impure-cc = super.callPackage ./impure-cc {};
       mkShell = super.mkShell.override {
         stdenv = super.stdenvNoCC;
