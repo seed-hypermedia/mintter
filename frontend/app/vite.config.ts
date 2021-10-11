98235a1711272c716be4d2ef165654da516387e9
Jonas Kruckenberg <iterpre@protonmail.com>
2021-10-11 18:11:33 +0200

    build: optimize for production but respect TAURI_DEBUG

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -4,11 +4,12 @@ import reactJSX from 'vite-react-jsx'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
-
 export default defineConfig({
   plugins: [reactRefresh(), reactJSX(), tsconfigPaths()],
   build: {
-    sourcemap: true,
+    target: 'es2021',
+    minify: !process.env.TAURI_DEBUG && 'esbuild',
+    sourcemap: !process.env.TAURI_DEBUG,
     chunkSizeWarningLimit: 2048,
   },
 })
