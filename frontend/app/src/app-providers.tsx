7e3a05b7ae1235feacd2ab955346472d5f882283
Horacio Herrera <hi@horacioh.com>
2021-11-09 00:42:31 +0100

    refactor auth machine

--- frontend/app/src/app-providers.tsx
+++ frontend/app/src/app-providers.tsx
@@ -1,4 +1,7 @@
 import {Theme} from '@mintter/ui/theme'
+import {useInterpret} from '@xstate/react'
+import {AuthProvider} from 'frontend/app/src/auth-context'
+import {authStateMachine} from 'frontend/app/src/authstate-machine'
 import {PropsWithChildren, Suspense} from 'react'
 import {Toaster} from 'react-hot-toast'
 import {dehydrate, Hydrate, QueryClient, QueryClientProvider} from 'react-query'
@@ -15,18 +18,21 @@ console.log('🚀 ~ file: app-providers.tsx ~ line 13 ~ queryClient', queryClien
 const dehydrateState = dehydrate(queryClient)
 
 export function AppProviders({children}: PropsWithChildren<unknown>) {
+  const authService = useInterpret(authStateMachine)
   return (
-    <Theme>
-      <Suspense fallback={<p>loading...</p>}>
-        <QueryClientProvider client={queryClient}>
-          <Hydrate state={dehydrateState}>
-            {children}
-            <Toaster position="bottom-right" />
-            {/** @TODO Uncommenting this causes an error with react-query. We should fix this */}
-            {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-          </Hydrate>
-        </QueryClientProvider>
-      </Suspense>
-    </Theme>
+    <AuthProvider value={authService}>
+      <Theme>
+        <Suspense fallback={<p>loading...</p>}>
+          <QueryClientProvider client={queryClient}>
+            <Hydrate state={dehydrateState}>
+              {children}
+              <Toaster position="bottom-right" />
+              {/** @TODO Uncommenting this causes an error with react-query. We should fix this */}
+              {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+            </Hydrate>
+          </QueryClientProvider>
+        </Suspense>
+      </Theme>
+    </AuthProvider>
   )
 }
