2588b24b59dd7ba60899826c190be4e8cea554f9
juligasa <11684004+juligasa@users.noreply.github.com>
2021-11-11 12:50:16 +0100

    lndhub paying invoice
    Former-commit-id: ed3b162ebc8796114e796a75a9967d598147494c

--- backend/lndhub/lndhub.go
+++ backend/lndhub/lndhub.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"regexp"
+	"strconv"
 	"sync"
 
 	"github.com/btcsuite/btcd/chaincfg"
@@ -77,9 +78,11 @@ func NewLndhub(credentials string) (*Lndhub, error) {
 		if res != nil {
 			return nil, fmt.Errorf("credentials contained more than necessary fields. it shoud be " +
 				"lndhub://c227a7fb5c71a22fac33:d2a48ab779aa1b02e858@https://lndhub.io")
+
 		} else {
 			return nil, fmt.Errorf("couldn't parse credentials, probalby wrong format. it shoud be " +
 				"lndhub://c227a7fb5c71a22fac33:d2a48ab779aa1b02e858@https://lndhub.io")
+
 		}
 	} else {
 		if refresh_token, access_token, err := auth(res[1], res[2], res[3]); err != nil {
@@ -115,30 +118,40 @@ func (l *Lndhub) GetBalance() (uint64, error) {
 			return 0, err
 		} else {
 			defer resp.Body.Close()
+
 			if resp.StatusCode != http.StatusOK {
 				return 0, fmt.Errorf("Response status " + http.StatusText(resp.StatusCode) +
 					" expected " + http.StatusText(http.StatusOK))
+
 			} else if err := json.NewDecoder(resp.Body).Decode(&ret); err != nil {
 				return 0, err
+
 			} else if err_field, ok := ret["error"]; ok && err_field.(bool) {
 				var code, message string
+
 				if code_field, ok := ret["code"]; ok {
 					code = fmt.Sprintf("%1.0f", code_field.(float64))
 				}
+
 				if message_field, ok := ret["message"]; ok {
 					message = message_field.(string)
 				}
+
 				return 0, fmt.Errorf("could't authernticate, error code " + code +
 					" message " + message)
 
 			} else if btc, ok := ret["BTC"]; !ok {
 				return 0, fmt.Errorf("no Bitcoin deposits found, please fund your account with btcs")
+
 			} else if balance, ok := btc.(map[string]interface{}); !ok {
 				return 0, fmt.Errorf("response in wrong format")
+
 			} else if funds, ok := balance["AvailableBalance"]; !ok {
 				return 0, fmt.Errorf("couldn't get AvailableBalance")
+
 			} else {
 				return uint64(funds.(float64)), nil
+
 			}
 		}
 
@@ -155,30 +168,41 @@ func auth(user, password, apiurl string) (string, string, error) {
 
 	if json_data, err := json.Marshal(payload); err != nil {
 		return refresh_token, access_token, err
+
 	} else if resp, err := http.Post(apiurl+authRoute, "application/json",
 		bytes.NewBuffer(json_data)); err != nil {
 		return refresh_token, access_token, err
+
 	} else if resp.StatusCode != http.StatusOK {
 		return refresh_token, access_token, fmt.Errorf("Response status " + http.StatusText(resp.StatusCode) +
 			"expected " + http.StatusText(http.StatusOK))
+
 	} else if err := json.NewDecoder(resp.Body).Decode(&ret); err != nil {
 		return refresh_token, access_token, err
+
 	} else if err_field, ok := ret["error"]; ok && err_field.(bool) {
+
 		var code, message string
+
 		if code_field, ok := ret["code"]; ok {
 			code = fmt.Sprintf("%1.0f", code_field.(float64))
 		}
 		if message_field, ok := ret["message"]; ok {
 			message = message_field.(string)
 		}
+
 		return refresh_token, access_token, fmt.Errorf("could't authenticate, error code " + code +
 			" message " + message)
+
 	} else if refresh_token, ok := ret["refresh_token"]; !ok {
 		return refresh_token.(string), access_token, fmt.Errorf("refresh_token not present in response")
+
 	} else if access_token, ok := ret["access_token"]; !ok {
 		return refresh_token.(string), access_token.(string), fmt.Errorf("access_token not present in response")
+
 	} else {
 		return refresh_token.(string), access_token.(string), nil
+
 	}
 }
 
@@ -221,7 +245,7 @@ func (l *Lndhub) CreateInvoice(amount uint64, memo string) (string, error) {
 				if message_field, ok := ret["message"]; ok {
 					message = message_field.(string)
 				}
-				return invoice, fmt.Errorf("could't authenticate, error code " + code +
+				return invoice, fmt.Errorf("could't create invoice, error code " + code +
 					" message " + message)
 
 			} else if pay_req, ok := ret["payment_request"]; !ok {
@@ -267,6 +291,56 @@ func (l *Lndhub) DecodeInvoice(pay_req string) (*zpay32.Invoice, error) {
 		return nil, err
 	} else {
 		return decodedInvoice, nil
+	}
+
+}
+
+// This function tries to pay the invoice provided. With the amount provided in satoshis. The
+// enconded amount in the invoid should match the provided amount as a double check
+func (l *Lndhub) PayInvoice(pay_req string, sats uint64) error {
+
+	var ret map[string]interface{}
+	payload := map[string]string{"invoice": pay_req}
+
+	if invoice, err := l.DecodeInvoice(pay_req); err != nil {
+		return nil
+	} else if uint64(invoice.MilliSat.ToSatoshis()) != sats {
+		return fmt.Errorf("amount mismatch. Invoice amt is " + invoice.MilliSat.ToSatoshis().String() +
+			"sats and provided amount is " + strconv.FormatInt(int64(sats), 10))
+	} else if json_data, err := json.Marshal(payload); err != nil {
+		return err
+	} else if req, err := http.NewRequest("POST", l.apiurl+payInvoiceRoute, bytes.NewBuffer(json_data)); err != nil {
+		return err
+	} else {
+		// add authorization header to the req
+		req.Header.Add("Authorization", "Bearer "+l.access_token)
+		req.Header.Add("Content-Type", `application/json`)
+		// Send req using http Client
+		client := &http.Client{}
+		if resp, err := client.Do(req); err != nil {
+			return err
+		} else {
+			defer resp.Body.Close()
+			if resp.StatusCode != http.StatusOK {
+				return fmt.Errorf("Response status " + http.StatusText(resp.StatusCode) +
+					"expected " + http.StatusText(http.StatusOK))
+			} else if err := json.NewDecoder(resp.Body).Decode(&ret); err != nil {
+				return err
+			} else if err_field, ok := ret["error"]; ok && err_field.(bool) {
+				var code, message string
+				if code_field, ok := ret["code"]; ok {
+					code = fmt.Sprintf("%1.0f", code_field.(float64))
+				}
+				if message_field, ok := ret["message"]; ok {
+					message = message_field.(string)
+				}
+				return fmt.Errorf("could't pay invoice, error code " + code +
+					" message " + message)
+
+			} else {
+				return nil
+			}
+		}
 	}
 
 }
--- backend/lndhub/lndhub_test.go
+++ backend/lndhub/lndhub_test.go
@@ -18,39 +18,65 @@ const (
 
 func TestLndhub(t *testing.T) {
 	tests := [...]struct {
-		name        string
-		credentials string
-		mustFail    bool
+		name            string
+		credentials     string
+		generateInvoice bool
+		payInvoice      bool
+		mustFail        bool
 	}{
 		{
-			name:        "syntaxErrorCredentials",
-			credentials: syntaxErrorCredentials,
-			mustFail:    true,
+			name:            "syntaxErrorCredentials",
+			credentials:     syntaxErrorCredentials,
+			generateInvoice: false,
+			payInvoice:      false,
+			mustFail:        true,
 		},
 		{
-			name:        "syntaxErrorCredentials2",
-			credentials: syntaxErrorCredentials2,
-			mustFail:    true,
+			name:            "syntaxErrorCredentials2",
+			credentials:     syntaxErrorCredentials2,
+			generateInvoice: false,
+			payInvoice:      false,
+			mustFail:        true,
 		},
 		{
-			name:        "syntaxErrorCredentials3",
-			credentials: syntaxErrorCredentials3,
-			mustFail:    true,
+			name:            "syntaxErrorCredentials3",
+			credentials:     syntaxErrorCredentials3,
+			generateInvoice: false,
+			payInvoice:      false,
+			mustFail:        true,
 		},
 		{
-			name:        "semanticErrorCredentials",
-			credentials: semanticErrorCredentials,
-			mustFail:    true,
+			name:            "semanticErrorCredentials",
+			credentials:     semanticErrorCredentials,
+			generateInvoice: false,
+			payInvoice:      false,
+			mustFail:        true,
 		},
 		{
-			name:        "goodCredentials",
-			credentials: goodCredentials,
-			mustFail:    false,
+			name:            "goodCredentials",
+			credentials:     goodCredentials,
+			generateInvoice: false,
+			payInvoice:      false,
+			mustFail:        false,
+		},
+		{
+			name:            "generateInvoice",
+			credentials:     goodCredentials,
+			generateInvoice: true,
+			payInvoice:      false,
+			mustFail:        false,
+		},
+		{
+			name:            "payInvoice",
+			credentials:     goodCredentials,
+			generateInvoice: true,
+			payInvoice:      true,
+			mustFail:        true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := lndhubTest(t, tt.credentials)
+			err := lndhubTest(t, tt.credentials, tt.generateInvoice, tt.payInvoice)
 			if tt.mustFail {
 				require.Error(t, err, tt.name+". must fail")
 			} else {
@@ -62,30 +88,51 @@ func TestLndhub(t *testing.T) {
 
 }
 
-func lndhubTest(t *testing.T, credentials string) error {
+func lndhubTest(t *testing.T, credentials string, generateInvoice, payInvoice bool) error {
 
 	t.Helper()
 	memo := "test invoice"
 	amt := 100
-	if lndhub, err := NewLndhub(credentials); err != nil {
+	lndhub, err := NewLndhub(credentials)
+	if err != nil {
 		return err
+
 	} else if _, err := lndhub.GetBalance(); err != nil {
 		return err
-	} else if pay_req, err := lndhub.CreateInvoice(uint64(amt), memo); err != nil {
-		return err
-	} else if invoice, err := lndhub.DecodeInvoice(pay_req); err != nil {
-		return err
-	} else if *invoice.Description != memo {
-		return fmt.Errorf("Decoded invoice memo " + *invoice.Description + " expected:" + memo)
-	} else if int64(invoice.MilliSat.ToSatoshis()) != int64(amt) {
-		return fmt.Errorf("Decoded invoice amt " + invoice.MilliSat.ToSatoshis().String() +
-			" expected:" + strconv.FormatInt(int64(amt), 10))
-	} else if invoice, err := lndhub.DecodeInvoiceAPI(pay_req); err != nil {
-		return err
-	} else if invoice.Description != memo {
-		return fmt.Errorf("Decoded invoice memo " + invoice.Description + " expected:" + memo)
-	} else if n, err := strconv.ParseUint(invoice.Num_satoshis, 10, 64); err != nil || n != uint64(amt) {
-		return fmt.Errorf("Decoded invoice amt " + invoice.Num_satoshis + " expected:" + strconv.FormatInt(int64(amt), 10))
+	}
+
+	if generateInvoice {
+		if pay_req, err := lndhub.CreateInvoice(uint64(amt), memo); err != nil {
+			return err
+
+		} else if invoice, err := lndhub.DecodeInvoice(pay_req); err != nil {
+			return err
+
+		} else if *invoice.Description != memo {
+			return fmt.Errorf("Decoded invoice memo " + *invoice.Description + " expected:" + memo)
+
+		} else if int64(invoice.MilliSat.ToSatoshis()) != int64(amt) {
+			return fmt.Errorf("Decoded invoice amt " + invoice.MilliSat.ToSatoshis().String() +
+				" expected:" + strconv.FormatInt(int64(amt), 10))
+
+		} else if api_invoice, err := lndhub.DecodeInvoiceAPI(pay_req); err != nil {
+			return err
+
+		} else if api_invoice.Description != memo {
+			return fmt.Errorf("Decoded invoice memo " + api_invoice.Description + " expected:" + memo)
+
+		} else if n, err := strconv.ParseUint(api_invoice.Num_satoshis, 10, 64); err != nil || n != uint64(amt) {
+			return fmt.Errorf("Decoded invoice amt " + api_invoice.Num_satoshis + " expected:" + strconv.FormatInt(int64(amt), 10))
+
+		} else if payInvoice {
+			if err := lndhub.PayInvoice(pay_req, uint64(invoice.MilliSat.ToSatoshis())); err != nil {
+				return err
+			} else {
+				return nil
+			}
+		} else {
+			return nil
+		}
 	} else {
 		return nil
 	}
