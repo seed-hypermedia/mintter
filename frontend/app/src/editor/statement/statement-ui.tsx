29cdbd79771c9eb3707cdec767823cf54602d670
Horacio Herrera <hi@horacioh.com>
2021-12-10 17:51:31 +0100

    fix(lists): markdown plugin + list markers
    - modify markdown group creation
    - fix(lists): fix fucking marker duplication

--- frontend/app/src/editor/statement/statement-ui.tsx
+++ frontend/app/src/editor/statement/statement-ui.tsx
@@ -3,16 +3,19 @@ import {css, styled} from '@mintter/ui/stitches.config'
 export const statementStyle = css({
   marginTop: '$6',
   padding: 0,
-  listStyle: 'none',
-  position: 'relative',
-  [`[data-element-type="orderedList"] > &, [data-element-type="unorderedList"] > &`]: {
-    listStyle: 'inherit',
-    '&:marker': {
-      userSelect: 'none',
-      WebkitUserSelect: 'none',
-    },
-  },
+  // listStyle: 'none',
+  listStyleType: '*',
   wordBreak: 'break-word',
+  position: 'relative',
+  '&::marker': {
+    color: '$text-muted',
+  },
+  [`[data-element-type="orderedList"] > &::marker`]: {
+    fontSize: '0.8em',
+  },
+  [`[data-element-type="unorderedList"] > &::marker`]: {
+    fontSize: '1.2em',
+  },
 })
 
 export const StatementUI = styled('li', statementStyle)
