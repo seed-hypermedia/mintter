95df199b977d26abc5beb19db048580dc3ec1d2b
Alexandr Burdiyan <burdiyan@gmail.com>
2021-12-15 12:30:35 +0100

    refactor(backend): simplify background jobs start

--- backend/backend.go
+++ backend/backend.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"net"
 	"net/http"
-	"reflect"
 	"sync"
 	"time"
 
@@ -21,7 +20,6 @@ import (
 	"github.com/multiformats/go-multiaddr"
 	"go.uber.org/multierr"
 	"go.uber.org/zap"
-	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/connectivity"
@@ -31,7 +29,6 @@ import (
 
 	accounts "mintter/backend/api/accounts/v1alpha"
 	p2p "mintter/backend/api/p2p/v1alpha"
-	"mintter/backend/cleanup"
 	"mintter/backend/ipfs"
 	"mintter/backend/ipfs/sqlitebs"
 	"mintter/backend/lndhub"
@@ -100,9 +97,7 @@ func newBackend(log *zap.Logger, pool *sqlitex.Pool, r *repo, p2p *p2pNode) *bac
 // when the process is finished or ctx is canceled.
 func (srv *backend) Start(ctx context.Context) (err error) {
 	// When we start the backend it may be that we don't have the account
-	// yet, thus we don't want to start all the other services.
-	// So we wait until the repo is ready, then start the P2P node,
-	// wait until that one is ready, and then provide our own account on the DHT.
+	// yet, thus we don't want to start all the other services, until the registration happen.
 	select {
 	case <-ctx.Done():
 		return ctx.Err()
@@ -110,142 +105,9 @@ func (srv *backend) Start(ctx context.Context) (err error) {
 		break
 	}
 
-	var clean cleanup.Stack
-	defer func() {
-		err = multierr.Append(err, clean.Close())
-	}()
-	clean.Add(&srv.rpc)
+	defer multierr.AppendInvoke(&err, multierr.Close(&srv.rpc))
 
-	sub, err := srv.p2p.libp2p.Host.EventBus().Subscribe([]interface{}{
-		&event.EvtPeerIdentificationCompleted{},
-	})
-	if err != nil {
-		return fmt.Errorf("failed to setup libp2p event listener: %w", err)
-	}
-	clean.Add(sub)
-
-	g, ctx := errgroup.WithContext(ctx)
-
-	// Start libp2p listener for Mintter protocol.
-	g.Go(func() error {
-		lis, err := gostream.Listen(srv.p2p.libp2p.Host, ProtocolID)
-		if err != nil {
-			return fmt.Errorf("failed to setup gostream listener: %w", err)
-		}
-
-		s := grpc.NewServer()
-		p2p.RegisterP2PServer(s, &p2pAPI{back: srv})
-
-		go func() {
-			<-ctx.Done()
-			s.GracefulStop()
-		}()
-
-		return s.Serve(lis)
-	})
-
-	// Handle libp2p events.
-	g.Go(func() error {
-		var wg sync.WaitGroup
-		defer wg.Wait()
-
-		// For some very-very-very annoying reason libp2p might deliver duplicate
-		// events. This is especially annoying in tests, because after initial peer
-		// verification is done we may go into another one because of the duplicate event.
-		// I couldn't figure out why it was happenning, so I implemented a simply dedupe here.
-		// We'll track events that we receive and launch handling for, and we remove them
-		// from the map some time after they are handled.
-
-		// On this channel handled events will be returned, so we can clean them up from the dedupe map.
-		handled := make(chan interface{}, 10)
-
-		seen := map[interface{}]struct{}{}
-
-		for {
-			select {
-			case <-ctx.Done():
-				return ctx.Err()
-			case evt := <-handled:
-				delete(seen, evt)
-			case evt, ok := <-sub.Out():
-				if !ok {
-					return fmt.Errorf("libp2p event channel closed")
-				}
-
-				// TODO: collect metrics about how many duplicate events we get of each type.
-				// See if reporting an issue is necessary.
-
-				if _, ok := seen[evt]; ok {
-					continue
-				}
-
-				seen[evt] = struct{}{}
-
-				wg.Add(1)
-				go func() {
-					err := srv.handleLibp2pEvent(ctx, evt)
-					if err != nil {
-						srv.log.Error("FailedToHandleLibp2pEvent",
-							zap.Any("event", evt),
-							zap.String("eventType", reflect.TypeOf(evt).String()),
-							zap.Error(err),
-						)
-					}
-					wg.Done()
-
-					// We'll sleep for some arbitrary amount of time so that potential duplicate
-					// event have time to be deduped. We're doing this after wg.Done
-					// because we don't to wait for this if we want to exit the app.
-					time.Sleep(time.Second)
-					handled <- evt
-				}()
-			}
-		}
-	})
-
-	// Start P2P node.
-	g.Go(func() error {
-		return srv.p2p.Start(ctx)
-	})
-
-	// Start reprovider when P2P node is ready to use.
-	g.Go(func() error {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		case <-srv.p2p.Ready():
-			return srv.p2p.prov.StartReproviding(ctx)
-		}
-	})
-
-	// Start pulling account updates.
-	g.Go(func() error {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		case <-srv.p2p.Ready():
-			break
-		}
-
-		t := time.NewTimer(time.Hour)
-		t.Stop()
-		defer t.Stop()
-
-		for {
-			if err := srv.SyncAccounts(ctx); err != nil {
-				return err
-			}
-
-			t.Reset(accountsPullInterval)
-
-			select {
-			case <-ctx.Done():
-				return ctx.Err()
-			case <-t.C:
-				continue
-			}
-		}
-	})
+	g := srv.startBackgroundJobs(ctx)
 
 	// Provide our own account on the DHT.
 	acc, err := srv.repo.Account()
