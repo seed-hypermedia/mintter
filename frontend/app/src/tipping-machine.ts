aab7aba555a9629364be4595364f4514bf4b8d30
Horacio Herrera <hi@horacioh.com>
2021-12-21 16:24:58 +0100

    feat(tipping): tip from document
    Former-commit-id: 9faab43a11f65c9a8cc5218dd658e9250215cd73

---
+++ frontend/app/src/tipping-machine.ts
@@ -0,0 +1,202 @@
+import request, {gql} from 'graphql-request'
+import {createModel} from 'xstate/lib/model'
+import {MINTTER_GRAPHQL_API_URL} from './wallet-machine'
+
+export const tippingModel = createModel(
+  {
+    amount: 0,
+    accountID: '',
+    publicationID: '',
+    invoice: '',
+    errorMessage: '',
+  },
+  {
+    events: {
+      OPEN: () => ({}),
+      CLOSE: () => ({}),
+      RETRY: () => ({}),
+      SET_TIP_DATA: (publicationID: string, accountID: string) => ({publicationID, accountID}),
+      UPDATE_AMOUNT: (amount: number) => ({amount}),
+      REQUEST_INVOICE: () => ({}),
+      REPORT_INVOICE_RECEIVED: (invoice: string) => ({invoice}),
+      REPORT_INVOICE_ERRORED: (errorMessage: string) => ({errorMessage}),
+      PAY_INVOICE: () => ({}),
+      REPORT_PAID: () => ({}),
+      REPORT_PAID_ERRORED: (errorMessage: string) => ({errorMessage}),
+    },
+  },
+)
+
+export const tippingMachine = tippingModel.createMachine({
+  initial: 'close',
+  context: tippingModel.initialContext,
+  on: {
+    SET_TIP_DATA: {
+      actions: [
+        tippingModel.assign((_, event) => ({
+          publicationID: event.publicationID,
+          accountID: event.accountID,
+        })),
+      ],
+    },
+  },
+  states: {
+    close: {
+      on: {
+        OPEN: [
+          {
+            target: 'open',
+          },
+        ],
+      },
+    },
+    open: {
+      initial: 'setAmount',
+      on: {
+        CLOSE: 'close',
+      },
+      states: {
+        setAmount: {
+          on: {
+            REQUEST_INVOICE: {
+              target: 'requestInvoice',
+            },
+            UPDATE_AMOUNT: {
+              actions: [
+                tippingModel.assign({
+                  amount: (_, event) => event.amount,
+                }),
+              ],
+            },
+          },
+        },
+        errored: {
+          tags: ['pending'],
+          on: {
+            RETRY: {
+              target: 'setAmount',
+              actions: [
+                tippingModel.assign({
+                  errorMessage: '',
+                }),
+              ],
+            },
+            REQUEST_INVOICE: {
+              target: 'requestInvoice',
+              actions: [
+                tippingModel.assign({
+                  errorMessage: '',
+                }),
+              ],
+            },
+          },
+        },
+        requestInvoice: {
+          tags: ['pending'],
+          invoke: {
+            src: (context) => (sendBack) => {
+              let mutation = gql`
+                mutation requestInvoice($input: RequestInvoiceInput!) {
+                  requestInvoice(input: $input) {
+                    paymentRequest
+                  }
+                }
+              `
+              request<{paymentRequest: string}>(MINTTER_GRAPHQL_API_URL, mutation, {
+                input: {
+                  amountSats: context.amount,
+                  accountID: context.accountID,
+                  publicationID: context.publicationID,
+                },
+              })
+                .then((response) => {
+                  console.log('mutation response: ', response)
+                  sendBack(tippingModel.events.REPORT_INVOICE_RECEIVED(response.paymentRequest))
+                })
+                .catch((err) => {
+                  console.log('ERROR: ', err)
+
+                  sendBack(
+                    tippingModel.events.REPORT_INVOICE_ERRORED(
+                      err.response.errors.map((e: any) => e.message).join(' | '),
+                    ),
+                  )
+                })
+            },
+          },
+          on: {
+            REPORT_INVOICE_RECEIVED: {
+              target: 'readyToPay',
+              actions: [
+                tippingModel.assign({
+                  invoice: (_, event) => event.invoice,
+                }),
+              ],
+            },
+            REPORT_INVOICE_ERRORED: {
+              target: 'errored',
+              actions: [
+                tippingModel.assign({
+                  errorMessage: (_, event) => event.errorMessage,
+                }),
+              ],
+            },
+          },
+        },
+        readyToPay: {
+          on: {
+            PAY_INVOICE: {
+              target: 'paying',
+            },
+          },
+        },
+        paying: {
+          tags: ['pending'],
+          invoke: {
+            src: (context) => (sendBack) => {
+              let mutation = gql`
+                mutation payInvoice($input: PayInvoiceInput!) {
+                  payInvoice(input: $input) {
+                    walletID
+                  }
+                }
+              `
+              request<{walletID: string}>(MINTTER_GRAPHQL_API_URL, mutation, {
+                input: {
+                  paymentRequest: context.invoice,
+                },
+              })
+                .then((response) => {
+                  console.log('mutation response: ', response)
+                  sendBack(tippingModel.events.REPORT_PAID())
+                })
+                .catch((err) => {
+                  console.log('ERROR: ', err)
+
+                  sendBack(
+                    tippingModel.events.REPORT_PAID_ERRORED(err.response.errors.map((e: any) => e.message).join(' | ')),
+                  )
+                })
+            },
+          },
+          on: {
+            REPORT_PAID: {
+              target: 'paid',
+            },
+            REPORT_PAID_ERRORED: {
+              target: 'errored',
+              actions: [
+                tippingModel.assign({
+                  errorMessage: (_, event) => event.errorMessage,
+                }),
+              ],
+            },
+          },
+        },
+        paid: {
+          type: 'final',
+        },
+      },
+    },
+  },
+})
