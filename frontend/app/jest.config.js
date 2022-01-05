39efa782a9af9ccff2f9d6e5a02d5b5635d5ff24
Horacio Herrera <hi@horacioh.com>
2022-01-05 01:03:30 +0100

    chore(app): remove old testing setup
    Former-commit-id: 4c0e83eb9e0aecba1b8fbe7fc50ec7326124a8f4

--- frontend/app/jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('@jest/types').Config.InitialOptions} */
-module.exports = {
-  preset: 'vite-jest',
-  testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: {
-    '\\.(css|sass|scss)$': 'identity-obj-proxy',
-  },
-}
--- frontend/app/src/editor/heading/__tests__/heading-normalize.test.ts
+++
@@ -1,9 +0,0 @@
-import {buildEditorHook, EditorMode} from '../../plugin-utils'
-import {plugins} from '../../plugins'
-
-describe('Heading Plugin', () => {
-  test('should render text', () => {
-    const editor = buildEditorHook(plugins, EditorMode.Draft)
-    console.log('editor: ', editor)
-  })
-})
