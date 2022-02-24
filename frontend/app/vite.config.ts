6ccbd5310bc2455854d13914d77167a3abc7e88a
Jonas Kruckenberg <iterpre@protonmail.com>
2022-02-24 10:58:48 +0100

    fix: update vite config

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -1,11 +1,28 @@
 /// <reference types="vitest" />
 
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   cacheDir: '.vite',
+  // prevent vite from obscuring rust errors
+  clearScreen: false,
+  // tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+  // to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`, `TAURI_PLATFORM_VERSION`, `TAURI_PLATFORM_TYPE` and `TAURI_DEBUG` env variables
+  envPrefix: ['VITE_', 'TAURI_'],
+  build: {
+    // tauri supports es2021
+    target: ['es2021', 'chrome97', 'safari13'],
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG && 'esbuild',
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
   plugins: [
     tsconfigPaths(),
     // checker({
@@ -18,12 +35,6 @@ export default defineConfig({
       fastRefresh: process.env.NODE_ENV != 'test',
     }),
   ],
-  build: {
-    target: ['es2021', 'chrome97', 'safari13'],
-    minify: !process.env.TAURI_DEBUG && 'esbuild',
-    sourcemap: !!process.env.TAURI_DEBUG,
-    chunkSizeWarningLimit: 2048,
-  },
   test: {
     setupFiles: ['./src/test/setup.ts'],
   },
