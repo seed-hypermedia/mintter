7a7aa50991aa17bd547c841e752b06ee5db47a0c
Horacio Herrera <hi@horacioh.com>
2022-03-02 13:00:06 -0500

    fix(discussion): fix discussion loading/rendering
    Former-commit-id: 5f74ebd29e30ede795ced7eecbbc94b24fb114e4

--- frontend/app/src/pages/publication.typegen.ts
+++ frontend/app/src/pages/publication.typegen.ts
@@ -10,22 +10,37 @@ export interface Typegen0 {
     assignError: 'PUBLICATION.REPORT.ERROR' | 'REPORT.DISCUSSION.ERROR'
     assignLinks: 'REPORT.DISCUSSION.SUCCESS'
     assignDiscussion: 'REPORT.DISCUSSION.SUCCESS'
-    clearLinks: 'PUBLICATION.FETCH.DATA'
-    clearDiscussion: 'PUBLICATION.FETCH.DATA'
-    clearError: 'PUBLICATION.FETCH.DATA'
+    clearLinks: 'PUBLICATION.FETCH.DATA' | 'TOGGLE.DISCUSSION'
+    clearDiscussion: 'PUBLICATION.FETCH.DATA' | 'TOGGLE.DISCUSSION'
+    clearError: 'PUBLICATION.FETCH.DATA' | 'TOGGLE.DISCUSSION'
   }
   internalEvents: {
+    '': {type: ''}
     'xstate.init': {type: 'xstate.init'}
+    'done.invoke.fetchDiscussionData': {
+      type: 'done.invoke.fetchDiscussionData'
+      data: unknown
+      __tip: 'See the XState TS docs to learn how to strongly type this.'
+    }
+    'error.platform.fetchDiscussionData': {type: 'error.platform.fetchDiscussionData'; data: unknown}
   }
-  invokeSrcNameMap: {}
+  invokeSrcNameMap: {
+    fetchPublicationData: 'done.invoke.publication-machine.fetching:invocation[0]'
+    fetchDiscussionData: 'done.invoke.fetchDiscussionData'
+  }
   missingImplementations: {
     actions: never
-    services: never
+    services: 'fetchPublicationData' | 'fetchDiscussionData'
     guards: never
     delays: never
   }
-  eventsCausingServices: {}
-  eventsCausingGuards: {}
+  eventsCausingServices: {
+    fetchPublicationData: 'PUBLICATION.FETCH.DATA'
+    fetchDiscussionData: '' | 'TOGGLE.DISCUSSION'
+  }
+  eventsCausingGuards: {
+    isDiscussionFetched: ''
+  }
   eventsCausingDelays: {}
   matchesStates:
     | 'idle'
@@ -35,10 +50,11 @@ export interface Typegen0 {
     | 'discussion.idle'
     | 'discussion.fetching'
     | 'discussion.ready'
+    | 'discussion.errored'
     | 'discussion.finish'
     | 'errored'
-    | {discussion?: 'idle' | 'fetching' | 'ready' | 'finish'}
-  tags: 'pending'
+    | {discussion?: 'idle' | 'fetching' | 'ready' | 'errored' | 'finish'}
+  tags: never
 }
 export interface Typegen1 {
   '@@xstate/typegen': true
