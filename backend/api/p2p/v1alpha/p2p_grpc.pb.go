0683392e931f67762fe88baa20c017b7bcb3bbad
Alexandr Burdiyan <burdiyan@gmail.com>
2022-05-11 17:53:49 +0200

    fixup! wip(backend): remove obsolete code
    Former-commit-id: ab34a540ab55ec4a99920ff13fbcad9c0b5ce76d

--- backend/api/p2p/v1alpha/p2p_grpc.pb.go
+++ backend/api/p2p/v1alpha/p2p_grpc.pb.go
@@ -7,7 +7,6 @@ import (
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
-	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
 // This is a compile-time assertion to ensure that this generated file
@@ -23,13 +22,6 @@ type P2PClient interface {
 	GetPeerInfo(ctx context.Context, in *GetPeerInfoRequest, opts ...grpc.CallOption) (*PeerInfo, error)
 	// Get version of a specific object.
 	GetObjectVersion(ctx context.Context, in *GetObjectVersionRequest, opts ...grpc.CallOption) (*Version, error)
-	// Exchange want lists upon initial connection.
-	ExchangeWantList(ctx context.Context, in *WantList, opts ...grpc.CallOption) (*WantList, error)
-	// Send delta updates of our own wants.
-	UpdateWantList(ctx context.Context, in *WantList, opts ...grpc.CallOption) (*UpdateWantsResponse, error)
-	// Notify remote peer about an updated object version.
-	// Only updated PeerVersions can be included if desired.
-	UpdateObjectVersion(ctx context.Context, in *Version, opts ...grpc.CallOption) (*emptypb.Empty, error)
 	// Request a peer to issue a lightning BOLT-11 invoice
 	RequestInvoice(ctx context.Context, in *RequestInvoiceRequest, opts ...grpc.CallOption) (*RequestInvoiceResponse, error)
 }
@@ -60,33 +52,6 @@ func (c *p2PClient) GetObjectVersion(ctx context.Context, in *GetObjectVersionRe
 	return out, nil
 }
 
-func (c *p2PClient) ExchangeWantList(ctx context.Context, in *WantList, opts ...grpc.CallOption) (*WantList, error) {
-	out := new(WantList)
-	err := c.cc.Invoke(ctx, "/com.mintter.p2p.v1alpha.P2P/ExchangeWantList", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *p2PClient) UpdateWantList(ctx context.Context, in *WantList, opts ...grpc.CallOption) (*UpdateWantsResponse, error) {
-	out := new(UpdateWantsResponse)
-	err := c.cc.Invoke(ctx, "/com.mintter.p2p.v1alpha.P2P/UpdateWantList", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *p2PClient) UpdateObjectVersion(ctx context.Context, in *Version, opts ...grpc.CallOption) (*emptypb.Empty, error) {
-	out := new(emptypb.Empty)
-	err := c.cc.Invoke(ctx, "/com.mintter.p2p.v1alpha.P2P/UpdateObjectVersion", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
 func (c *p2PClient) RequestInvoice(ctx context.Context, in *RequestInvoiceRequest, opts ...grpc.CallOption) (*RequestInvoiceResponse, error) {
 	out := new(RequestInvoiceResponse)
 	err := c.cc.Invoke(ctx, "/com.mintter.p2p.v1alpha.P2P/RequestInvoice", in, out, opts...)
@@ -104,13 +69,6 @@ type P2PServer interface {
 	GetPeerInfo(context.Context, *GetPeerInfoRequest) (*PeerInfo, error)
 	// Get version of a specific object.
 	GetObjectVersion(context.Context, *GetObjectVersionRequest) (*Version, error)
-	// Exchange want lists upon initial connection.
-	ExchangeWantList(context.Context, *WantList) (*WantList, error)
-	// Send delta updates of our own wants.
-	UpdateWantList(context.Context, *WantList) (*UpdateWantsResponse, error)
-	// Notify remote peer about an updated object version.
-	// Only updated PeerVersions can be included if desired.
-	UpdateObjectVersion(context.Context, *Version) (*emptypb.Empty, error)
 	// Request a peer to issue a lightning BOLT-11 invoice
 	RequestInvoice(context.Context, *RequestInvoiceRequest) (*RequestInvoiceResponse, error)
 }
@@ -124,15 +82,6 @@ func (UnimplementedP2PServer) GetPeerInfo(context.Context, *GetPeerInfoRequest) 
 }
 func (UnimplementedP2PServer) GetObjectVersion(context.Context, *GetObjectVersionRequest) (*Version, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetObjectVersion not implemented")
-}
-func (UnimplementedP2PServer) ExchangeWantList(context.Context, *WantList) (*WantList, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method ExchangeWantList not implemented")
-}
-func (UnimplementedP2PServer) UpdateWantList(context.Context, *WantList) (*UpdateWantsResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UpdateWantList not implemented")
-}
-func (UnimplementedP2PServer) UpdateObjectVersion(context.Context, *Version) (*emptypb.Empty, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UpdateObjectVersion not implemented")
 }
 func (UnimplementedP2PServer) RequestInvoice(context.Context, *RequestInvoiceRequest) (*RequestInvoiceResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method RequestInvoice not implemented")
@@ -185,60 +134,6 @@ func _P2P_GetObjectVersion_Handler(srv interface{}, ctx context.Context, dec fun
 	return interceptor(ctx, in, info, handler)
 }
 
-func _P2P_ExchangeWantList_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(WantList)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(P2PServer).ExchangeWantList(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/com.mintter.p2p.v1alpha.P2P/ExchangeWantList",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(P2PServer).ExchangeWantList(ctx, req.(*WantList))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _P2P_UpdateWantList_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(WantList)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(P2PServer).UpdateWantList(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/com.mintter.p2p.v1alpha.P2P/UpdateWantList",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(P2PServer).UpdateWantList(ctx, req.(*WantList))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _P2P_UpdateObjectVersion_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(Version)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(P2PServer).UpdateObjectVersion(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/com.mintter.p2p.v1alpha.P2P/UpdateObjectVersion",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(P2PServer).UpdateObjectVersion(ctx, req.(*Version))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
 func _P2P_RequestInvoice_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(RequestInvoiceRequest)
 	if err := dec(in); err != nil {
@@ -271,18 +166,6 @@ var P2P_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetObjectVersion",
 			Handler:    _P2P_GetObjectVersion_Handler,
-		},
-		{
-			MethodName: "ExchangeWantList",
-			Handler:    _P2P_ExchangeWantList_Handler,
-		},
-		{
-			MethodName: "UpdateWantList",
-			Handler:    _P2P_UpdateWantList_Handler,
-		},
-		{
-			MethodName: "UpdateObjectVersion",
-			Handler:    _P2P_UpdateObjectVersion_Handler,
 		},
 		{
 			MethodName: "RequestInvoice",
