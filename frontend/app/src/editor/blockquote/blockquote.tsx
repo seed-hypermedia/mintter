0267f0584bc7363e66162d03dab754962990b1a6
Jonas Kruckenberg <iterpre@protonmail.com>
2022-06-20 14:54:52 +0200

    fix: replace stateful changeService for stateless interface (#300)
    Former-commit-id: 3a2bd822d5353d61001e25cecdcaa469199325c3

--- frontend/app/src/editor/blockquote/blockquote.tsx
+++ frontend/app/src/editor/blockquote/blockquote.tsx
@@ -1,5 +1,4 @@
 import {BlockWrapper} from '@app/editor/block-wrapper'
-import {changesService} from '@app/editor/mintter-changes/plugin'
 import {EditorMode} from '@app/editor/plugin-utils'
 import {css} from '@app/stitches.config'
 import {Box} from '@components/box'
@@ -63,8 +62,6 @@ export const createBlockquotePlugin = (): EditorPlugin => ({
             })
             Transforms.select(editor, Path.next(quotePath))
             Transforms.collapse(editor, {edge: 'start'})
-            changesService.addChange(['moveBlock', newBlock.id])
-            changesService.addChange(['replaceBlock', newBlock.id])
           })
         }
       }
