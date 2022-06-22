6d2c76fcfbf488ee638efc47171a781dc7502209
Jonas Kruckenberg <iterpre@protonmail.com>
2022-06-22 10:07:47 +0200

    update tauri cli
    Former-commit-id: 6b48e5ce596f0eb45a4b90fdaab588b22ccf5e8d

--- build/nix/tauri-cli/default.nix
+++ build/nix/tauri-cli/default.nix
@@ -11,12 +11,12 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "tauri-cli";
-  version = "1.0.0-rc.12";
+  version = "1.0.0";
   src = fetchCrate {
     crateName = pname;
     version = version;
     # To update the hash run nix-prefetch fetchCrate --crateName tauri-cli --version <desired-version>.
-    sha256 = "sha256-GSx1V9xNBxwBVV81kTpvpy9NHm4+g8bsPD8ogc1Il4o=";
+    sha256 = "sha256-h2fgira8pkEn68Er4oNs+CD5tEu8GuYfMeHGa531l+A=";
   };
   cargoLock = {
     lockFile = "${src}/Cargo.lock";
