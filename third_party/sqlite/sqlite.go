88968164a5e20b1d804183a80b197d3dea40ab70
Alexandr Burdiyan <burdiyan@gmail.com>
2022-06-24 16:32:05 +0200

    chore(backend/sqlite): remove transitive closure extension
    Closes: https://www.notion.so/mintter/Get-rid-of-SQLite-transitive-closure-extension-881f847279d0417f8f64aa316f9feff9

--- third_party/sqlite/sqlite.go
+++ third_party/sqlite/sqlite.go
@@ -46,7 +46,6 @@ package sqlite
 // #include <stdlib.h>
 // #include <string.h>
 // #include "wrappers.h"
-// #include "closure.h"
 //
 // // Use a helper function here to avoid the cgo pointer detection
 // // logic treating SQLITE_TRANSIENT as a Go pointer.
@@ -63,9 +62,8 @@ package sqlite
 //   return sqlite3_db_config(db, op, onoff, NULL);
 // }
 //
-// static void load_extensions() {
-//   sqlite3_auto_extension((void(*)(void))sqlite3_closure_init);
-// }
+// // Insert desired loadable SQLite extensions here.
+// static void load_extensions() {}
 import "C"
 import (
 	"bytes"
