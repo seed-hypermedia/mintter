35a5da35347b804ffd0fb4ae5734fc7849468e31
Alexandr Burdiyan <burdiyan@gmail.com>
2022-07-11 16:14:02 +0200

    docs(backend): fix some comments [skip ci]
    Former-commit-id: e7de2f90fca013a1f52dbc9ce3c667d704d7bd47

--- backend/config/config.go
+++ backend/config/config.go
@@ -19,7 +19,6 @@ type LetsEncrypt struct {
 
 // P2P configuration.
 type P2P struct {
-	// TODO: make this a slice and add ip6 and quic addresses.
 	Port              int  `help:"Port to listen for incoming P2P connections" default:"55000"`
 	NoRelay           bool `help:"Disable libp2p circuit relay"`
 	NoBootstrap       bool `help:"Disable IPFS bootstrapping"`
