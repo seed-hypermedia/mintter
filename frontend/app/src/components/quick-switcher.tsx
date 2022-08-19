ac30f02718f7798f97192dbefe86f84e6c009535
Jonas Kruckenberg <iterpre@protonmail.com>
2022-08-19 11:03:02 +0200

    refactor(quick-switcher): replace hard coded vars with theme vars

--- frontend/app/src/components/quick-switcher.tsx
+++ frontend/app/src/components/quick-switcher.tsx
@@ -1,5 +1,6 @@
 import {mainService as defaultMainService} from '@app/app-providers'
 import {MINTTER_LINK_PREFIX} from '@app/constants'
+import {listen} from '@tauri-apps/api/event'
 import {useActor} from '@xstate/react'
 import {Command} from 'cmdk'
 import {useEffect, useState} from 'react'
@@ -27,83 +28,89 @@ export function QuickSwitcher({
       }
     }
 
+    let unlisten: () => void | undefined
+    listen('open_quick_switcher', () => {
+      setOpen(true)
+    }).then((f) => (unlisten = f))
+
     document.addEventListener('keydown', down)
-    return () => document.removeEventListener('keydown', down)
+    return () => {
+      document.removeEventListener('keydown', down)
+      unlisten?.()
+    }
   }, [])
 
   return (
-    <div className="vercel">
-      <Command.Dialog open={open} onOpenChange={setOpen} label="Quick Switcher">
-        <Command.Input value={search} onValueChange={setSearch} />
-        <Command.List>
-          <Command.Empty>No results found.</Command.Empty>
+    <Command.Dialog open={open} onOpenChange={setOpen} label="Quick Switcher">
+      <Command.Input value={search} onValueChange={setSearch} />
+      <Command.List>
+        <Command.Empty>No results found.</Command.Empty>
 
-          {isMintterLink(search) && (
-            <Command.Item
-              key="mtt-link"
-              value={search}
-              onSelect={() => {
-                const [docId, version, blockId] = getIdsfromUrl(search)
+        {isMintterLink(search) && (
+          <Command.Item
+            key="mtt-link"
+            value={search}
+            onSelect={() => {
+              const [docId, version, blockId] = getIdsfromUrl(search)
 
-                mainService.send({
-                  type: 'GO.TO.PUBLICATION',
-                  docId,
-                  version,
-                  blockId,
-                })
+              mainService.send({
+                type: 'GO.TO.PUBLICATION',
+                docId,
+                version,
+                blockId,
+              })
 
-                setOpen(false)
-              }}
-            >
-              Jump to {search}
-            </Command.Item>
-          )}
+              setOpen(false)
+            }}
+          >
+            Jump to {search}
+          </Command.Item>
+        )}
 
-          <Command.Group heading="Drafts">
-            {drafts.map((draft) => {
-              return (
-                <Command.Item
-                  key={draft.id}
-                  value={draft.title}
-                  onSelect={() => {
-                    setOpen(false)
+        <Command.Group heading="Drafts">
+          {drafts.map((draft) => {
+            return (
+              <Command.Item
+                key={draft.id}
+                value={draft.title}
+                onSelect={() => {
+                  setOpen(false)
 
-                    mainService.send({
-                      type: 'GO.TO.DRAFT',
-                      docId: draft.id,
-                    })
-                  }}
-                >
-                  {draft.title}
-                </Command.Item>
-              )
-            })}
-          </Command.Group>
+                  mainService.send({
+                    type: 'GO.TO.DRAFT',
+                    docId: draft.id,
+                  })
+                }}
+              >
+                {draft.title}
+              </Command.Item>
+            )
+          })}
+        </Command.Group>
 
-          <Command.Group heading="Publications">
-            {publications.map((publication) => {
-              return (
-                <Command.Item
-                  key={publication.document!.id}
-                  value={publication.document!.title}
-                  onSelect={() => {
-                    setOpen(false)
+        <Command.Group heading="Publications">
+          {publications.map((publication) => {
+            return (
+              <Command.Item
+                key={publication.document!.id}
+                value={publication.document!.title}
+                onSelect={() => {
+                  setOpen(false)
 
-                    mainService.send({
-                      type: 'GO.TO.PUBLICATION',
-                      docId: publication.document!.id,
-                      version: publication.version,
-                    })
-                  }}
-                >
-                  {publication.document?.title}
-                </Command.Item>
-              )
-            })}
-          </Command.Group>
-        </Command.List>
-      </Command.Dialog>
-    </div>
+                  mainService.send({
+                    type: 'GO.TO.PUBLICATION',
+                    docId: publication.document!.id,
+                    version: publication.version,
+                  })
+                }}
+              >
+                {publication.document?.title}
+              </Command.Item>
+            )
+          })}
+        </Command.Group>
+      </Command.List>
+    </Command.Dialog>
   )
 }
 
