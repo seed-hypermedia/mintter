67d1059343da164e5c898e92e2e0d9d61c305b31
Jonas Kruckenberg <iterpre@protonmail.com>
2022-09-23 11:21:10 +0200

    fix: make strikethrough render correctly (#751)
    Former-commit-id: 75e8aaee65634cacbda79b2b941431e8d6900eb6

--- frontend/app/src/editor/strikethrough/strikethrough.tsx
+++ frontend/app/src/editor/strikethrough/strikethrough.tsx
@@ -3,7 +3,7 @@ import type {EditorPlugin} from '../types'
 
 export const MARK_STRIKETHROUGH = 'strikethrough'
 
-export const Strikethrough = styled('span', {})
+export const Strikethrough = styled('s', {})
 
 export const createStrikethroughPlugin = (): EditorPlugin => ({
   name: MARK_STRIKETHROUGH,
