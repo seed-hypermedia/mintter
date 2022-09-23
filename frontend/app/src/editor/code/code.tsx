82bfc94df4412525754246a7f0bd7ecea1fd4f1b
Jonas Kruckenberg <iterpre@protonmail.com>
2022-09-23 16:09:27 +0200

    Fix: code plugin hook ordering issue (#759)
    * fix: codeblock theme hook ordering
    
    * fix type issues
    
    Former-commit-id: 7e4c4e37a58ff35f8cde1c77a31dbc874c5dd336

--- frontend/app/src/editor/code/code.tsx
+++ frontend/app/src/editor/code/code.tsx
@@ -14,6 +14,7 @@ import {
 import {styled} from '@app/stitches.config'
 import {useCurrentTheme} from '@app/theme'
 import {Box} from '@components/box'
+import {useEffect} from 'react'
 import {
   BUNDLED_LANGUAGES,
   getHighlighter,
@@ -33,6 +34,11 @@ import {findPath, lowerPoint, resetFlowContent} from '../utils'
 export const ELEMENT_CODE = 'code'
 const LEAF_TOKEN = 'codeToken'
 const HIGHLIGHTER = Symbol('shiki highlighter')
+// TODO make this user configurable in the future
+const THEMES = {
+  light: 'github-light',
+  dark: 'github-dark',
+}
 
 const SelectorWrapper = styled('div', {
   boxSizing: 'border-box',
@@ -46,7 +52,6 @@ const SelectorWrapper = styled('div', {
 })
 
 export const createCodePlugin = (): EditorPlugin => {
-  // const {theme = 'github-dark'} = props
   setCDN('/shiki/')
 
   return {
@@ -119,17 +124,10 @@ export const createCodePlugin = (): EditorPlugin => {
       ([node, path]) => {
         const ranges: Array<Range> = []
 
-        // TODO make this user configurable in the future
-        const themes = {
-          light: 'github-light',
-          dark: 'github-dark',
-        }
-        const theme = themes[useCurrentTheme()]
-
         // if the codeblock has a lang attribute but no highlighter yet, attach one
         if (isCode(node) && !node.data?.[HIGHLIGHTER] && node.lang) {
           getHighlighter({
-            themes: Object.values(themes),
+            themes: Object.values(THEMES),
             langs: [node.lang],
           }).then((highlighter) => {
             Transforms.setNodes(
@@ -148,13 +146,13 @@ export const createCodePlugin = (): EditorPlugin => {
               match: isCode,
             }) || []
 
-          if (!code || !code.data?.[HIGHLIGHTER]) return []
+          if (!code || !code.data?.[HIGHLIGHTER] || !code.data?.theme) return []
 
           const string = Node.string(node)
 
           const lines = (
             code.data?.[HIGHLIGHTER] as Highlighter
-          ).codeToThemedTokens(string, code.lang, theme, {
+          ).codeToThemedTokens(string, code.lang, code.data.theme as string, {
             includeExplanation: false,
           })
 
@@ -220,6 +218,20 @@ function Code({
       {at: path},
     )
   }
+
+  const theme = useCurrentTheme()
+
+  useEffect(() => {
+    const codeTheme = THEMES[theme]
+    const {...newData} = (element as CodeType).data || {}
+    delete newData[HIGHLIGHTER]
+
+    Transforms.setNodes(
+      editor,
+      {data: {...newData, theme: codeTheme}},
+      {at: path},
+    )
+  }, [theme, editor])
 
   if (mode == EditorMode.Embed || mode == EditorMode.Mention) {
     return (
