c9a2c2809f8d9825ea6e920bf0736f1fe7afb273
Jonas Kruckenberg <iterpre@protonmail.com>
2022-10-06 18:04:42 +0200

    Feat: add sentry crash reporting (#852)
    * add sentry crash reporting to rust and JS
    
    * update lockfile
    
    * update sentry plugin
    
    * pass sentry settings to dameon
    
    * Update entry-client.tsx
    
    * refactor onboarding icons
    
    * add crash reporting notice to onbaording
    
    * Update pnpm-lock.yaml

--- frontend/app/src/pages/onboarding/common.tsx
+++ frontend/app/src/pages/onboarding/common.tsx
@@ -173,58 +173,7 @@ export function OnboardingStepButton(
   return <Button type="button" shape="pill" size="3" {...props} />
 }
 
-export function SecurityPackIcon() {
-  return (
-    <IconContainer>
-      <svg
-        width="30"
-        height="36"
-        viewBox="0 0 30 36"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M15 34.6666C15 34.6666 28.3334 28 28.3334 18V6.33331L15 1.33331L1.66669 6.33331V18C1.66669 28 15 34.6666 15 34.6666Z"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    </IconContainer>
-  )
-}
-
-export function ProfileInformationIcon() {
-  return (
-    <IconContainer>
-      <svg
-        width="40"
-        height="40"
-        viewBox="0 0 40 40"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M33.3334 34V30.6667C33.3334 28.8986 32.631 27.2029 31.3807 25.9526C30.1305 24.7024 28.4348 24 26.6667 24H13.3334C11.5652 24 9.86955 24.7024 8.61931 25.9526C7.36907 27.2029 6.66669 28.8986 6.66669 30.6667V34"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M20 17.3333C23.6819 17.3333 26.6666 14.3486 26.6666 10.6667C26.6666 6.98477 23.6819 4 20 4C16.3181 4 13.3333 6.98477 13.3333 10.6667C13.3333 14.3486 16.3181 17.3333 20 17.3333Z"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    </IconContainer>
-  )
-}
-
-function IconContainer({children}: PropsWithChildren<unknown>) {
+export function IconContainer({children}: PropsWithChildren<unknown>) {
   return (
     <Box
       css={{
