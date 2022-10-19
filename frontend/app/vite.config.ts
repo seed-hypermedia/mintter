73228292149dea5efb44f928011f493872bbcf41
Jonas Kruckenberg <iterpre@protonmail.com>
2022-10-19 12:08:14 +0200

    fix conditional SCSS includes and provide convenience mixins
    Former-commit-id: b7d40aeaf8ec23695103503395143a17b6146115

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -1,8 +1,21 @@
 import react from '@vitejs/plugin-react'
+import {writeFileSync} from 'fs'
+import path from 'path'
 import {defineConfig} from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 let isTest = process.env.NODE_ENV == 'test'
+
+writeFileSync(
+  path.resolve(__dirname, './src/styles/_env.scss'),
+  `$TAURI_PLATFORM: '${process.env.TAURI_PLATFORM}';
+$TAURI_ARCH: '${process.env.TAURI_ARCH}';
+$TAURI_FAMILY: '${process.env.TAURI_FAMILY}';
+$TAURI_PLATFORM_VERSION: '${process.env.TAURI_PLATFORM_VERSION}';
+$TAURI_PLATFORM_TYPE: '${process.env.TAURI_PLATFORM_TYPE}';
+$TAURI_DEBUG: '${process.env.TAURI_DEBUG}';
+`,
+)
 
 export default defineConfig({
   cacheDir: '.vite',
@@ -22,26 +35,8 @@ export default defineConfig({
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_DEBUG,
   },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `$TAURI_PLATFORM: ${process.env.TAURI_PLATFORM};
-        $TAURI_ARCH: ${process.env.TAURI_ARCH};
-        $TAURI_FAMILY: ${process.env.TAURI_FAMILY};
-        $TAURI_PLATFORM_VERSION: ${process.env.TAURI_PLATFORM_VERSION};
-        $TAURI_PLATFORM_TYPE: ${process.env.TAURI_PLATFORM_TYPE};
-        $TAURI_DEBUG: ${process.env.TAURI_DEBUG};`,
-      },
-    },
-  },
   plugins: [
     tsconfigPaths(),
-    // checker({
-    //   typescript: true,
-    //   overlay: {
-    //     position: 'br',
-    //   },
-    // }),
     react({
       fastRefresh: !isTest,
       jsxRuntime: isTest ? 'classic' : 'automatic',
