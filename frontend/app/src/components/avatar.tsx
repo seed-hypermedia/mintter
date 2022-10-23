f483e2d198e2394e0f1ba167984fc7e561969b34
Horacio Herrera <hi@horacioh.com>
2022-10-24 00:29:58 +0200

    fix theme colors in dark mode

--- frontend/app/src/components/avatar.tsx
+++ frontend/app/src/components/avatar.tsx
@@ -1,11 +1,41 @@
+import {useQuery} from '@tanstack/react-query'
 import {useMemo} from 'react'
 import '../styles/avatar.scss'
 
-export function Avatar({alias, size = 2}: {alias: string; size: 1 | 2 | 3}) {
+export function Avatar({
+  accountId,
+  alias,
+  size = 2,
+}: {
+  accountId?: string
+  alias: string
+  size: 1 | 2 | 3
+}) {
+  let {data: color} = useQuery({
+    queryKey: ['avatarColor', accountId],
+    queryFn: () => getRandomColor(accountId),
+    enabled: !!accountId,
+    retry: false,
+    staleTime: Infinity,
+  })
   let initials = useMemo(() => alias[0], [alias])
   return (
-    <div className="avatar-circle" data-size={size}>
+    <div
+      className="avatar-circle"
+      style={{backgroundColor: color}}
+      data-size={size}
+    >
       <span className="initials">{initials}</span>
     </div>
   )
+}
+
+function getRandomColor(id: string) {
+  let hash = 0
+  for (let i = 0; i < id.length; i++) {
+    hash = id.charCodeAt(i) + ((hash << 6) - hash)
+    hash = hash & hash // Convert to 32bit integer
+  }
+  const shortened = hash % 360
+  return 'hsl(' + shortened + ',50%,50%)'
 }
