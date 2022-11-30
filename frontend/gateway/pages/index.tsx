d7be5e9f280c6473e2a2243d774a88a33da1371e
Horacio Herrera <hi@horacioh.com>
2022-11-30 17:32:20 +0100

    feat(frontend): base setup for the gateway
    - add nextjs to the /frontend/gateway folder
    - add sass
    - add `./dev run-gateway` script to spawn the nextjs dev server

---
+++ frontend/gateway/pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Mintter Wateway</h1>
+    </div>
+  )
+}
