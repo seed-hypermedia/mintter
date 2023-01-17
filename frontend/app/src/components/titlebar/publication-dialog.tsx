3ca9b97b2777978b91998a34fc6e5e34266cabd0
Eric Vicenti <eric@vicenti.net>
2023-01-17 15:50:26 -0800

    Publication dialog improvements

--- frontend/app/src/components/titlebar/publication-dialog.tsx
+++ frontend/app/src/components/titlebar/publication-dialog.tsx
@@ -1,9 +1,10 @@
+import {useMain} from '@app/main-context'
 import {styled} from '@app/stitches.config'
 import {Button} from '@components/button'
 import {dialogContentStyles, overlayStyles} from '@components/dialog-styles'
 import {TextField} from '@components/text-field'
 import * as DialogPrimitive from '@radix-ui/react-dialog'
-import {useState} from 'react'
+import {useMemo, useState} from 'react'
 
 const StyledOverlay = styled(DialogPrimitive.Overlay, overlayStyles)
 const StyledContent = styled(DialogPrimitive.Content, dialogContentStyles)
@@ -14,10 +15,58 @@ function writePathState(s: string) {
 function readPathState(s: string) {
   return s.replace(/_+$/, '')
 }
+const Heading = styled('h2', {
+  margin: 0,
+  fontSize: '$4',
+})
+function PublishDialogForm({
+  siteId,
+  docId,
+  onDone,
+}: {
+  siteId: string
+  docId?: string
+  onDone?: () => void
+}) {
+  const main = useMain()
+  const init = useMemo(() => {
+    const doc = main.getSnapshot().context.current
+    const docState = doc?.getSnapshot()
+    const title = docState?.context.title
+    const path = title ? writePathState(title) : 'untitled'
 
+    return {path, docId: docState?.context.documentId}
+  }, [])
+  const [path, setPath] = useState<string>(init.path)
+
+  return (
+    <>
+      <Heading>Publish to {siteId}</Heading>
+      <TextField
+        placeholder={'Unlisted Document'}
+        id="pretty-path"
+        name="pretty-path"
+        label="Public URL (/Path)"
+        value={path}
+        onChange={(e) => {
+          setPath(writePathState(e.target.value))
+        }}
+      />
+      <URLPreview>
+        https://{siteId}/{path === '' ? `p/${init.docId}` : readPathState(path)}
+      </URLPreview>
+      <Button
+        onClick={() => {
+          onDone?.()
+        }}
+      >
+        Publish Document
+      </Button>
+    </>
+  )
+}
 export function usePublicationDialog(docId?: string) {
   const [openSiteId, setOpenSiteId] = useState<null | string>(null)
-  const [path, setPath] = useState<string>('')
   function open(siteId: string) {
     setOpenSiteId(siteId)
   }
@@ -32,29 +81,15 @@ export function usePublicationDialog(docId?: string) {
         <DialogPrimitive.Portal>
           <StyledOverlay />
           <StyledContent>
-            <DialogPrimitive.Title>
-              Publish to {openSiteId}
-            </DialogPrimitive.Title>
-
-            <TextField
-              id="pretty-path"
-              name="pretty-path"
-              label="Public /URL (Path)"
-              value={path}
-              onChange={(e) => {
-                setPath(writePathState(e.target.value))
-              }}
-            />
-            <URLPreview>
-              https://{openSiteId}/{readPathState(path)}
-            </URLPreview>
-            <Button
-              onClick={() => {
-                setOpenSiteId(null)
-              }}
-            >
-              Publish Document
-            </Button>
+            {openSiteId && (
+              <PublishDialogForm
+                docId={docId}
+                siteId={openSiteId}
+                onDone={() => {
+                  setOpenSiteId(null)
+                }}
+              />
+            )}
           </StyledContent>
         </DialogPrimitive.Portal>
       </DialogPrimitive.Root>
