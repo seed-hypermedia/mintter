3acfcf58df59f2f71bd373d631a1ec6570b12478
Horacio Herrera <hi@horacioh.com>
2023-01-17 15:43:07 +0100

    frontend(packages): create a shared package

---
+++ frontend/shared/src/client/index.ts
@@ -0,0 +1,82 @@
+export type {
+  Account,
+  Account_DevicesEntry,
+  Device,
+  GetAccountRequest,
+  ListAccountsRequest,
+  ListAccountsResponse,
+  Profile,
+} from './.generated/accounts/v1alpha/accounts'
+export type {
+  GenMnemonicRequest,
+  GenMnemonicResponse,
+  GetInfoRequest,
+  Info,
+  RegisterRequest,
+  RegisterResponse,
+} from './.generated/daemon/v1alpha/daemon'
+export {
+  Annotation,
+  Block,
+  BlockNode,
+  CreateDraftRequest,
+  DeleteDraftRequest,
+  DeletePublicationRequest,
+  DocumentChange,
+  GetDraftRequest,
+  GetPublicationRequest,
+  Link as MttLink,
+  LinkNode,
+  ListDraftsRequest,
+  ListDraftsResponse,
+  ListPublicationsRequest,
+  ListPublicationsResponse,
+  PublishDraftRequest,
+  UpdateDraftRequestV2,
+  ListCitationsResponse,
+} from './.generated/documents/v1alpha/documents'
+export {ConnectionStatus} from './.generated/networking/v1alpha/networking'
+export type {
+  ConnectRequest,
+  ConnectResponse,
+  GetPeerInfoRequest,
+  PeerInfo,
+} from './.generated/networking/v1alpha/networking'
+export * from './.generated/types'
+export {
+  generateMnemonic,
+  getAccount,
+  listAccounts,
+  registerAccount,
+  updateProfile,
+} from './accounts'
+export {getInfo, forceSync} from './daemon'
+export {
+  createDraft,
+  deleteDraft,
+  getDraft,
+  listDrafts,
+  publishDraft,
+  updateDraftV2,
+} from './drafts'
+export {createGrpcClient, MINTTER_API_URL_DEFAULT} from './grpc-client'
+export type {GrpcClient} from './grpc-client'
+export {connect, getPeerInfo, listPeerAddrs} from './networking'
+export {
+  deletePublication,
+  getPublication,
+  listCitations,
+  listPublications,
+} from './publications'
+import {
+  Document,
+  Publication as APIPublication,
+} from './.generated/documents/v1alpha/documents'
+export type Publication = APIPublication & {
+  document?: Document
+}
+export {Document}
+export * from './block-to-slate'
+export * from './block-to-api'
+export * from './client'
+export * from './change-creators'
---
+++ frontend/shared/src/hast-mtt/handlers/figure.ts
@@ -0,0 +1,5 @@
+// export function figure(h: H, node: HastNode) {
+
+export function figure() {
+  return
+}
