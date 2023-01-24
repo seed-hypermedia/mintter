ee5a5adf3d4be2bdaaef16f2c780fb84ee776815
Alexandr Burdiyan <burdiyan@gmail.com>
2023-01-24 23:49:35 +0100

    build(nix): update nixpgs, Go, and Node versions

--- build/nix/overlay.nix
+++ build/nix/overlay.nix
@@ -1,19 +1,19 @@
 self: super: {
-  go = super.go_1_18;
-  nodejs = super.nodejs-18_x;
+  go = super.go_1_19;
+  nodejs = super.nodejs-19_x;
   bazel-wrapper = super.callPackage ./bazel-wrapper {};
   impure-cc = super.callPackage ./impure-cc {};
   golangci-lint = super.golangci-lint.override {
-    buildGoModule = self.buildGo117Module;
+    buildGoModule = self.buildGo119Module;
   };
   mkShell = super.mkShell.override {
     stdenv = super.stdenvNoCC;
   };
   please = super.callPackage ./please {
-    buildGoModule = self.buildGo117Module;
+    buildGoModule = self.buildGo119Module;
   };
   robo = super.callPackage ./robo {
-    buildGoModule = self.buildGo117Module;
+    buildGoModule = self.buildGo119Module;
   };
   mkLazyWrapper = super.callPackage ./mk-lazy-wrapper {};
   rust-stable = (super.rust-bin.stable.latest.default.overrideAttrs (oldAttrs: {
