d485eae4ab0e08bf843f75dacaa46f61ba94a88c
Horacio Herrera <hi@horacioh.com>
2023-01-25 12:58:05 +0100

    build working, but app broken

--- frontend/gateway/pages/index.tsx
+++ frontend/gateway/pages/index.tsx
@@ -1,12 +1,31 @@
 import {PublicationPage} from '../publication-page'
+import {createChannel, createClient} from 'nice-grpc'
+import {PublicationsClient, PublicationsDefinition} from '@mintter/shared'
 
-export default function IndexPage({document}: any) {
+export default function IndexPage({publication}: any) {
+  console.log('🚀 ~ file: index.tsx:6 ~ IndexPage ~ publication', publication)
+
   return (
     <PublicationPage
       documentId="bafy2bzaceb5cnsnhyrwfx22xqixh4qmphj6k6a3gqxpfu6s7euk5bovurmrhk"
       onlyContent
     />
   )
+}
+
+export async function getServerSideProps() {
+  const ch = createChannel('http://localhost:55002')
+  let client: PublicationsClient = createClient(PublicationsDefinition, ch)
+
+  let publication = await client.getPublication({
+    documentId:
+      'bafy2bzaceckvlm2pgjzasei2x3lort3enboc4inophpvqikoyfxdf74vusbcm',
+  })
+  return {
+    props: {
+      publication,
+    },
+  }
 }
 
 // mintter://bafy2bzaceb5cnsnhyrwfx22xqixh4qmphj6k6a3gqxpfu6s7euk5bovurmrhk/baeaxdiheaiqpazrdmjji6xifvr5nuky2ibn6fic3ktayrycudsasfephqprna5a
--- frontend/shared/src/client/index.ts
+++ frontend/shared/src/client/index.ts
@@ -34,7 +34,9 @@ export {
   PublishDraftRequest,
   UpdateDraftRequestV2,
   ListCitationsResponse,
+  PublicationsDefinition,
 } from './.generated/documents/v1alpha/documents'
+export type {PublicationsClient} from './.generated/documents/v1alpha/documents'
 export {ConnectionStatus} from './.generated/networking/v1alpha/networking'
 export type {
   ConnectRequest,
@@ -43,31 +45,8 @@ export type {
   PeerInfo,
 } from './.generated/networking/v1alpha/networking'
 export * from './.generated/types'
-export {
-  generateMnemonic,
-  getAccount,
-  listAccounts,
-  registerAccount,
-  updateProfile,
-} from './accounts'
-export {getInfo, forceSync} from './daemon'
-export {
-  createDraft,
-  deleteDraft,
-  getDraft,
-  listDrafts,
-  publishDraft,
-  updateDraftV2,
-} from './drafts'
 export {createGrpcClient, MINTTER_API_URL_DEFAULT} from './grpc-client'
 export type {GrpcClient} from './grpc-client'
-export {connect, getPeerInfo, listPeerAddrs} from './networking'
-export {
-  deletePublication,
-  getPublication,
-  listCitations,
-  listPublications,
-} from './publications'
 import {
   Document,
   Publication as APIPublication,
