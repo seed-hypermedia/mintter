72fd262974f37a40a1c5bd19b5f7495c1acb27a2
Horacio Herrera <hi@horacioh.com>
2023-01-27 17:56:13 +0100

    fix(gateway): fix vercel builds

--- frontend/gateway/pages/index.tsx
+++ frontend/gateway/pages/index.tsx
@@ -1,3 +1,4 @@
+import {getPublication} from '@mintter/shared'
 import {PublicationPage} from '../publication-page'
 
 export default function IndexPage({document}: any) {
@@ -10,3 +11,9 @@ export default function IndexPage({document}: any) {
 }
 
 // mintter://bafy2bzacebhlxpfnz2qju2zruckm2ueamioabjj532hidrlxgbgucr44tmewc/baeaxdiheaiqjuqjo4f34bu52tbzn3cfc4uu6m6imv2ildn4qkgl7jmnag7itcsq
+
+// export async function getServerSideProps() {
+//   return await getPublication(
+//     'bafy2bzacec67bkejkhjcfomg7o72vgdkwrto3ry72zcb72sfb4cphuvtgiuny',
+//   )
+// }
