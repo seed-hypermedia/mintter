e39988cda84cca8efc1c4b8b7cf8f84d0035cf74
juligasa <11684004+juligasa@users.noreply.github.com>
2023-02-06 17:57:42 +0100

    change default site name

--- backend/config/config.go
+++ backend/config/config.go
@@ -50,7 +50,7 @@ func Default() Config {
 		},
 		Site: Site{
 			InviteTokenExpirationDelay: time.Hour * 24 * 7,
-			Hostname:                   "mintter.com", // TODO(juligasa): Just a conf to eric. Remove after mockups
+			Hostname:                   "example.com", // TODO(juligasa): Just a conf to eric. Remove after mockups
 		},
 		Syncing: Syncing{
 			WarmupDuration: time.Minute,
