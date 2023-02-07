2fc19dce97385179edebc4fa4abea57defa5f21c
Eric Vicenti <eric@vicenti.net>
2023-02-07 09:17:05 -0800

    Log RPC from front end client

--- frontend/shared/src/client/client.ts
+++ frontend/shared/src/client/client.ts
@@ -1,5 +1,17 @@
-import {createGrpcWebTransport} from '@bufbuild/connect-web'
+import {createGrpcWebTransport, Interceptor} from '@bufbuild/connect-web'
+
+const loggingInterceptor: Interceptor = (next) => async (req) => {
+  try {
+    const result = await next(req)
+    console.log(`🔃 to ${req.method.name} `, req.message, result.message)
+    return result
+  } catch (e) {
+    console.error(`🚨 to ${req.method.name} `, e)
+    throw e
+  }
+}
 
 export const transport = createGrpcWebTransport({
   baseUrl: 'http://localhost:55001',
+  interceptors: import.meta.env.DEV ? [loggingInterceptor] : [],
 })
