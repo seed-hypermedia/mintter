7ec8400a9cd42c7a1d803d7b9ef5842a413efeb8
Eric Vicenti <eric@vicenti.net>
2023-02-13 11:58:36 -0800

    SSR web publish with pretty path

--- frontend/gateway/pages/p/[...ids].tsx
+++ frontend/gateway/pages/p/[...ids].tsx
@@ -1,54 +1,13 @@
-import {blockNodeToSlate, getPublication, Publication} from '@mintter/shared'
+import {getPublication, Publication} from '@mintter/shared'
 import {GetServerSideProps} from 'next'
-import {useRouter} from 'next/router'
-import {PublicationMetadata} from '../../author'
-import Footer from '../../footer'
-import {SiteHead} from '../../site-head'
-import {SlateReactPresentation} from '../../slate-react-presentation'
-import {useRenderElement} from '../../slate-react-presentation/render-element'
-import {useRenderLeaf} from '../../slate-react-presentation/render-leaf'
+import PublicationPage from '../../ssr-publication-page'
 
-export default function PublicationPageWrapper({
+export default function CIDPublicationPage({
   publication,
 }: {
   publication?: Publication
 }) {
-  const router = useRouter()
-  // const [docId, version] = router.query.ids || []
-  const renderElement = useRenderElement()
-  const renderLeaf = useRenderLeaf()
-  const blockChildren = publication?.document?.children
-  const slateChildren = blockChildren
-    ? blockNodeToSlate(blockChildren, 'group')
-    : undefined
-  return (
-    <>
-      <SiteHead />
-      <main
-        id="main-content"
-        tabIndex={-1}
-        className="main-content wrapper text-size-100"
-      >
-        <article className="sidebar">
-          <div>
-            <PublicationMetadata publication={publication} />
-          </div>
-          <div>
-            {slateChildren ? (
-              <SlateReactPresentation
-                value={[slateChildren]}
-                renderElement={renderElement}
-                renderLeaf={renderLeaf}
-              />
-            ) : (
-              <p>Empty document.</p>
-            )}
-          </div>
-        </article>
-      </main>
-      <Footer />
-    </>
-  )
+  return <PublicationPage publication={publication} />
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
