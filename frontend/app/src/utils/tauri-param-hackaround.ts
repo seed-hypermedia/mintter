e319ee4d1f9aa2858333c84d36e15ae799eeda7f
Eric Vicenti <eric@vicenti.net>
2023-02-13 11:58:36 -0800

    Work around tauri dots in params

---
+++ frontend/app/src/utils/tauri-param-hackaround.ts
@@ -0,0 +1,7 @@
+export function tauriEncodeParam(p: string): string {
+  return p.replaceAll('.', '%DOT%')
+}
+export function tauriDecodeParam(p?: string): string | undefined {
+  if (!p) return p
+  return p.replaceAll('%DOT%', '.')
+}
