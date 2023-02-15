7629933c9da233506d83b5cf209258aad39fa2ec
Horacio Herrera <hi@horacioh.com>
2023-02-15 14:37:08 +0100

    fix mintter.com website with hardcoded document

--- frontend/gateway/pages/index.tsx
+++ frontend/gateway/pages/index.tsx
@@ -1,9 +1,11 @@
-import {getLocalWebSiteClient} from '@mintter/shared'
-import {transport} from '../client'
-import {Publication} from '@mintter/shared'
+import {
+  getLocalWebSiteClient,
+  getPublication,
+  Publication,
+} from '@mintter/shared'
 import {GetServerSideProps} from 'next'
+import {transport} from '../client'
 import PublicationPage from '../ssr-publication-page'
-
 function DefaultHomePage() {
   return (
     <>
@@ -21,14 +23,14 @@ export default function HomePage({
   publication?: Publication | null
 }) {
   if (!publication) return <DefaultHomePage />
-  return <PublicationPage publication={publication} />
+  return <PublicationPage publication={publication} metadata={false} />
 }
 
 async function getHomePublication(): Promise<Publication | null> {
   const site = getLocalWebSiteClient(transport)
   try {
     const pathRecord = await site.getPath({path: '/'})
-    const publication = pathRecord.publication
+    const publication = pathRecord?.publication
     return publication || null
   } catch (e) {
     return null
@@ -36,7 +38,13 @@ async function getHomePublication(): Promise<Publication | null> {
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const publication = await getHomePublication()
+  // mintter://bafy2bzacedq36zy5yrhutg5pocnpv2lzxr6xwfs6eeng7saoe7syxkeiq3zsm/baeaxdiheaiqjrcwamuudzuc7vmzfnebn6xs45fcbgorb4vfkc44aehc3eevt25q
+  const publication = await getPublication(
+    'bafy2bzacedq36zy5yrhutg5pocnpv2lzxr6xwfs6eeng7saoe7syxkeiq3zsm',
+    'baeaxdiheaiqjrcwamuudzuc7vmzfnebn6xs45fcbgorb4vfkc44aehc3eevt25q',
+    // transport,
+  )
+  // const publication = await getHomePublication()
   return {
     props: {
       publication: publication?.toJson(),
