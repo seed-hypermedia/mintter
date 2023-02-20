5f69242fa5e95f058f762448a1cd07c65adfd5ee
juligasa <11684004+juligasa@users.noreply.github.com>
2023-02-20 19:54:24 +0100

    wip: site tests

--- backend/mttnet/site_test.go
+++ backend/mttnet/site_test.go
@@ -8,6 +8,7 @@ import (
 	siteproto "mintter/backend/genproto/documents/v1alpha"
 	"mintter/backend/pkg/future"
 	"mintter/backend/pkg/must"
+	"mintter/backend/testutil"
 	"mintter/backend/vcs/mttacc"
 	vcsdb "mintter/backend/vcs/sqlitevcs"
 	"testing"
@@ -28,7 +29,77 @@ func TestLocalPublish(t *testing.T) {
 	defer stopSite()
 }
 
-func TestGenerateToken(t *testing.T) {
+func TestProxying(t *testing.T) {
+	t.Skip("not ready yet")
+	t.Parallel()
+	ownerSrv, docSrv, stopowner := makeTestSrv(t, "alice")
+	owner, ok := ownerSrv.Node.Get()
+	require.True(t, ok)
+	defer stopowner()
+
+	cfg := config.Default()
+	cfg.Site.Hostname = "127.0.0.1:55001"
+
+	cfg.Site.OwnerID = owner.accountObjectID.String()
+	siteSrv, _, stopSite := makeTestSrv(t, "bob", cfg.Site)
+	site, ok := siteSrv.Node.Get()
+	require.True(t, ok)
+	defer stopSite()
+
+	docSrv.SetSiteAccount(site.accountObjectID.String())
+
+	ctx := context.Background()
+	tsFuture := time.Now().Add(48 * time.Hour).Unix()
+	_, err := ownerSrv.CreateInviteToken(ctx, &documents.CreateInviteTokenRequest{
+		Role:       documents.Member_EDITOR,
+		ExpireTime: &timestamppb.Timestamp{Seconds: tsFuture},
+	})
+	require.Error(t, err)
+
+	_, err = siteSrv.CreateInviteToken(ctx, &documents.CreateInviteTokenRequest{
+		Role:       documents.Member_EDITOR,
+		ExpireTime: &timestamppb.Timestamp{Seconds: tsFuture},
+	})
+	require.Error(t, err)
+
+	require.NoError(t, owner.Connect(ctx, site.AddrInfo()))
+	time.Sleep(5 * time.Second)
+	header := metadata.New(map[string]string{MttHeader: cfg.Site.Hostname})
+	ctx = metadata.NewIncomingContext(ctx, header) // Usually, the headers are written by the client in the outgoing context and server receives them in the incoming. But here we are writing the server directly
+	token, err := ownerSrv.CreateInviteToken(ctx, &documents.CreateInviteTokenRequest{
+		Role:       documents.Member_EDITOR,
+		ExpireTime: &timestamppb.Timestamp{Seconds: tsFuture},
+	})
+	require.NoError(t, err)
+	require.Equal(t, tsFuture, token.ExpireTime.Seconds)
+}
+
+func TestGetSiteInfo(t *testing.T) {
+	t.Parallel()
+	ownerSrv, docSrv, stopowner := makeTestSrv(t, "alice")
+	owner, ok := ownerSrv.Node.Get()
+	require.True(t, ok)
+	defer stopowner()
+
+	cfg := config.Default()
+	cfg.Site.Hostname = "127.0.0.1:55001"
+	cfg.Site.Title = "My title"
+	cfg.Site.OwnerID = owner.accountObjectID.String()
+	siteSrv, _, stopSite := makeTestSrv(t, "bob", cfg.Site)
+	site, ok := siteSrv.Node.Get()
+	require.True(t, ok)
+	defer stopSite()
+
+	docSrv.SetSiteAccount(site.accountObjectID.String())
+
+	ctx := context.Background()
+	siteInfo, err := siteSrv.GetSiteInfo(ctx, &documents.GetSiteInfoRequest{})
+	require.NoError(t, err)
+	require.Equal(t, cfg.Site.Hostname, siteInfo.Hostname)
+	require.Equal(t, cfg.Site.OwnerID, siteInfo.Owner)
+	require.Equal(t, cfg.Site.Title, siteInfo.Title)
+}
+func TestE2e(t *testing.T) {
 	t.Skip("not ready yet")
 	t.Parallel()
 	ownerSrv, docSrv, stopowner := makeTestSrv(t, "alice")
@@ -58,17 +129,6 @@ func TestGenerateToken(t *testing.T) {
 
 	ctx := context.Background()
 	tsFuture := time.Now().Add(48 * time.Hour).Unix()
-	_, err := ownerSrv.CreateInviteToken(ctx, &documents.CreateInviteTokenRequest{
-		Role:       documents.Member_EDITOR,
-		ExpireTime: &timestamppb.Timestamp{Seconds: tsFuture},
-	})
-	require.Error(t, err)
-
-	_, err = siteSrv.CreateInviteToken(ctx, &documents.CreateInviteTokenRequest{
-		Role:       documents.Member_EDITOR,
-		ExpireTime: &timestamppb.Timestamp{Seconds: tsFuture},
-	})
-	require.Error(t, err)
 
 	require.NoError(t, owner.Connect(ctx, site.AddrInfo()))
 	header := metadata.New(map[string]string{MttHeader: cfg.Site.Hostname})
@@ -124,21 +184,30 @@ func makeTestSrv(t *testing.T, name string, siteCfg ...config.Site) (*Server, *s
 	reg, err := mttacc.Register(context.Background(), u.Account, u.Device, conn)
 	release()
 	require.NoError(t, err)
+	cfg := config.Default()
 
-	cfg := config.Default().P2P
-	cfg.Port = 0
-	cfg.NoRelay = true
-	cfg.BootstrapPeers = nil
-	cfg.NoMetrics = true
+	require.LessOrEqual(t, len(siteCfg), 1)
+	if len(siteCfg) == 1 {
+		cfg.Site = siteCfg[0]
+	}
 
-	n, err := New(cfg, hvcs, reg, u.Identity, must.Do2(zap.NewDevelopment()).Named(name))
+	cfg.HTTPPort = 0
+	cfg.GRPCPort = 0
+	cfg.RepoPath = testutil.MakeRepoPath(t)
+	cfg.P2P.Port = 0
+	cfg.P2P.BootstrapPeers = nil
+	cfg.P2P.NoRelay = true
+	cfg.P2P.NoMetrics = true
+
+	n, err := New(cfg.P2P, hvcs, reg, u.Identity, must.Do2(zap.NewDevelopment()).Named(name))
 	require.NoError(t, err)
 
 	errc := make(chan error, 1)
 	ctx, cancel := context.WithCancel(context.Background())
 	f := future.New[*Node]()
 	docsSrv := newSimulatedDocs(&siteproto.Publication{}, "")
-	srv := NewServer(ctx, config.Default().Site, f.ReadOnly, docsSrv)
+
+	srv := NewServer(ctx, cfg.Site, f.ReadOnly, docsSrv)
 	require.NoError(t, f.Resolve(n))
 
 	go func() {
