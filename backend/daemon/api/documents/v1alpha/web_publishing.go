d485c994f27d93c0136ef3fe449d40258cccad6a
juligasa <11684004+juligasa@users.noreply.github.com>
2023-02-20 19:54:24 +0100

    wip: site tests

--- backend/daemon/api/documents/v1alpha/web_publishing.go
+++ backend/daemon/api/documents/v1alpha/web_publishing.go
@@ -104,7 +104,6 @@ func (api *Server) AddSite(ctx context.Context, in *documents.AddSiteRequest) (*
 	var role documents.Member_Role
 	// make it a proxy call since we want to talk with the site by attaching headers
 	header := metadata.New(map[string]string{mttnet.MttHeader: in.Hostname})
-	//ctx = metadata.AppendToIncomingContext(ctx, mttnet.MttHeader, in.Hostname)
 	ctx = metadata.NewIncomingContext(ctx, header) // Usually, the headers are written by the client in the outgoing context and server receives them in the incoming. But here we are writing the server directly
 	ctx = context.WithValue(ctx, mttnet.SiteAccountIDCtxKey, accountID)
 	if in.InviteToken != "" {
