01125f6eac065b7b2402f014b7fcb9a335f88827
Eric Vicenti <eric@vicenti.net>
2023-02-22 15:35:21 -0800

    gw vercel check and render client side pub

--- frontend/gateway/pages/index.tsx
+++ frontend/gateway/pages/index.tsx
@@ -11,17 +11,6 @@ import {SiteHead} from '../site-head'
 import PublicationPage from '../ssr-publication-page'
 
 function DefaultHomePage() {
-  return (
-    <>
-      <h1>Welcome.</h1>
-      <h2>
-        This is the default home page. You haven&apos;t published anything here.
-      </h2>
-    </>
-  )
-}
-
-export default function HomePage() {
   let {data} = useQuery({
     queryKey: [
       'pub',
@@ -53,37 +42,51 @@ export default function HomePage() {
   )
 }
 
-// export default function HomePage({
-//   publication,
-// }: {
-//   publication?: Publication | null
-// }) {
-//   if (!publication) return <DefaultHomePage />
-//   return <PublicationPage publication={publication} metadata={false} />
-// }
+export default function HomePage({
+  publication,
+}: {
+  publication?: Publication | null
+}) {
+  if (!publication) return <DefaultHomePage />
+  return <PublicationPage publication={publication} metadata={false} />
+}
 
-// async function getHomePublication(): Promise<Publication | null> {
-//   const site = getLocalWebSiteClient(transport)
-//   try {
-//     const pathRecord = await site.getPath({path: '/'})
-//     const publication = pathRecord?.publication
-//     return publication || null
-//   } catch (e) {
-//     return null
-//   }
-// }
+async function getHomePublication(): Promise<Publication | null> {
+  if (!process.env.GW_NEXT_HOST) {
+    // Temp Mintter home screen document:
+    return await getPublication(
+      'bafy2bzacedq36zy5yrhutg5pocnpv2lzxr6xwfs6eeng7saoe7syxkeiq3zsm',
+      'baeaxdiheaiqjrcwamuudzuc7vmzfnebn6xs45fcbgorb4vfkc44aehc3eevt25q',
+      transport,
+    )
+  }
+  const site = getLocalWebSiteClient(transport)
+  try {
+    const pathRecord = await site.getPath({path: '/'})
+    const publication = pathRecord?.publication
+    return publication || null
+  } catch (e) {
+    return null
+  }
+}
 
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   // mintter://bafy2bzacedq36zy5yrhutg5pocnpv2lzxr6xwfs6eeng7saoe7syxkeiq3zsm/baeaxdiheaiqjrcwamuudzuc7vmzfnebn6xs45fcbgorb4vfkc44aehc3eevt25q
-//   const publication = await getPublication(
-//     'bafy2bzacedq36zy5yrhutg5pocnpv2lzxr6xwfs6eeng7saoe7syxkeiq3zsm',
-//     'baeaxdiheaiqjrcwamuudzuc7vmzfnebn6xs45fcbgorb4vfkc44aehc3eevt25q',
-//     transport,
-//   )
-//   // const publication = await getHomePublication()
-//   return {
-//     props: {
-//       publication: publication?.toJson(),
-//     },
-//   }
-// }
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  if (process.env.VERCEL) {
+    return {
+      props: {
+        publication: null,
+      },
+    }
+  }
+  const publication = await getHomePublication()
+  if (!publication) {
+    return {
+      notFound: true,
+    }
+  }
+  return {
+    props: {
+      publication: publication?.toJson(),
+    },
+  }
+}
--- frontend/gateway/pages/p/[...ids].tsx
+++ frontend/gateway/pages/p/[...ids].tsx
@@ -1,46 +1,53 @@
 import {getPublication, Publication} from '@mintter/shared'
 import {useQuery} from '@tanstack/react-query'
+import {GetServerSidePropsContext} from 'next'
 import {useRouter} from 'next/router'
 import {transport} from '../../client'
 import {PublicationPlaceholder} from '../../publication-placeholder'
 import {SiteHead} from '../../site-head'
 import PublicationPage from '../../ssr-publication-page'
 
-// export default function CIDPublicationPage({
-//   publication,
-// }: {
-//   publication?: Publication
-// }) {
-//   return <PublicationPage publication={publication} />
-// }
+export default function CIDPublicationPage({
+  publication,
+}: {
+  publication?: Publication | null
+}) {
+  if (publication === null) return <ClientCIDPage />
+  return <PublicationPage publication={publication} />
+}
 
-// export const getServerSideProps = async ({
-//   params,
-//   res,
-// }: GetServerSidePropsContext) => {
-//   const [documentId, version] = params?.ids || []
-//   res.setHeader(
-//     'Cache-Control',
-//     `public, s-maxage=${
-//       version ? '2592000, stale-while-revalidate=3599' : '86400'
-//     }`,
-//   )
+export const getServerSideProps = async ({
+  params,
+  res,
+}: GetServerSidePropsContext) => {
+  if (process.env.VERCEL) {
+    return {
+      props: {
+        publication: null,
+      },
+    }
+  }
+  const [documentId, version] = params?.ids || []
+  // res.setHeader(
+  //   'Cache-Control',
+  //   `public, s-maxage=${
+  //     version ? '2592000, stale-while-revalidate=3599' : '86400'
+  //   }`,
+  // )
+  const publication = await getPublication(documentId, version, transport)
+  if (!publication) {
+    return {
+      notFound: true,
+    }
+  }
+  return {
+    props: {
+      publication: publication.toJson(),
+    },
+  }
+}
 
-//   const publication = await getPublication(documentId, version, transport)
-
-//   if (!publication) {
-//     return {
-//       notFound: true,
-//     }
-//   }
-//   return {
-//     props: {
-//       publication: publication.toJson(),
-//     },
-//   }
-// }
-
-export default function CIDPublicationPage() {
+function ClientCIDPage() {
   const router = useRouter()
   let [docId, version = ''] = router.query.ids || []
   let {data} = useQuery({
