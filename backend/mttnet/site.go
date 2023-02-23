83b4b34172f1855964bcf9d172b156b24f381eea
juligasa <11684004+juligasa@users.noreply.github.com>
2023-02-23 20:19:36 +0100

    allow unpublish by path

--- backend/mttnet/site.go
+++ backend/mttnet/site.go
@@ -306,7 +306,8 @@ func (srv *Server) UnpublishDocument(ctx context.Context, in *site.UnpublishDocu
 	}
 	var toDelete []string
 	for key, record := range srv.WebPublicationRecordDB {
-		if record.Document.ID == in.DocumentId && (in.Version == "" || in.Version == record.Document.Version) {
+		if (in.Path == "" && record.Document.ID == in.DocumentId && (in.Version == "" || in.Version == record.Document.Version)) ||
+			(in.Path != "" && in.Path == record.Path) {
 			doc, err := srv.localFunctions.GetPublication(ctx, &site.GetPublicationRequest{
 				DocumentId: record.Document.ID,
 				Version:    record.Document.Version,
