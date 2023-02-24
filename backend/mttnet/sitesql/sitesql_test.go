0af8c674b2f6f8950c04b2832b63587fdc8a6166
juligasa <11684004+juligasa@users.noreply.github.com>
2023-02-24 13:11:01 +0100

    wip: site member management in db

--- backend/mttnet/sitesql/sitesql_test.go
+++ backend/mttnet/sitesql/sitesql_test.go
@@ -10,6 +10,7 @@ import (
 
 	"crawshaw.io/sqlite"
 	"crawshaw.io/sqlite/sqlitex"
+	"github.com/ipfs/go-cid"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/multierr"
 )
@@ -23,6 +24,41 @@ var (
 	futureTime = time.Now().Add(time.Minute)
 	pastTime   = time.Now().Add(-time.Minute)
 )
+
+func TestMembers(t *testing.T) {
+	conn, closer, err := makeConn()
+	require.NoError(t, err)
+	defer func() { require.NoError(t, closer()) }()
+
+	{
+		const validAccount = "bahezrj4iaqacicabciqfnrov4niome6csw43r244roia35q6fiak75bmapk2zjudj3uffea" // leads to thos multihash in binary x'00240801122056C5D5E350E613C295B9B8EB9C8B900DF61E2A00AFF42C03D5ACA6834EE85290
+		const fakeAccount = "bahezrj4iaqacicabciqhpkuxan2hm6vfh6rek3mvvs54wj5utodglwdrcatmif7jpmllyui"
+		accountCID, err := cid.Decode(validAccount)
+		require.NoError(t, err)
+		require.NoError(t, AddMember(conn, accountCID, int64(site.Member_EDITOR)))
+		role, err := GetMemberRole(conn, accountCID)
+		require.NoError(t, err)
+		require.Equal(t, site.Member_EDITOR, role)
+		accountCIDFake, err := cid.Decode(fakeAccount)
+		require.NoError(t, err)
+		_, err = GetMemberRole(conn, accountCIDFake)
+		require.Error(t, err)
+		members, err := ListMembers(conn)
+		require.NoError(t, err)
+		require.Len(t, members, 1)
+		account, ok := members[accountCID]
+		require.True(t, ok)
+		require.Equal(t, site.Member_EDITOR, account)
+		require.NoError(t, RemoveMember(conn, accountCIDFake))
+		members, err = ListMembers(conn)
+		require.NoError(t, err)
+		require.Len(t, members, 1)
+		require.NoError(t, RemoveMember(conn, accountCID))
+		members, err = ListMembers(conn)
+		require.NoError(t, err)
+		require.Len(t, members, 0)
+	}
+}
 
 func TestTokens(t *testing.T) {
 	conn, closer, err := makeConn()
@@ -104,8 +140,16 @@ func makeConn() (conn *sqlite.Conn, closer func() error, err error) {
 		-- Subjective (locally perceived) time when the item was created.
 		create_time INTEGER DEFAULT (strftime('%s', 'now')) NOT NULL
 	);
+	CREATE TABLE site_members (
+		-- The account id that has been linked to a role on this site
+		account_id INTEGER REFERENCES accounts ON DELETE CASCADE NOT NULL PRIMARY KEY,
+		-- The role the account holds ROLE_UNSPECIFIED = 0 | OWNER = 1 | EDITOR = 2
+		role INTEGER NOT NULL
+	) WITHOUT ROWID;
 	`)
-
+	err = sqlitex.ExecScript(conn, `
+	INSERT INTO accounts (multihash) VALUES(x'00240801122056C5D5E350E613C295B9B8EB9C8B900DF61E2A00AFF42C03D5ACA6834EE85290');
+	`)
 	if err != nil {
 		return nil, nil, err
 	}
