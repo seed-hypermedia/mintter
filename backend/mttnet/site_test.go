08c39094d4c98053e63875ac063625d4b87d34be
juligasa <11684004+juligasa@users.noreply.github.com>
2023-02-27 13:27:05 +0100

    fix site tests

--- backend/mttnet/site_test.go
+++ backend/mttnet/site_test.go
@@ -29,24 +29,32 @@ func TestLocalPublish(t *testing.T) {
 	defer stopSite()
 }
 
-func TestProxying(t *testing.T) {
-	t.Skip("not ready yet")
+func TestCreateAndRedeemTokens(t *testing.T) {
 	t.Parallel()
 	ownerSrv, docSrv, stopowner := makeTestSrv(t, "alice")
 	owner, ok := ownerSrv.Node.Get()
 	require.True(t, ok)
 	defer stopowner()
 
+	owner2Srv, _, stopowner2 := makeTestSrv(t, "alice-2")
+	require.True(t, ok)
+	defer stopowner2()
+
+	editorSrv, _, stopeditor := makeTestSrv(t, "bob")
+
+	require.True(t, ok)
+	defer stopeditor()
+
 	cfg := config.Default()
 	cfg.Site.Hostname = "127.0.0.1:55001"
 
-	cfg.Site.OwnerID = owner.accountObjectID.String()
-	siteSrv, _, stopSite := makeTestSrv(t, "bob", cfg.Site)
+	cfg.Site.OwnerID = owner.me.AccountID().String()
+	siteSrv, _, stopSite := makeTestSrv(t, "carol", cfg.Site)
 	site, ok := siteSrv.Node.Get()
 	require.True(t, ok)
 	defer stopSite()
 
-	docSrv.SetSiteAccount(site.accountObjectID.String())
+	docSrv.SetSiteAccount(site.me.AccountID().String())
 
 	ctx := context.Background()
 	tsFuture := time.Now().Add(48 * time.Hour).Unix()
@@ -61,79 +69,10 @@ func TestProxying(t *testing.T) {
 		ExpireTime: &timestamppb.Timestamp{Seconds: tsFuture},
 	})
 	require.Error(t, err)
-
-	require.NoError(t, owner.Connect(ctx, site.AddrInfo()))
-	time.Sleep(5 * time.Second)
-	header := metadata.New(map[string]string{string(MttHeader): cfg.Site.Hostname})
-	ctx = metadata.NewIncomingContext(ctx, header) // Usually, the headers are written by the client in the outgoing context and server receives them in the incoming. But here we are writing the server directly
-	token, err := ownerSrv.CreateInviteToken(ctx, &documents.CreateInviteTokenRequest{
-		Role:       documents.Member_EDITOR,
-		ExpireTime: &timestamppb.Timestamp{Seconds: tsFuture},
-	})
-	require.NoError(t, err)
-	require.Equal(t, tsFuture, token.ExpireTime.Seconds)
-}
-
-func TestGetSiteInfo(t *testing.T) {
-	t.Parallel()
-	ownerSrv, docSrv, stopowner := makeTestSrv(t, "alice")
-	owner, ok := ownerSrv.Node.Get()
-	require.True(t, ok)
-	defer stopowner()
-
-	cfg := config.Default()
-	cfg.Site.Hostname = "127.0.0.1:55001"
-	cfg.Site.Title = "My title"
-	cfg.Site.OwnerID = owner.accountObjectID.String()
-	siteSrv, _, stopSite := makeTestSrv(t, "bob", cfg.Site)
-	site, ok := siteSrv.Node.Get()
-	require.True(t, ok)
-	defer stopSite()
-
-	docSrv.SetSiteAccount(site.accountObjectID.String())
-
-	ctx := context.Background()
-	time.Sleep(100 * time.Millisecond)
-	siteInfo, err := siteSrv.GetSiteInfo(ctx, &documents.GetSiteInfoRequest{})
-	require.NoError(t, err)
-	require.Equal(t, cfg.Site.Hostname, siteInfo.Hostname)
-	require.Equal(t, cfg.Site.OwnerID, siteInfo.Owner)
-	require.Equal(t, cfg.Site.Title, siteInfo.Title)
-}
-func TestE2e(t *testing.T) {
-	t.Skip("not ready yet")
-	t.Parallel()
-	ownerSrv, docSrv, stopowner := makeTestSrv(t, "alice")
-	owner, ok := ownerSrv.Node.Get()
-	require.True(t, ok)
-	defer stopowner()
-
-	editorSrv, _, stopeditor := makeTestSrv(t, "bob")
-
-	require.True(t, ok)
-	defer stopeditor()
-
-	cfg := config.Default()
-	cfg.Site.Hostname = "127.0.0.1:55001"
-
-	cfg.Site.OwnerID = owner.accountObjectID.String()
-	siteSrv, _, stopSite := makeTestSrv(t, "carol", cfg.Site)
-	site, ok := siteSrv.Node.Get()
-	require.True(t, ok)
-	defer stopSite()
-
-	docSrv.SetSiteAccount(site.accountObjectID.String())
-
-	owner2Srv, _, stopowner2 := makeTestSrv(t, "alice-2")
-	require.True(t, ok)
-	defer stopowner2()
-
-	ctx := context.Background()
-	tsFuture := time.Now().Add(48 * time.Hour).Unix()
-
 	require.NoError(t, owner.Connect(ctx, site.AddrInfo()))
 	header := metadata.New(map[string]string{string(MttHeader): cfg.Site.Hostname})
 	ctx = metadata.NewIncomingContext(ctx, header) // Usually, the headers are written by the client in the outgoing context and server receives them in the incoming. But here we are writing the server directly
+	ctx = context.WithValue(ctx, SiteAccountIDCtxKey, site.me.AccountID().String())
 	token, err := ownerSrv.CreateInviteToken(ctx, &documents.CreateInviteTokenRequest{
 		Role:       documents.Member_EDITOR,
 		ExpireTime: &timestamppb.Timestamp{Seconds: tsFuture},
@@ -158,19 +97,48 @@ func TestE2e(t *testing.T) {
 		Role:       documents.Member_ROLE_UNSPECIFIED,
 		ExpireTime: &timestamppb.Timestamp{Seconds: tsFuture},
 	})
-	require.Error(t, err)
+	require.NoError(t, err)
 
 	_, err = editorSrv.CreateInviteToken(ctx, &documents.CreateInviteTokenRequest{
 		Role:       documents.Member_EDITOR,
 		ExpireTime: &timestamppb.Timestamp{Seconds: tsFuture},
 	})
+
 	require.Error(t, err)
 
 	_, err = owner2Srv.CreateInviteToken(ctx, &documents.CreateInviteTokenRequest{
 		Role:       documents.Member_EDITOR,
 		ExpireTime: &timestamppb.Timestamp{Seconds: tsFuture},
 	})
+
 	require.Error(t, err)
+}
+
+func TestGetSiteInfo(t *testing.T) {
+	t.Parallel()
+	ownerSrv, docSrv, stopowner := makeTestSrv(t, "alice")
+	owner, ok := ownerSrv.Node.Get()
+	require.True(t, ok)
+	defer stopowner()
+
+	cfg := config.Default()
+	cfg.Site.Hostname = "127.0.0.1:55001"
+	cfg.Site.Title = "My title"
+	cfg.Site.OwnerID = owner.me.AccountID().String()
+	siteSrv, _, stopSite := makeTestSrv(t, "bob", cfg.Site)
+	site, ok := siteSrv.Node.Get()
+	require.True(t, ok)
+	defer stopSite()
+
+	docSrv.SetSiteAccount(site.me.AccountID().String())
+
+	ctx := context.Background()
+	time.Sleep(100 * time.Millisecond)
+	siteInfo, err := siteSrv.GetSiteInfo(ctx, &documents.GetSiteInfoRequest{})
+	require.NoError(t, err)
+	require.Equal(t, cfg.Site.Hostname, siteInfo.Hostname)
+	require.Equal(t, cfg.Site.OwnerID, siteInfo.Owner)
+	require.Equal(t, cfg.Site.Title, siteInfo.Title)
 }
 
 func makeTestSrv(t *testing.T, name string, siteCfg ...config.Site) (*Server, *simulatedDocs, context.CancelFunc) {
