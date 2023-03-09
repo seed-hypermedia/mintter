b09a3747a8767efc91c550dd6f5e72075506ceba
Horacio Herrera <hi@horacioh.com>
2023-03-09 01:04:54 +0100

    citations number in blocks

--- frontend/app/src/editor/drag-section.tsx
+++ frontend/app/src/editor/drag-section.tsx
@@ -1,9 +1,11 @@
 import {useDrag} from '@app/drag-context'
 import {useCitationsForBlock} from '@app/editor/comments/citations-context'
-import {useConversations} from '@app/editor/comments/conversations-context'
 import {EditorMode} from '@app/editor/plugin-utils'
 import {useMouse} from '@app/mouse-context'
+import {Button} from '@components/button'
 import {ConversationBlockBubble} from '@components/conversation-block-bubble'
+import {Icon} from '@components/icon'
+import {Text} from '@components/text'
 import {FlowContent} from '@mintter/shared'
 import React from 'react'
 import {RenderElementProps, useSlate} from 'slate-react'
@@ -49,9 +51,9 @@ export const ElementDrag = ({
       {editor.mode == EditorMode.Publication ? (
         <span contentEditable={false}>
           <ConversationBlockBubble block={element as FlowContent} />
-          {/* {editor.mode == EditorMode.Publication ? (
+          {editor.mode == EditorMode.Publication ? (
             <CitationNumber block={element as FlowContent} />
-          ) : null} */}
+          ) : null}
         </span>
       ) : null}
     </li>
@@ -59,8 +61,52 @@ export const ElementDrag = ({
 }
 
 function CitationNumber({block}: {block: FlowContent}) {
-  let cites = useCitationsForBlock(block.id)
-  console.log('🚀 ~ file: drag-section.tsx:62 ~ CitationNumber ~ cites:', cites)
+  let {citations = [], onCitationsOpen} = useCitationsForBlock(block.id)
+  console.log(
+    '🚀 ~ file: drag-section.tsx:65 ~ CitationNumber ~ citations:',
+    citations,
+  )
 
-  return cites ? <span>{cites.length}</span> : null
+  return citations?.length ? (
+    <Button
+      onClick={(e) => {
+        e.preventDefault()
+        onCitationsOpen(citations)
+      }}
+      color="muted"
+      variant="ghost"
+      size="0"
+      contentEditable={false}
+      css={{
+        userSelect: 'none',
+        position: 'absolute',
+        top: 32,
+        right: -54,
+        display: 'flex',
+        alignItems: 'center',
+        gap: '$2',
+        paddingInline: '$3',
+        paddingBlock: '$1',
+        // borderRadius: '$2',
+        zIndex: '$max',
+        '&:hover': {
+          backgroundColor: '$base-component-bg-hover',
+          cursor: 'pointer',
+        },
+      }}
+    >
+      <Icon name="ArrowTopRight" size="2" color="muted" />
+      <Text
+        size="2"
+        color="muted"
+        css={{
+          '&:hover': {
+            color: '$base-text-low',
+          },
+        }}
+      >
+        {citations.length}
+      </Text>
+    </Button>
+  ) : null
 }
