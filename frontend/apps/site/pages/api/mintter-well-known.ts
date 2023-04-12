24b91e85b98f98ad8edbde0b317ef25e0438698f
Eric Vicenti <eric@vicenti.net>
2023-04-11 19:43:37 -0700

    Well known response fix

--- frontend/apps/site/pages/api/mintter-well-known.ts
+++ frontend/apps/site/pages/api/mintter-well-known.ts
@@ -1,26 +1,15 @@
 import {daemonClient, networkingClient} from '../../client'
-import {NextRequest, NextResponse} from 'next/server'
+import {NextApiRequest, NextApiResponse} from 'next'
 
-export default async function handler(req: NextRequest, res: NextResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
   const info = await daemonClient.getInfo({})
-  console.log('🚀 ~ file: mintter-well-known.ts:6 ~ handler ~ info:', info)
   const peerInfo = await networkingClient.getPeerInfo({peerId: info.peerId})
-  console.log(
-    '🚀 ~ file: mintter-well-known.ts:8 ~ handler ~ peerInfo:',
-    peerInfo,
-  )
-
   const wellKnown = {
     account_id: info.accountId,
     addresses: peerInfo.addrs,
   }
-  console.log(
-    '🚀 ~ file: mintter-well-known.ts:14 ~ handler ~ wellKnown:',
-    wellKnown,
-  )
-  return new Response(JSON.stringify(wellKnown), {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
+  res.status(200).send(wellKnown)
 }
