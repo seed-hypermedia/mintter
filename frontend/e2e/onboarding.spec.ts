4db7fb1acd41d5afe57c0d50d877af05a1a99265
Eric Vicenti <eric@vicenti.net>
2023-04-19 01:38:16 -0700

    yarn e2e workflow for onboarding

---
+++ frontend/e2e/onboarding.spec.ts
@@ -0,0 +1,25 @@
+import {test, expect} from '@playwright/test'
+
+test('has expected title - vite is here', async ({page}) => {
+  await page.goto('http://localhost:5173/')
+  await expect(page).toHaveTitle('Vite + React + TS')
+})
+
+test('welcome steps', async ({page}) => {
+  await page.goto('http://localhost:5173/')
+  await expect(page.getByText('Welcome to Mintter')).toBeVisible()
+  await page.getByText('NEXT').click()
+  await expect(page.getByText('Your Keys')).toBeVisible()
+  await page.getByText('NEXT').click()
+  await expect(page.getByText('Link your personal data')).toBeVisible()
+  await page.getByLabel('Alias').fill('Monkey')
+  await page.getByText('NEXT').click()
+  // currently failing on this step:
+  await expect(page.getByText('Analytics')).toBeVisible()
+  await page.getByText('NEXT').click()
+  await expect(
+    page.getByText('You just created your Mintter account'),
+  ).toBeVisible()
+  await page.getByText('Open Mintter App').click()
+  await expect(page.getByText('You have no Publications yet.')).toBeVisible()
+})
--- playwright.config.ts
+++ playwright.config.ts
@@ -36,42 +36,12 @@ export default defineConfig({
       name: 'chromium',
       use: {...devices['Desktop Chrome']},
     },
-
-    {
-      name: 'firefox',
-      use: {...devices['Desktop Firefox']},
-    },
-
-    {
-      name: 'webkit',
-      use: {...devices['Desktop Safari']},
-    },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ..devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'yarn workspace @mintter/e2e-dev-server start',
+    url: 'http://127.0.0.1:5173',
+    // reuseExistingServer: !process.env.CI,
+  },
 })
