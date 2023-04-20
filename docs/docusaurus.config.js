798206b0f48874c8e60663f39e4eccf0b75d50f4
Eric Vicenti <eric@vicenti.net>
2023-04-20 01:42:12 -0700

    Fix the editUrl

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -39,18 +39,9 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/mintterteam/mintter/tree/main/docs/docs/',
+            'https://github.com/mintterteam/mintter/tree/main/docs/',
         },
-        // blog: {
-        //   showReadingTime: true,
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        // },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
