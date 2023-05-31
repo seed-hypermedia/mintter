54cc0f64af9a3a0956f0d0b86bde39350080529a
Alexandr Burdiyan <burdiyan@gmail.com>
2023-05-31 17:24:30 +0200

    build(nix): update Rust

--- build/nix/nixpkgs.nix
+++ build/nix/nixpkgs.nix
@@ -10,13 +10,13 @@ let
     rev = "21eb6c6ba74dcbe3ea5926ee46287300fb066630";
     sha256 = "sha256-ZLnSr35L6C49pCZS9fZCCqkIKNAeQzykov2QfosNG9w=";
   };
-  # Rust overlay as of 2022-01-24.
+  # Rust overlay as of 2023-05-31.
   # Update sha256 with `nix-prefetch fetchFromGitHub --owner oxalica --repo rust-overlay --rev <commit>`.
   rustOverlay = import (pkgs.fetchFromGitHub {
     owner = "oxalica";
     repo = "rust-overlay";
-    rev = "4e093ce661a63aca4bcbace33695225eae4ef4e4";
-    sha256 = "sha256-YsWu3C9IGbH3+xguTzEDyQorFe/igr6FGZ+Q5T2ocxE=";
+    rev = "9651f0beee6e7a9783cc02eac722854851c65ae7";
+    sha256 = "sha256-P6wLC+P8o9w4XNLZAbZy3BwKkp1xi/+H9dF+7SXDP70=";
   });
   mintterOverlay = import ./overlay.nix;
 in
