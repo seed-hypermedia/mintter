64cfa4372861e24fd02e109256c74ee6eb2d6c7b
Eric Vicenti <eric@vicenti.net>
2023-07-23 14:51:08 -0700

    Fix to useOpenDraft

--- frontend/packages/app/src/utils/open-draft.ts
+++ frontend/packages/app/src/utils/open-draft.ts
@@ -1,12 +1,14 @@
 import {queryKeys} from '@mintter/app/src/models/query-keys'
 import {toast} from 'react-hot-toast'
-import {DocumentChange} from '@mintter/shared'
+import {DocumentChange, GRPCClient} from '@mintter/shared'
 import {DraftRoute, useNavigate, useNavRoute} from './navigation'
 import {useQueryInvalidator} from '@mintter/app'
 import {useGRPCClient} from '../app-context'
 
-async function createDraft(siteHostname: string | undefined): Promise<string> {
-  const grpcClient = useGRPCClient()
+async function createDraft(
+  grpcClient: GRPCClient,
+  siteHostname: string | undefined,
+): Promise<string> {
   const doc = await grpcClient.drafts.createDraft({})
   if (siteHostname) {
     await grpcClient.drafts.updateDraft({
@@ -26,9 +28,10 @@ export function useOpenDraft() {
   const route = useNavRoute()
   const spawn = useNavigate('spawn')
   const invalidate = useQueryInvalidator()
+  const grpcClient = useGRPCClient()
   console.log('hello invalidate', invalidate)
   function openNewDraft(newWindow = true, hostname?: string | undefined) {
-    createDraft(hostname)
+    createDraft(grpcClient, hostname)
       .then((docId: string) => {
         const draftRoute: DraftRoute = {
           key: 'draft',
