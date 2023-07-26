99ef1f4b963ac108c14f75f8f62ef0c722c78d38
Alexandr Burdiyan <burdiyan@gmail.com>
2023-07-26 17:00:29 +0200

    fix migration test check

--- backend/daemon/storage/migrations_test.go
+++ backend/daemon/storage/migrations_test.go
@@ -7,8 +7,15 @@ import (
 	"golang.org/x/exp/slices"
 )
 
-func TestMigrationsSorted(t *testing.T) {
+func TestMigrationList(t *testing.T) {
 	require.True(t, slices.IsSortedFunc(migrations, func(a, b migration) bool {
 		return a.Version < b.Version
 	}), "the list of migrations must be sorted")
+
+	out := slices.CompactFunc(migrations, func(a, b migration) bool {
+		return a.Version == b.Version
+	})
+	if len(out) != len(migrations) {
+		t.Fatalf("the list of migrations must not contain duplicates: %v", migrations)
+	}
 }
--- backend/daemon/storage/schema_test.go
+++ backend/daemon/storage/schema_test.go
@@ -2,26 +2,50 @@ package storage
 
 import (
 	"bytes"
+	"io"
+	"mintter/backend/pkg/must"
 	"mintter/backend/pkg/sqlitedbg"
 	"mintter/backend/pkg/sqlitegen"
+	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
 )
 
-func TestSchemaMatchPreMigrationsDir(t *testing.T) {
-	t.Skip("Not working after first migration")
-	old, err := OpenSQLite("./testdata/initial-data-dir/db/db.sqlite", 0, 1)
+func TestMigrateMatchesFreshSchema(t *testing.T) {
+	// We have manually snapshot the data dir from before the migration framework was implemented.
+	// It's stored in ./testdata/initial-data-dir.
+	// We want to test that the data dir with all applied migrations matches the data dir created from scratch.
+	// So we copy the initial snapshot to a temporary directory, apply all migrations to it,
+	// and then compare it with a fresh directory.
+
+	tmpDir := t.TempDir()
+	err := copyDir("./testdata/initial-data-dir", tmpDir)
 	require.NoError(t, err)
-	defer old.Close()
 
-	db := MakeTestDB(t)
+	oldDir, err := New(tmpDir, zap.NewNop())
+	require.NoError(t, err)
+	require.NoError(t, oldDir.Migrate())
 
-	oldSchema, err := sqlitegen.IntrospectSchema(old)
+	newDir, err := New(t.TempDir(), zap.NewNop())
+	require.NoError(t, err)
+	require.NoError(t, newDir.Migrate())
+
+	oldDB, err := OpenSQLite(oldDir.SQLitePath(), 0, 1)
+	require.NoError(t, err)
+	defer oldDB.Close()
+
+	newDB, err := OpenSQLite(newDir.SQLitePath(), 0, 1)
+	require.NoError(t, err)
+	defer newDB.Close()
+
+	oldSchema, err := sqlitegen.IntrospectSchema(oldDB)
 	require.NoError(t, err)
 
-	newSchema, err := sqlitegen.IntrospectSchema(db)
+	newSchema, err := sqlitegen.IntrospectSchema(newDB)
 	require.NoError(t, err)
 
 	require.Equal(t, oldSchema, newSchema)
@@ -31,8 +55,49 @@ func TestSchemaMatchPreMigrationsDir(t *testing.T) {
 		newSQL bytes.Buffer
 	)
 
-	sqlitedbg.Exec(old, &oldSQL, "select sql from sqlite_schema")
-	sqlitedbg.Exec(db, &newSQL, "select sql from sqlite_schema")
+	sqlitedbg.Exec(oldDB, &oldSQL, "select sql from sqlite_schema order by name")
+	sqlitedbg.Exec(newDB, &newSQL, "select sql from sqlite_schema order by name")
+	require.Equal(t, oldSQL.String(), newSQL.String())
 
-	require.Equal(t, strings.Replace(oldSQL.String(), "\t", "    ", -1), newSQL.String())
+	// We want to check that the version file matches the version of the last migration.
+	require.Equal(t, migrations[len(migrations)-1].Version, must.Do2(readVersionFile(oldDir.path)))
+	require.Equal(t, migrations[len(migrations)-1].Version, must.Do2(readVersionFile(newDir.path)))
+}
+
+func copyDir(src, dst string) error {
+	src = strings.TrimPrefix(src, "./")
+	return filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		dstPath := strings.Replace(path, src, dst, 1)
+
+		if info.IsDir() {
+			return os.MkdirAll(dstPath, 0750)
+		}
+
+		return copyFile(path, dstPath)
+	})
+}
+
+func copyFile(src, dst string) error {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	dstFile, err := os.OpenFile(dst, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		return err
+	}
+
+	return dstFile.Sync()
 }
