2872a58fb906305fd0859f8cd70a39d98da10e49
Horacio Herrera <hi@horacioh.com>
2023-08-09 23:29:57 +0200

    start from master. wip

--- frontend/apps/electron/forge.config.js
+++ frontend/apps/electron/forge.config.js
@@ -15,14 +15,6 @@ const LLVM_TRIPLES = {
 function getPlatformTriple() {
   return LLVM_TRIPLES[`${process.platform}/${process.arch}`]
 }
-
-console.log(
-  '====== TRIPLES',
-  process.platform,
-  process.arch,
-  getPlatformTriple(),
-)
-
 const daemonBinaryPath = path.join(
   devProjectRoot,
   // TODO: parametrize this for each platform
