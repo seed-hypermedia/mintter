3d35867e49985ac6862cea483b2a82c01f312d2e
Horacio Herrera <hi@horacioh.com>
2023-08-18 16:30:00 +0200

    remove use client react-query warning

--- frontend/apps/desktop/vite.main.config.ts
+++ frontend/apps/desktop/vite.main.config.ts
@@ -12,6 +12,19 @@ export default defineConfig(({command, mode}) => {
       sourcemap: true,
       rollupOptions: {
         external: ['@trpc/client'],
+        /**
+         * Ignore "use client" waning since we are not using SSR
+         * @see {@link https://github.com/TanStack/query/pull/5161#issuecomment-1477389761 Preserve 'use client' directives TanStack/query#5161}
+         */
+        onwarn(warning, warn) {
+          if (
+            warning.code === 'MODULE_LEVEL_DIRECTIVE' &&
+            warning.message.includes(`"use client"`)
+          ) {
+            return
+          }
+          warn(warning)
+        },
       },
     },
     resolve: {
