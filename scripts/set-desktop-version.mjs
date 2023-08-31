650bcc40d5ce93b869260ee2f34522f4ef9f6eec
Horacio Herrera <hi@horacioh.com>
2023-08-31 17:38:14 +0200

    frontend: add temporal version to package.json

---
+++ scripts/set-desktop-version.mjs
@@ -0,0 +1,19 @@
+import fs from "fs";
+// Read the package.json file
+const packageJsonPath = "./frontend/apps/desktop/package.json";
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+
+// Update the version attribute
+packageJson.version = process.env.APP_VERSION;
+
+try {
+  // Write the updated package.json back to the file
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+  console.log(
+    `[SET-VERSION]: Successfully change the app version to ${process.env.APP_VERSION}`
+  );
+} catch (error) {
+  console.error(
+    `[SET-VERSION]: ERROR. something went wrong: ${JSON.stringify(error)}`
+  );
+}
