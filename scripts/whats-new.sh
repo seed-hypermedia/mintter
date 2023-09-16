cc93d48e3bb9285a90ddcd248012fe75b0fee7a6
Horacio Herrera <hi@horacioh.com>
2023-09-16 15:06:25 +0200

    feat: whats-new script improved

--- scripts/whats-new.sh
+++ scripts/whats-new.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-# Check if tag parameter is provided
-if [ $# -eq 0 ]; then
-    echo "Error: Please provide a tag name as a parameter."
-    exit 1
-fi
-
-# Get the tag name from the parameter
-tag_name=$1
-
 # Pull the latest changes
 git pull origin master
 
 # List commits after the specified tag
-git log $tag_name..HEAD > whats-new.md
+git log `git describe --tags --match "*.*.*" --abbrev=0`..HEAD > whats-new.txt
