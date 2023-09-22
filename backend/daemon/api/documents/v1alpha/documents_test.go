085306a35756cf52472efbd852c5759fe3e3852e
Alexandr Burdiyan <burdiyan@gmail.com>
2023-09-22 17:43:52 +0200

    fix(backend): fix annoying flaky document test with timestamps

--- backend/daemon/api/documents/v1alpha/documents_test.go
+++ backend/daemon/api/documents/v1alpha/documents_test.go
@@ -114,7 +114,7 @@ func TestUpdateDraft_WithBlocks(t *testing.T) {
 	api := newTestDocsAPI(t, "alice")
 	ctx := context.Background()
 
-	start := time.Now().Add(-1 * time.Second).UTC().UnixMicro()
+	start := time.Now().Add(-2 * time.Second).UTC().UnixMicro()
 
 	draft, err := api.CreateDraft(ctx, &documents.CreateDraftRequest{})
 	require.NoError(t, err)
