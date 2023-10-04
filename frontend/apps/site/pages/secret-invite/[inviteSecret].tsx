7c1229b826463b1d0a35202e512064575b334878
Eric Vicenti <eric@vicenti.net>
2023-10-03 18:42:12 -0700

    Very simple secret-invite page for new site

---
+++ frontend/apps/site/pages/secret-invite/[inviteSecret].tsx
@@ -0,0 +1,31 @@
+import {PageSection, SizableText, YStack} from '@mintter/ui'
+import {SiteHead} from '../../site-head'
+
+export default function NotFoundPage() {
+  return (
+    <YStack flex={1}>
+      <SiteHead pageTitle="Secret Setup URL" />
+      <PageSection.Root>
+        <PageSection.Side />
+        <PageSection.Content tag="main" id="main-content" tabIndex={-1}>
+          <YStack
+            paddingVertical="$7"
+            paddingHorizontal="$5"
+            borderRadius="$5"
+            elevation="$1"
+            borderColor="$color5"
+            borderWidth={1}
+            backgroundColor="$color3"
+            gap="$3"
+          >
+            <SizableText size="$5" fontWeight="800" textAlign="center">
+              Welcome to your new site. Paste this URL into the Mintter app to
+              publish your group to this new site.
+            </SizableText>
+          </YStack>
+        </PageSection.Content>
+        <PageSection.Side />
+      </PageSection.Root>
+    </YStack>
+  )
+}
