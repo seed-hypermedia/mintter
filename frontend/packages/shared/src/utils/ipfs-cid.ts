42dc9e7c7b2f7325e85ba496f4879cc856a35af2
Eric Vicenti <eric@vicenti.net>
2023-10-12 10:55:17 +0200

    Fixing nextjs runtime error

--- frontend/packages/shared/src/utils/ipfs-cid.ts
+++ frontend/packages/shared/src/utils/ipfs-cid.ts
@@ -1,4 +1,5 @@
-export function getCIDFromIPFSUrl(url: string): string | null {
+export function getCIDFromIPFSUrl(url: string | undefined): string | null {
+  if (!url) return null
   const regex = /ipfs:\/\/(.+)/
   const match = url.match(regex)
   return match ? match[1] : null
