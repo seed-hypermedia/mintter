104dad36d02d129ac8d0b8d21ffebfbc2dcc070c
Eric Vicenti <eric@vicenti.net>
2023-10-17 13:01:03 +0200

    Frontend rm fix

--- scripts/cleanup-frontend.sh
+++ scripts/cleanup-frontend.sh
@@ -1,4 +1,4 @@
 rm yarn.lock \; 2>/dev/null || true 
 find . -type d -name "node_modules" -exec rm -r {} \; 2>/dev/null || true 
 find . -type d -name ".tamagui" -exec rm -r {} \; 2>/dev/null || true 
-rm frontend/packages/ui/src/themes-generated.ts
+rm -f frontend/packages/ui/src/themes-generated.ts
