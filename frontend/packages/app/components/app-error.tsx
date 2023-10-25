772d6f840d2411182bc1d193b53c0c0167665ca3
Eric Vicenti <eric@vicenti.net>
2023-10-25 16:30:48 -0700

    App error theming

--- frontend/packages/app/components/app-error.tsx
+++ frontend/packages/app/components/app-error.tsx
@@ -1,13 +1,29 @@
-import {Button, Text, YStack} from '@mintter/ui'
+import {Button, Heading, SizableText, Text, XStack, YStack} from '@mintter/ui'
 import {FallbackProps} from 'react-error-boundary'
 
 export function AppError({error, resetErrorBoundary}: FallbackProps) {
   return (
-    <YStack role="alert" space>
-      <Text>Something went wrong loading the App:</Text>
-      <Text tag="pre">{error.message}</Text>
-      <Button onPress={resetErrorBoundary}>Try again</Button>
-    </YStack>
+    <XStack jc="center" ai="center" f={1} backgroundColor={'$color2'}>
+      <YStack
+        space
+        theme="red"
+        backgroundColor="$color1"
+        maxWidth={500}
+        padding="$4"
+        f={1}
+        borderRadius="$4"
+      >
+        <Heading color="$red10">Something went wrong</Heading>
+        <YStack padding="$4" backgroundColor={'$gray2'} borderRadius="$2">
+          <SizableText tag="pre" fontFamily={'$mono'}>
+            {error.message}
+          </SizableText>
+        </YStack>
+        <XStack jc="center">
+          <Button onPress={resetErrorBoundary}>Try again</Button>
+        </XStack>
+      </YStack>
+    </XStack>
   )
 }
 
