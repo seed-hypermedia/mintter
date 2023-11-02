3c98c162d1c547d7b6e520f711993431cb26c97b
Horacio Herrera <hi@horacioh.com>
2023-11-02 15:25:11 +0100

    fix(titlebar): add more options to Win32/Linux titlebar

--- frontend/packages/app/components/titlebar-windows-linux.tsx
+++ frontend/packages/app/components/titlebar-windows-linux.tsx
@@ -7,6 +7,7 @@ import {
   Close,
   CloseAll,
   Delete,
+  Draft,
   Hide,
   ListItem,
   ListItemProps,
@@ -37,6 +38,14 @@ import {
   MaximizeOrRestoreButton,
   MinimizeButton,
 } from './window-controls'
+import {ForceSyncRequest} from '@mintter/shared/src/client/.generated/daemon/v1alpha/daemon_pb'
+import {
+  dispatchAppNavigation,
+  useNavRoute,
+  useNavigationDispatch,
+} from '../utils/navigation'
+import {toast} from '../toast'
+import {Bookmark, Contact, Globe, Library} from '@tamagui/lucide-icons'
 
 export default function TitleBarWindows(props: TitleBarProps) {
   if (props.clean) {
@@ -135,8 +144,11 @@ export function WindowsLinuxTitleBar({
 export function SystemMenu() {
   const {hide, close} = useWindowUtils()
   const spawn = useNavigate('spawn')
+  const push = useNavigate('push')
+  const navDispatch = useNavigationDispatch()
+  const route = useNavRoute()
   const triggerFocusedWindow = useTriggerWindowEvent()
-  const {invoke, send} = useIPC()
+  const {invoke} = useIPC()
   const menuItems: Array<MenuItemElement> = useMemo(
     () => [
       {
@@ -149,6 +161,19 @@ export function SystemMenu() {
             accelerator: 'Ctrl+,',
             onSelect: () => spawn({key: 'settings'}),
             icon: Settings,
+          },
+          {
+            id: 'quickswitcher',
+            title: 'Search / Open',
+            accelerator: 'Ctrl+K',
+            onSelect: () => triggerFocusedWindow('openQuickSwitcher'),
+            icon: Search,
+          },
+          {
+            id: 'forcesync',
+            title: 'Trigger sync with Peers',
+            accelerator: 'Opt+Ctrl+R',
+            onSelect: () => triggerFocusedWindow('triggerPeerSync'),
           },
           {
             id: 'hide',
@@ -170,15 +195,22 @@ export function SystemMenu() {
         id: 'file',
         children: [
           {
-            id: 'newwindow',
-            title: 'New Window',
+            id: 'newdocument',
+            title: 'New Document',
             accelerator: 'Ctrl+N',
             onSelect: () => spawn({key: 'home'}),
             icon: AddSquare,
           },
           {
+            id: 'newwindow',
+            title: 'New Window',
+            accelerator: 'Ctrl+Shift+N',
+            onSelect: () => spawn({key: 'home'}),
+            icon: AddSquare,
+          },
+          {
             id: 'close',
-            title: 'Close',
+            title: 'Close Window  ',
             accelerator: 'Ctrl+F4',
             onSelect: () => close(),
             icon: Close,
@@ -192,10 +224,83 @@ export function SystemMenu() {
           },
         ],
       },
+      // TODO: we need to pass the `editor` object to make all this actions possible: copy, cut, paste...
+      // {
+      //   id: 'edit',
+      //   title: 'Edit',
+      //   children: [
+      //     {
+      //       id: 'undo',
+      //       title: 'Undo',
+      //       accelerator: 'Ctrl+Z',
+      //       disabled: true,
+      //       onSelect: () => toast.error('Action Not implemented from the menu'),
+      //     },
+      //     {
+      //       id: 'redo',
+      //       title: 'Redo',
+      //       accelerator: 'Ctrl+Shift+Z',
+      //       disabled: true,
+      //       onSelect: () => toast.error('Action Not implemented from the menu'),
+      //     },
+      //   ],
+      // },
       {
         id: 'view',
         title: 'View',
         children: [
+          {
+            id: 'back',
+            title: 'Back',
+            accelerator: 'Ctrl+◀︎',
+            onSelect: () => navDispatch({type: 'pop'}),
+          },
+          {
+            id: 'forward',
+            title: 'Forward',
+            accelerator: 'Ctrl+▶︎',
+            onSelect: () => navDispatch({type: 'forward'}),
+          },
+          {
+            id: 'publications',
+            title: 'Publications',
+            accelerator: 'Ctrl+1',
+            onSelect: () => push({key: 'home'}),
+            icon: Bookmark,
+            disabled: route.key == 'home',
+          },
+          {
+            id: 'allpublications',
+            title: 'All Publications',
+            accelerator: 'Ctrl+2',
+            onSelect: () => push({key: 'all-publications'}),
+            icon: Globe,
+            disabled: route.key == 'all-publications',
+          },
+          {
+            id: 'groups',
+            title: 'Groups',
+            accelerator: 'Ctrl+3',
+            onSelect: () => push({key: 'groups'}),
+            icon: Library,
+            disabled: route.key == 'groups',
+          },
+          {
+            id: 'drafts',
+            title: 'Drafts',
+            accelerator: 'Ctrl+8',
+            onSelect: () => push({key: 'drafts'}),
+            icon: Draft,
+            disabled: route.key == 'drafts',
+          },
+          {
+            id: 'contacts',
+            title: 'Contacts',
+            accelerator: 'Ctrl+9',
+            onSelect: () => push({key: 'contacts'}),
+            icon: Contact,
+            disabled: route.key == 'contacts',
+          },
           {
             id: 'reload',
             title: 'Reload',
@@ -204,23 +309,25 @@ export function SystemMenu() {
             icon: Reload,
           },
           {
-            id: 'quickswitcher',
-            title: 'Quick Switcher',
-            accelerator: 'Ctrl+K',
-            onSelect: () => triggerFocusedWindow('openQuickSwitcher'),
-            icon: Search,
+            id: 'reload',
+            title: 'Force Reload',
+            accelerator: 'Ctrl+Shift+R',
+            onSelect: () => window.location.reload(),
+            icon: Reload,
           },
-          // { // todo, fix this
-          //   id: 'contacts',
-          //   title: 'Contacts',
-          //   accelerator: 'Ctrl+9',
-          //   onSelect: () => {},
-          //   icon: Reload,
-          // },
         ],
       },
     ],
-    [close, hide, invoke, spawn, triggerFocusedWindow],
+    [
+      close,
+      hide,
+      invoke,
+      spawn,
+      triggerFocusedWindow,
+      route.key,
+      navDispatch,
+      push,
+    ],
   )
 
   return (
@@ -229,10 +336,11 @@ export function SystemMenu() {
         <Popover key={item.id} placement="bottom-start">
           <Popover.Trigger asChild>
             <Button
-              size="$1.5"
+              size="$1"
               backgroundColor="transparent"
               borderRadius={0}
               paddingHorizontal="$2"
+              fontWeight={item.id == 'mintter' ? 'bold' : undefined}
             >
               {item.title}
             </Button>
@@ -263,19 +371,22 @@ export function SystemMenu() {
                     pressTheme
                     hoverStyle={{
                       cursor: 'pointer',
+                      backgroundColor: '$backgroundFocus',
                     }}
                     paddingHorizontal="$3"
                     paddingVertical="$1"
                     backgroundColor="transparent"
                     onPress={p.onSelect}
+                    size="$2"
+                    disabled={p.disabled}
                   >
-                    <SizableText fontSize="$2" flex={1}>
+                    <SizableText fontSize="$1" flex={1}>
                       {p.title}
                     </SizableText>
                     {p.accelerator && (
                       <SizableText
                         marginLeft="$2"
-                        fontSize="$2"
+                        fontSize="$1"
                         color={'$color9'}
                       >
                         {p.accelerator}
