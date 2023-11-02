858fdf69b48c55d01441fd0ac75ed7f445a6ff1a
Eric Vicenti <eric@vicenti.net>
2023-11-01 22:26:12 -0700

    OpenGraph Metadata for Twitter

--- frontend/apps/site/src/head.tsx
+++ frontend/apps/site/src/head.tsx
@@ -11,3 +11,29 @@ export function OGImageMeta({url}: {url: string}) {
     </>
   )
 }
+
+export function BasicOGMeta({
+  title,
+  description,
+}: {
+  title?: string
+  description?: string
+}) {
+  return (
+    <>
+      {description ? (
+        <>
+          <meta property="og:title" content={title} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content={title} />
+        </>
+      ) : null}
+      {description ? (
+        <>
+          <meta property="og:description" content={description} />
+          <meta name="twitter:description" content={description} />
+        </>
+      ) : null}
+    </>
+  )
+}
