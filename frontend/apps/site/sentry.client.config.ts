d6d1bea5e423b445baf75a78cde7bd7124b140e6
Horacio Herrera <hi@horacioh.com>
2023-11-28 02:59:15 +0100

    sites: hardcoding sentry SDN

--- frontend/apps/site/sentry.client.config.ts
+++ frontend/apps/site/sentry.client.config.ts
@@ -6,8 +6,7 @@ import * as Sentry from '@sentry/nextjs'
 console.log('=== SENTRY SETUP: NODE_ENV ====', process.env.NODE_ENV)
 if (process.env.NODE_ENV == 'production') {
   Sentry.init({
-    dsn: process.env.MINTTER_SENTRY_SITES,
-
+    dsn: 'https://47c66bd7a6d64db68a59c03f2337e475@o4504088793841664.ingest.sentry.io/4505527493328896',
     // Adjust this value in production, or use tracesSampler for greater control
     tracesSampleRate: 1,
 
