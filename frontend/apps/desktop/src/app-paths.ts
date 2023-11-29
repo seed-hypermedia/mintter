9f390bffa6776bf572517709a47c538d0480a719
Horacio Herrera <hi@horacioh.com>
2023-11-29 15:47:39 +0100

    hotfix(desktop): prevent exposing test data on mainnet

--- frontend/apps/desktop/src/app-paths.ts
+++ frontend/apps/desktop/src/app-paths.ts
@@ -2,9 +2,11 @@ import {app} from 'electron'
 import path from 'path'
 
 export const APP_FOLDER_NAME =
-  process.env.NODE_ENV != 'production' && process.env.APP_FOLDER_NAME
-    ? process.env.APP_FOLDER_NAME
-    : 'Mintter'
+  process.env.MINTTER_P2P_TESTNET_NAME == '' ||
+  process.env.NODE_ENV == 'production' ||
+  !process.env.APP_FOLDER_NAME
+    ? 'Mintter'
+    : process.env.APP_FOLDER_NAME
 
 export const APP_USER_DATA_PATH = path.join(
   app.getPath('appData'),
