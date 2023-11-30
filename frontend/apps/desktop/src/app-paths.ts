74c6d1ea5249970dbe57cc5021f0898913eceadf
Eric Vicenti <eric@vicenti.net>
2023-11-29 16:26:42 -0800

    Revert "hotfix(desktop): prevent exposing test data on mainnet"
    This reverts commit 9f390bffa6776bf572517709a47c538d0480a719.

--- frontend/apps/desktop/src/app-paths.ts
+++ frontend/apps/desktop/src/app-paths.ts
@@ -2,11 +2,9 @@ import {app} from 'electron'
 import path from 'path'
 
 export const APP_FOLDER_NAME =
-  process.env.MINTTER_P2P_TESTNET_NAME == '' ||
-  process.env.NODE_ENV == 'production' ||
-  !process.env.APP_FOLDER_NAME
-    ? 'Mintter'
-    : process.env.APP_FOLDER_NAME
+  process.env.NODE_ENV != 'production' && process.env.APP_FOLDER_NAME
+    ? process.env.APP_FOLDER_NAME
+    : 'Mintter'
 
 export const APP_USER_DATA_PATH = path.join(
   app.getPath('appData'),
