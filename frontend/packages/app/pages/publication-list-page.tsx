102d4957397a8030b476f89e0430e4d8a565184f
Eric Vicenti <eric@vicenti.net>
2024-01-17 08:46:47 -0800

    Re-upgrade tamagui to 1.86.5

--- frontend/packages/app/pages/publication-list-page.tsx
+++ frontend/packages/app/pages/publication-list-page.tsx
@@ -23,7 +23,12 @@ import {
   idToUrl,
   unpackHmId,
 } from '@mintter/shared'
-import {Globe, Pencil, Trash, Verified} from '@tamagui/lucide-icons'
+import {
+  Globe,
+  Pencil,
+  Trash,
+  BadgeCheck as Verified,
+} from '@tamagui/lucide-icons'
 import copyTextToClipboard from 'copy-text-to-clipboard'
 import {ComponentProps, memo, useRef, useState} from 'react'
 import {useAppContext} from '../app-context'
@@ -224,6 +229,7 @@ function PublicationsList({}: {}) {
       f={1}
       height={'100%'}
       onLayout={(e) => {
+        console.log('======', e.nativeEvent)
         setContainerHeight(e.nativeEvent.layout.height)
         setContainerWidth(e.nativeEvent.layout.width)
       }}
