a4c77074b5c2c2d48ed4d8a180b287e3c05dc249
Horacio Herrera <hi@horacioh.com>
2024-01-18 14:32:25 +0100

    frontend: setup sentry replay plugin

--- frontend/apps/desktop/src/renderer.ts
+++ frontend/apps/desktop/src/renderer.ts
@@ -33,6 +33,10 @@ if (IS_PROD_DESKTOP) {
   Sentry.init({
     dsn: 'https://8d3089ffb71045dc911bc66efbd3463a@o4504088793841664.ingest.sentry.io/4505527460429824',
     debug: true,
+    integrations: [new Sentry.Replay()],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
   })
 }
 // setTimeout(() => {
