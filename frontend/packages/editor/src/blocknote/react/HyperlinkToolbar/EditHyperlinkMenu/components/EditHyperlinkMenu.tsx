5369205ed3f8a66b2b98e86b634eab2a277a1c11
Horacio Herrera <hi@horacioh.com>
2024-02-15 13:53:11 +0100

    revert latest link

--- frontend/packages/editor/src/blocknote/react/HyperlinkToolbar/EditHyperlinkMenu/components/EditHyperlinkMenu.tsx
+++ frontend/packages/editor/src/blocknote/react/HyperlinkToolbar/EditHyperlinkMenu/components/EditHyperlinkMenu.tsx
@@ -1,16 +1,11 @@
-import {Checkbox, createStyles, Stack} from '@mantine/core'
-import {createHmDocLink, isHypermediaScheme, unpackHmId} from '@mintter/shared'
-import {XStack} from '@mintter/ui'
-import {forwardRef, HTMLAttributes, useCallback, useState} from 'react'
+import {createStyles, Stack} from '@mantine/core'
+import {forwardRef, HTMLAttributes, useState} from 'react'
 import {RiLink, RiText} from 'react-icons/ri'
 import {EditHyperlinkMenuItem} from './EditHyperlinkMenuItem'
 
 export type EditHyperlinkMenuProps = {
   url: string
   text: string
-  isLatest: boolean
-  editCheckboxRef: any
-  handleLatest: (value: boolean) => void
   update: (url: string, text: string, latest: boolean) => void
 }
 
@@ -21,82 +16,37 @@ export type EditHyperlinkMenuProps = {
 export const EditHyperlinkMenu = forwardRef<
   HTMLDivElement,
   EditHyperlinkMenuProps & HTMLAttributes<HTMLDivElement>
->(
-  (
-    {
-      url,
-      text,
-      isLatest,
-      handleLatest,
-      editCheckboxRef,
-      update,
-      className,
-      ...props
-    },
-    ref,
-  ) => {
-    const {classes} = createStyles({root: {}})(undefined, {
-      name: 'EditHyperlinkMenu',
-    })
+>(({url, text, update, className, ...props}, ref) => {
+  const {classes} = createStyles({root: {}})(undefined, {
+    name: 'EditHyperlinkMenu',
+  })
 
-    const [currentUrl, setCurrentUrl] = useState(url)
-    const [currentText, setCurrentText] = useState(text)
-    const isHmHref = isHypermediaScheme(url)
+  const [currentUrl, setCurrentUrl] = useState(url)
+  const [currentText, setCurrentText] = useState(text)
 
-    const handleVersion = useCallback((versionMode: boolean) => {
-      let unpackedRef = unpackHmId(url)
-      if (unpackedRef) {
-        let newUrl = createHmDocLink({
-          documentId: unpackedRef?.qid,
-          version: unpackedRef?.version,
-          blockRef: unpackedRef?.blockRef,
-          variants: unpackedRef?.variants,
-          latest: versionMode,
-        })
-        setCurrentUrl(newUrl)
-
-        update(newUrl, text, true)
-      }
-    }, [])
-
-    return (
-      <Stack
-        {...props}
-        className={className ? `${classes.root} ${className}` : classes.root}
-        ref={ref}
-      >
-        <EditHyperlinkMenuItem
-          icon={RiLink}
-          mainIconTooltip={'Edit URL'}
-          autofocus={true}
-          placeholder={'Edit URL'}
-          value={currentUrl}
-          onChange={(value) => setCurrentUrl(value)}
-          onSubmit={() => update(currentUrl, text, false)}
-        />
-        <EditHyperlinkMenuItem
-          icon={RiText}
-          mainIconTooltip={'Edit Title'}
-          placeholder={'Edit Title'}
-          value={currentText}
-          onChange={(value) => setCurrentText(value)}
-          onSubmit={() => update(currentUrl, currentText, false)}
-        />
-        {isHmHref ? (
-          <XStack padding="$2">
-            <Checkbox
-              ref={editCheckboxRef}
-              size="xs"
-              label="Link to Latest Version"
-              checked={isLatest}
-              onChange={(event) => {
-                // handleVersion(event.currentTarget.checked)
-                handleLatest(event.currentTarget.checked)
-              }}
-            />
-          </XStack>
-        ) : null}
-      </Stack>
-    )
-  },
-)
+  return (
+    <Stack
+      {...props}
+      className={className ? `${classes.root} ${className}` : classes.root}
+      ref={ref}
+    >
+      <EditHyperlinkMenuItem
+        icon={RiLink}
+        mainIconTooltip={'Edit URL'}
+        autofocus={true}
+        placeholder={'Edit URL'}
+        value={currentUrl}
+        onChange={(value) => setCurrentUrl(value)}
+        onSubmit={() => update(currentUrl, text, false)}
+      />
+      <EditHyperlinkMenuItem
+        icon={RiText}
+        mainIconTooltip={'Edit Title'}
+        placeholder={'Edit Title'}
+        value={currentText}
+        onChange={(value) => setCurrentText(value)}
+        onSubmit={() => update(currentUrl, currentText, false)}
+      />
+    </Stack>
+  )
+})
