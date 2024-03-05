c50633e3e87223dbbeaea762a003a45a0ed29f48
Horacio Herrera <hi@horacioh.com>
2024-03-05 13:19:30 +0100

    caption font is not working

--- frontend/packages/ui/src/config/fonts.ts
+++ frontend/packages/ui/src/config/fonts.ts
@@ -126,33 +126,3 @@ export const editorBody = createGenericFont(
     sizeLineHeight: (x) => x * 1.5,
   },
 )
-
-export const captionFont = createGenericFont(
-  `caption`,
-  {
-    weight: {
-      1: '500',
-    },
-    size: {
-      1: 11,
-      2: 12,
-      3: 13,
-      4: 14,
-      5: 16,
-      6: 18,
-      7: 20,
-      8: 22,
-      9: 30,
-      10: 42,
-      11: 52,
-      12: 62,
-      13: 72,
-      14: 92,
-      15: 114,
-      16: 124,
-    },
-  },
-  {
-    sizeLineHeight: (x) => x * 1.5,
-  },
-)
